e7e7d5241ce24e7312ea2ff8d978b3663e6ade65
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-01-05 16:14:42 +0530

    Create 31. Next Permutation.cpp

---
+++ Arrays/31. Next Permutation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(nums.size()<=1){
+            return;
+        }
+        int i=nums.size()-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        
+        if(i>=0){
+            int j=nums.size()-1;
+            while(nums[j]<=nums[i]){
+                j--;
+            }
+            swap(nums[i],nums[j]);
+        }
+        
+        int j=nums.size()-1;
+        i++;
+        while(i<j){
+            swap(nums[i],nums[j]);
+                i++;
+                j--;
+        }
+    }
+};
