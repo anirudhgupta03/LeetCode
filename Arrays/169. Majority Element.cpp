8cd89b87228b78586528477d1853a6255bb889cb
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-01-27 15:51:06 +0530

    Create 169. Majority Element.cpp

---
+++ Arrays/169. Majority Element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int ele, cnt= 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            if(cnt == 0){
+                ele = nums[i];
+            }
+            
+            if(nums[i] == ele){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+            
+            
+        }
+        return ele;
+    }
+};
