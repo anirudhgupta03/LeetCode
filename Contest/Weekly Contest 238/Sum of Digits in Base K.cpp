a640e2fd7bf3352f5a852d4eb42a39fe35905477
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-04-25 14:38:24 +0530

    Create Sum of Digits in Base K.cpp

---
+++ Contest/Weekly Contest 238/Sum of Digits in Base K.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        
+        int sum = 0;
+        
+        while(n){
+            
+            sum += n % k;
+            n /= k;
+        }
+        
+        return sum;
+    }
+};
