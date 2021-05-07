65f8dd9d32b301d2e51fa8b017240fd5e152309f
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-07 22:30:59 +0530

    Create 1780. Check if Number is a Sum of Powers of Three.cpp

---
+++ Contest/Biweekly Contest 47/1780. Check if Number is a Sum of Powers of Three.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        
+        while(n){
+            
+            if(n % 3 == 0){
+                n /= 3;
+            }
+            else if(n % 3 == 1){
+                n--;
+                n /= 3;
+            }
+            else{
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
