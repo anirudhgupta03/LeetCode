596f15e1965d76b6f8a967dcdf68b25a61561dd0
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-25 15:47:21 +0530

    Create 1332. Remove Palindromic Subsequences.cpp

---
+++ Contest/Weekly Contest 173/1332. Remove Palindromic Subsequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        
+        string p = s;
+        reverse(p.begin(),p.end());
+        
+        if(s == p){
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+};
