44a0d298dc89e9b61dc87a50cb54df5020761c33
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-05-28 16:50:49 +0530

    Create 438. Find All Anagrams in a String - Best.cpp

---
+++ Sliding Window/438. Find All Anagrams in a String - Best.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        
+        vector<int> res;
+        
+        unordered_map<char,int> m;
+        
+        for(int i = 0; i < p.size(); i++){
+            m[p[i]]++;
+        }
+        
+        int k = p.length();
+        
+        int count = m.size();
+        
+        int i = 0, j = 0;
+        
+        while(j < s.size()){
+            
+            if(m.find(s[j]) != m.end()){
+                
+                m[s[j]]--;
+                
+                if(m[s[j]] == 0){
+                    count--;
+                }
+            }
+            
+            if(j - i + 1 < k){
+                j++;
+            }
+            else if(j - i + 1 == k){
+                
+                if(count == 0)
+                res.push_back(i);
+                
+                if(m.find(s[i]) != m.end()){
+                    
+                    m[s[i]]++;
+                    
+                    if(m[s[i]] == 1){
+                        count++;
+                    }
+                }
+                
+                i++;
+                j++;
+            }
+        }
+        return res;
+    }
+};
