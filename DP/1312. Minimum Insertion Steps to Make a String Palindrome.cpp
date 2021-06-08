b1d2f2798a9376ba6b351b2fa8c384791a90a66e
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-08 18:48:05 +0530

    Create 1312. Minimum Insertion Steps to Make a String Palindrome.cpp

---
+++ DP/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        
+        int n = s.size();
+        string p = s;
+        reverse(p.begin(),p.end());
+        
+        string dp[n+1][n+1];
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= n; j++){
+                
+                if(i == 0 || j == 0){
+                    dp[i][j] = "";
+                }
+                else if(s[i-1] == p[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + s[i-1];
+                }
+                else{
+                    if(dp[i-1][j].size() > dp[i][j-1].size()){
+                        dp[i][j] = dp[i-1][j];
+                    }
+                    else{
+                        dp[i][j] = dp[i][j-1];
+                    }
+                }
+            }
+        }
+        
+        return n - dp[n][n].size();
+    }
+};
