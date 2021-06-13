c610f88e9e3bfd2726f6945d51191f8777b9c3c4
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-13 16:17:06 +0530

    Kadane's Algo

---
+++ Arrays/1800. Maximum Ascending Subarray Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        
+        int currSum = nums[0], maxSum = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++){
+            
+            if(nums[i] <= nums[i-1]){
+                maxSum = max(maxSum,currSum);
+                currSum = nums[i];
+            }
+            else{
+                currSum += nums[i];
+            }
+        }
+        maxSum = max(maxSum,currSum);
+        return maxSum;
+    }
+};
