2dc6cd186d100191696d5642a39f8c4bd278e288
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-17 22:57:16 +0530

    Create 70. Climbing Stairs.cpp

---
+++ Top Interview Questions/70. Climbing Stairs.cpp
@@ -0,0 +1,21 @@
+// Time complexity : O(n). Single loop upto n.
+// Space complexity : O(n). dp array of size n is used.
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if(n == 1 || n == 2){
+            return n;
+        }
+        int dp[n+1];
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+};
