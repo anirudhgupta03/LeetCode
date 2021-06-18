d1667ee91d04fbe8464d19c088236170e26af895
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 20:13:40 +0530

    Update 1092. Shortest Common Supersequence.cpp

--- DP/1092. Shortest Common Supersequence.cpp
+++ DP/1092. Shortest Common Supersequence.cpp
@@ -314,3 +314,63 @@ public:
         return scs;
     }
 };
+
+//Method - 5
+//Ref: https://www.youtube.com/watch?v=VDhRg-ZJTuc&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=30
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        
+        int n1 = str1.size(), n2 = str2.size();
+        
+        int dp[n1+1][n2+1];
+        
+        for(int i = 0; i <= n1; i++){
+            for(int j = 0; j <= n2; j++){
+                
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        
+        string scs;
+        int i = n1, j = n2;
+        
+        while(i != 0 && j != 0){
+            
+            if(str1[i-1] == str2[j-1]){
+                scs.push_back(str1[i-1]);
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i-1][j] > dp[i][j-1]){
+                    scs.push_back(str1[i-1]);
+                    i--;
+                }
+                else{
+                    scs.push_back(str2[j-1]);
+                    j--;
+                }
+            }
+        }
+        
+        while(i){
+            scs.push_back(str1[i-1]);
+            i--;
+        }
+        while(j){
+            scs.push_back(str2[j-1]);
+            j--;
+        }
+        reverse(scs.begin(),scs.end());
+        return scs;
+    }
+};
