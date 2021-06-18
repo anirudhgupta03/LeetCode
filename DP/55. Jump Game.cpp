58a6aa5bac9063e6d62d0b6d8ac2aa12edd312f2
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-18 22:29:46 +0530

    Create 55. Jump Game.cpp

---
+++ DP/55. Jump Game.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int dp[n];
+        
+        memset(dp,0,sizeof(dp));
+        
+        dp[0] = 1;
+        
+        int cov = 0;      // So far the current max index we can jump to.
+        
+        for(int i = 0; i < n - 1; i++){
+            
+            int jumpLen = nums[i];
+            
+            if(dp[i]){
+                for(int j = cov + 1; j <= i + jumpLen; j++){
+                    if(j < n)
+                        dp[j] = 1;
+                    else
+                        break;
+                }
+                cov = max(cov,i + jumpLen);
+            }
+            else{
+                return false;
+            }
+
+        }
+        return dp[n-1];
+    }
+};
