88f53025d65203cf22c90d8c4e7a0cbd07216f01
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-06-25 23:17:05 +0530

    Create 23. Merge k Sorted Lists.cpp

---
+++ Linked List/23. Merge k Sorted Lists.cpp
@@ -0,0 +1,88 @@
+//Method - 1
+//Time complexity : O(kN) where k is the number of linked lists.
+//Space complexity : O(1)
+
+We can merge two sorted linked list in O(1)O(1) space.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        
+        if(l1 == NULL && l2 == NULL){
+            return NULL;
+        }
+        if(l1 == NULL){
+            return l2;
+        }
+        if(l2 == NULL){
+            return l1;
+        }
+        
+        ListNode* head, *prev = NULL;
+        
+        while(l1 && l2){
+            
+            if(l1 -> val < l2 -> val){
+                if(prev == NULL){
+                    head = l1;
+                    prev = head;
+                }
+                else{
+                    prev -> next = l1;
+                    prev = l1;
+                }
+                l1 = l1 -> next;
+            }
+            else{
+                if(prev == NULL){
+                    head = l2;
+                    prev = head;
+                }
+                else{
+                    prev -> next = l2;
+                    prev = l2;
+                }
+                l2 = l2 -> next;
+            }
+        }
+        
+        if(l1){
+            prev -> next = l1;
+        }
+        
+        if(l2){
+            prev -> next = l2;
+        }
+        
+        return head;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        int n = lists.size();
+        
+        if(n == 0){
+            return NULL;
+        }
+        if(n == 1){
+            return lists[0];
+        }
+        
+        ListNode* root = merge(lists[0],lists[1]);
+        
+        for(int i = 2; i < n; i++){
+            root = merge(root,lists[i]);
+        }
+        
+        return root;
+    }
+};
+
