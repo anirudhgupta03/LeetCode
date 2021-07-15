66ce6fef8276e3d0e3141bdc03de90fd2ca06d0f
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-15 15:24:43 +0530

    Create 217. Contains Duplicate.cpp

---
+++ Hashing/217. Contains Duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        unordered_set<int> us;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            if(us.find(nums[i]) != us.end()){
+                return true;
+            }
+            else{
+                us.insert(nums[i]);
+            }
+        }
+        
+        return false;
+    }
+};
