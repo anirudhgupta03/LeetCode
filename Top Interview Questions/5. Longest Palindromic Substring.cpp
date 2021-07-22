21ebc10ea672c72125874a60fcd489004695d545
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-07-22 22:48:41 +0530

    Update 5. Longest Palindromic Substring.cpp

--- Top Interview Questions/5. Longest Palindromic Substring.cpp
+++ Top Interview Questions/5. Longest Palindromic Substring.cpp
@@ -46,3 +46,45 @@ public:
         return s.substr(start,max_len);
     }
 };
+
+//Alter
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        int n = s.size();
+        
+        int dp[n][n];
+        memset(dp,0,sizeof(dp));
+        
+        int maxLen = 1;
+        
+        string res;
+        res = s[0];
+        
+        for(int i = 0; i < n; i++){
+            dp[i][i] = 1;
+        }
+        
+        for(int i = 0; i < n - 1; i++){
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = 1;
+                res = s.substr(i,2);
+                maxLen = 2;
+            }
+        }
+        
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = i + 2; j < n; j++){
+                if(s[i] == s[j] && dp[i+1][j-1] == 1){
+                    dp[i][j] = 1;
+                    if(j - i + 1 > maxLen){
+                        maxLen = j - i + 1;
+                        res = s.substr(i,j-i+1);    
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
