be5f5a87872089113aeb543ee19d1cd96f4c2a4c
anirudhkgupta222 <72066036+anirudhkgupta222@users.noreply.github.com>
2021-12-17 12:03:40 +0530

    Create 678. Valid Parenthesis String.cpp

---
+++ Stack/678. Valid Parenthesis String.cpp
@@ -0,0 +1,38 @@
+//Ref: https://www.youtube.com/watch?v=KuE_Cn3xhxI
+class Solution {
+public:
+    bool checkValidString(string s) {
+        
+        stack<int> open, star;
+        int len = s.size();
+        
+        for(int i = 0; i < s.size(); i++){
+            
+            if(s[i] == '('){
+                open.push(i);
+            }
+            else if(s[i] == '*'){
+                star.push(i);
+            }
+            else{
+                if(!open.empty()) open.pop();
+                else if(!star.empty()) star.pop();
+                else return false;
+            }
+        }
+        
+        while(!open.empty()){
+            if(star.empty()){
+                return false;
+            }
+            else if(open.top() < star.top()){
+                open.pop();
+                star.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
