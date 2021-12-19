f1a02a4239f2c67bc9b875dbd923b3a2aa8d52ca
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2021-12-19 18:03:50 +0530

    Update 119. Pascal's Triangle II.cpp

--- DP/119. Pascal's Triangle II.cpp
+++ DP/119. Pascal's Triangle II.cpp
@@ -1,3 +1,4 @@
+//Approach - 1
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
@@ -12,6 +13,25 @@ public:
                     dp[j] = dp[j] + dp[j-1];
                 }
             }
+        }
+        return dp;
+    }
+};
+
+//Approach - 2
+//Best Approach
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+        vector<int> dp(rowIndex + 1);
+        
+        long long val = 1;
+        dp[0] = 1;
+        
+        for(int i = 1; i <= rowIndex; i++){
+            val = val*(rowIndex - i + 1)/i;
+            dp[i] = (int)val;
         }
         return dp;
     }
