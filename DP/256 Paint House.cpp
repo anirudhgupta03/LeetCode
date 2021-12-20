ca14f3dde291363260ef25c34d7f6d49b30039fe
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2021-12-20 18:17:05 +0530

    Update and rename Paint House.cpp to 256 Paint House.cpp

--- DP/256 Paint House.cpp
+++ DP/256 Paint House.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -17,4 +18,27 @@ int minCost(vector<vector<int>> &cost)
         dp[2][i] = min(dp[0][i-1],dp[1][i-1]) + cost[i][2];
     }
     return min({dp[0][n-1],dp[1][n-1],dp[2][n-1]});
+}
+
+//Method - 2
+#include<bits/stdc++.h>
+using namespace std;
+
+int minCost(vector<vector<int>> &cost)
+{
+	int n = cost.size();
+    
+    int r,g,y;
+    
+    g = cost[0][0];
+    r = cost[0][1];
+    y = cost[0][2];
+    
+    for(int i = 1; i < n; i++){
+        int tg = g, tr = r, ty = y;
+        g = min(tr,ty) + cost[i][0];
+        r = min(tg,ty) + cost[i][1];
+        y = min(tg,tr) + cost[i][2];
+    }
+    return min({g,r,y});
 }
