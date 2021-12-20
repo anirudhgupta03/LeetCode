3f2a31b27bdb8657507cf5b6f9488eeb0be9d182
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2021-12-20 13:59:41 +0530

    Create 213. House Robber II.cpp

---
+++ DP/213. House Robber II.cpp
@@ -0,0 +1,107 @@
+//Nice Question
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n == 1){
+            return nums[0];
+        }
+        if(n == 2){
+            return max(nums[0],nums[1]);
+        }
+        
+        int maxrob = 0;
+        
+        vector<pair<int,int>> dp(n,{-1,-1});
+        
+        dp[0] = {nums[0],0};
+        if(nums[0] > nums[1]){
+            dp[1] = {max(nums[0],nums[1]),0};
+        }
+        else{
+            dp[1] = {max(nums[0],nums[1]),1};
+        }
+        
+        for(int i = 2; i < n - 1; i++){
+            if(dp[i-2].first + nums[i] > dp[i-1].first){        
+                dp[i] = {dp[i-2].first + nums[i],dp[i-2].second};
+            }
+            else if(dp[i-2].first + nums[i] == dp[i-1].first){
+                if(dp[i-1].second == 1 || dp[i-2].second == 1){
+                    dp[i] = {dp[i-2].first + nums[i],1};
+                }
+                else{
+                    dp[i] = {dp[i-2].first + nums[i],0};
+                }
+            }
+            else{
+                dp[i] = {dp[i-1].first,dp[i-1].second};
+            }
+        }
+        
+        dp[n-1] = {dp[n-2].first, dp[n-2].second};
+        
+        if(dp[n-3].second == 0){
+            if(dp[n-3].first > dp[n-1].first){
+                dp[n-1].first = dp[n-3].first;
+            }
+        }
+        else{
+            if(dp[n-3].first + nums[n-1] > dp[n-1].first){
+                dp[n-1].first = dp[n-3].first + nums[n-1]; 
+            }
+        }
+        if(nums[n-1] > dp[n-1].first) dp[n-1].first = nums[n-1];
+       
+        int res = dp[n-1].first;
+        
+        for(int i = 0; i < n; i++){
+            dp[i] = {-1,-1};
+        }
+        
+        dp[n-1] = {nums[n-1],0};
+        if(nums[n-1] > nums[n-2]){
+            dp[n-2] = {max(nums[n-1],nums[n-2]),0};
+        }
+        else{
+            dp[n-2] = {max(nums[n-1],nums[n-2]),1};
+        }
+        
+        for(int i = n-3; i >= 1; i--){
+            
+            if(dp[i+2].first + nums[i] > dp[i+1].first){        
+                dp[i] = {dp[i+2].first + nums[i],dp[i+2].second};
+            }
+            else if(dp[i+2].first + nums[i] == dp[i+1].first){
+                if(dp[i+1].second == 1 || dp[i+2].second == 1){
+                    dp[i] = {dp[i+2].first + nums[i],1};
+                }
+                else{
+                    dp[i] = {dp[i+2].first + nums[i],0};
+                }
+            }
+            else{
+                dp[i] = {dp[i+1].first,dp[i+1].second};
+            }
+        }
+        
+        dp[0] = {dp[1].first, dp[1].second};
+        
+        if(dp[2].second == 0){
+            if(dp[2].first > dp[0].first){
+                dp[0].first = dp[2].first;
+            }
+        }
+        else{
+            if(dp[2].first + nums[0] > dp[0].first){
+                dp[0].first = dp[2].first + nums[0]; 
+            }
+        }
+        if(nums[0] > dp[0].first) dp[0].first = nums[0];
+        
+        res = max(res, dp[0].first);
+        return res;
+    }
+};
