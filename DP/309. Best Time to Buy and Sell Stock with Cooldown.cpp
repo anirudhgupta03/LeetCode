775fc046f3021a3f875c22c639414bd9d74edb14
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2021-12-22 16:24:39 +0530

    Create 309. Best Time to Buy and Sell Stock with Cooldown.cpp

---
+++ DP/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -0,0 +1,43 @@
+//Ref: https://www.youtube.com/watch?v=GY0O57llkKQ&t=13s
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        
+        int obsp = -arr[0];
+        int ossp = 0;
+        int ocsp = 0;
+        
+        for(int i = 1; i < arr.size(); i++){
+            
+            int nbsp = 0;
+            int nssp = 0;
+            int ncsp = 0;
+            
+            if(ocsp - arr[i] > obsp){
+                nbsp = ocsp - arr[i];
+            }
+            else{
+                nbsp = obsp;
+            }
+            
+            if(obsp + arr[i] > ossp){
+                nssp = obsp + arr[i];
+            }
+            else{
+                nssp = ossp;
+            }
+            
+            if(ossp > ocsp){
+                ncsp = ossp;
+            }
+            else{
+                ncsp = ocsp;
+            }
+            
+            obsp = nbsp;
+            ossp = nssp;
+            ocsp = ncsp;
+        }
+        return ossp;
+    }
+};
