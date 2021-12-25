c21b03ea4d80c4eb7a03fecbc6139c5009fc3bb1
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2021-12-25 18:11:33 +0530

    Create 435. Non-overlapping Intervals.cpp

---
+++ DP/435. Non-overlapping Intervals.cpp
@@ -0,0 +1,23 @@
+//Greedy Approach
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(),intervals.end());
+        
+        int count = 0;
+    
+        int end = intervals[0][1];
+        
+        for(int i = 1; i < intervals.size(); i++){
+            if(end > intervals[i][0]){
+                count++;
+                end = min(end,intervals[i][1]);
+            }
+            else{
+                end = intervals[i][1];
+            }
+        }
+        return count;
+    }
+};
