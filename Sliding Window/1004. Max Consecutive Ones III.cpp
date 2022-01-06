d18675045be00ba0c5146430e66df218dddec0c2
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2022-01-06 10:49:28 +0530

    Create 1004. Max Consecutive Ones III.cpp

---
+++ Sliding Window/1004. Max Consecutive Ones III.cpp
@@ -0,0 +1,35 @@
+//Ref: https://www.youtube.com/watch?v=pwaVTOazeb4
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+        int lo = 0, hi = 0;
+        
+        int fc = 0, maxlen = 0;
+        
+        while(hi < nums.size()){
+            
+            if(nums[hi] == 1) hi++;
+            
+            else if(nums[hi] == 0 && fc < k){
+                hi++;
+                fc++;
+            }
+            else if(nums[hi] == 0 && fc == k){
+                
+                while(lo <= hi){
+                    maxlen = max(maxlen, hi - lo);
+                    if(nums[lo] == 1) lo++;
+                    else if(nums[lo] == 0){
+                        maxlen = max(maxlen, hi - lo);
+                        lo++;
+                        break;
+                    }
+                }
+                hi++;
+            }
+        }
+        maxlen = max(maxlen, hi - lo);
+        return maxlen;
+    }
+};
