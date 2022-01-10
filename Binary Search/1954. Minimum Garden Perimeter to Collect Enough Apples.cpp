e1f826c2912cb5a02815edd17b19cd84a982c630
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2022-01-10 14:32:33 +0530

    Update 1954. Minimum Garden Perimeter to Collect Enough Apples.cpp

--- Binary Search/1954. Minimum Garden Perimeter to Collect Enough Apples.cpp
+++ Binary Search/1954. Minimum Garden Perimeter to Collect Enough Apples.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
     #define ll long long
-    bool isPossible(ll side, ll neededApples, vector<ll> &v){
-        if(v[side - 1] >= neededApples){
-            return true;
-        }
-        return false;
-    }
     long long minimumPerimeter(long long neededApples) {
-        
-        vector<ll> v;
+    
         ll sum = 0, length = 0;
         
-        while(sum <= 1e15){
+        while(sum < neededApples){
             length++;
             sum += 12*(length*length);
-            v.push_back(sum);
         }
-        
-        ll lo = 1, hi = v.size(), ans;
-        
-      while(lo <= hi){
-            ll mid = (lo + hi)/2;
-            if(isPossible(mid, neededApples, v)){
-                ans = mid;
-                hi = mid - 1;
-            }
-            else{
-                lo = mid + 1;
-            }
-        }
-        return 8*ans;
+
+        return 8*length;
     }
 };
