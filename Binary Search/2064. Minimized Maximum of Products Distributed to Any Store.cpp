b411c5e70d95417cdac1f53d5dba2d19fba11929
Anirudh Gupta <72066036+anirudhkgupta222@users.noreply.github.com>
2022-01-10 16:27:11 +0530

    Create 2064. Minimized Maximum of Products Distributed to Any Store.cpp

---
+++ Binary Search/2064. Minimized Maximum of Products Distributed to Any Store.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPossible(int products, int n, vector<int> &quantities){
+        
+        int count = 0;
+        for(int i = 0; i < quantities.size(); i++){
+            count += quantities[i]/products;
+            if(quantities[i] % products != 0) count++;
+        }
+        if(count <= n){
+            return true;
+        }
+        return false;
+    }
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        
+        int lo = 1, hi = *max_element(quantities.begin(),quantities.end());
+        int ans;
+        
+        while(lo <= hi){
+            int mid = (lo + hi)/2;
+            if(isPossible(mid, n, quantities)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
