3bf82611645b617c676c586e3a5148a7d91ce957
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-01-19 11:12:42 +0530

    Create 1722. Minimize Hamming Distance After Swap Operations.cpp

---
+++ Graph Theory/1722. Minimize Hamming Distance After Swap Operations.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    void unionp(int node1, int node2, vector<int> &par){
+        par[node2] = node1;
+    }
+    int findp(int node, vector<int> &par){
+        if(par[node] == node){
+            return node;
+        }
+        return par[node] = findp(par[node], par);
+    }
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        
+        int n = source.size();
+        
+        vector<int> par(n);
+        
+        for(int i = 0; i < n; i++){
+            par[i] = i;
+        }
+        
+        for(auto &x: allowedSwaps){
+            
+            int u = x[0], v = x[1];
+            
+            int paru = findp(u, par);
+            int parv = findp(v, par);
+            
+            if(paru != parv){
+                unionp(paru, parv, par);
+            }
+        }
+        for(int i = 0; i < n; i++){
+            par[i] = findp(i, par);
+        }
+        
+        int count = 0;
+        map<int,unordered_map<int,int>> mp;
+        
+        for(int i = 0; i < n; i++){
+            mp[par[i]][source[i]]++;;
+        }
+   
+        unordered_set<int> us;
+        for(int i = 0; i < n; i++){
+            if(mp[par[i]][target[i]] != 0){
+                mp[par[i]][target[i]]--;
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
+    }
+};
