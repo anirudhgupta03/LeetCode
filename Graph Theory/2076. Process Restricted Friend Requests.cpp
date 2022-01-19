fe76651c881c6524d8769edda9d72cee6f1d9b11
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-01-19 20:42:33 +0530

    Create 2076. Process Restricted Friend Requests.cpp

---
+++ Graph Theory/2076. Process Restricted Friend Requests.cpp
@@ -0,0 +1,144 @@
+//Ref: https://www.youtube.com/watch?v=ePg-pEUzWAU
+//Method - 1
+class Solution {
+public:
+    int findp1(int node, vector<int> &par){
+        if(par[node] == node){
+            return node;
+        }
+        return par[node] = findp1(par[node], par);
+    }
+    void unionp(int node1, int node2, vector<int> &par){
+        par[node2] = node1;
+    }
+    int findp2(int node, vector<int> &par){
+        if(par[node] == node){
+            return node;
+        }
+        return findp2(par[node], par);
+    }
+    vector<bool> friendRequests(int n, vector<vector<int>>& restrictions, vector<vector<int>>& requests) {
+        
+        vector<int> par(n);
+        
+        for(int i = 0; i < n; i++){
+            par[i] = i;
+        }
+        
+        vector<bool> res;
+        
+        for(int i = 0; i < requests.size(); i++){
+            
+            int u = requests[i][0], v = requests[i][1];
+            
+            int paru = findp1(u, par);
+            int parv = findp1(v, par);
+            
+            if(paru == parv){
+                res.push_back(true);
+            }
+            else{
+                int temp = par[parv];
+                par[parv] = paru;
+                
+                bool flag = true;
+                
+                for(int j = 0; j < restrictions.size(); j++){
+                    int a = restrictions[j][0], b = restrictions[j][1];
+                    int para = findp2(a, par);
+                    int parb = findp2(b, par);
+                    
+                    if(para == parb){
+                        flag = false;
+                        par[parv] = temp;
+                        break;
+                    }
+                }
+                res.push_back(flag);
+            }
+        }
+        return res;
+    }
+};
+
+//Method - 2 (Union by Rank and Path Compression)
+class Solution {
+public:
+    int findp1(int node, vector<int> &par){
+        if(par[node] == node){
+            return node;
+        }
+        return par[node] = findp1(par[node], par);
+    }
+    void unionp(int node1, int node2, vector<int> &par){
+        par[node2] = node1;
+    }
+    int findp2(int node, vector<int> &par){
+        if(par[node] == node){
+            return node;
+        }
+        return findp2(par[node], par);
+    }
+    vector<bool> friendRequests(int n, vector<vector<int>>& restrictions, vector<vector<int>>& requests) {
+        
+        vector<int> par(n), rank(n, 1);
+        
+        for(int i = 0; i < n; i++){
+            par[i] = i;
+        }
+        
+        vector<bool> res;
+        
+        for(int i = 0; i < requests.size(); i++){
+            
+            int u = requests[i][0], v = requests[i][1];
+            
+            int paru = findp1(u, par);
+            int parv = findp1(v, par);
+            
+            if(paru == parv){
+                res.push_back(true);
+            }
+            else{
+                int temp1 = par[parv], temp2 = par[paru];
+                bool flag1 = false, flag2 = false;
+                if(rank[paru] >= rank[parv]){
+                    rank[paru] += rank[parv];
+                    par[parv] = paru;
+                    flag1 = true;
+                }
+                else{
+                    flag2 = true;
+                    rank[parv] += rank[paru];
+                    par[paru] = parv;
+                }
+                
+                bool flag = true;
+                
+                for(int j = 0; j < restrictions.size(); j++){
+                    int a = restrictions[j][0], b = restrictions[j][1];
+                    
+                    int para = findp2(a, par);
+                    int parb = findp2(b, par);
+                    
+                    if(para == parb){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(!flag){
+                    if(flag1){
+                        par[parv] = temp1;
+                        rank[paru] -= rank[parv];
+                    }
+                    else{
+                        par[paru] = temp2;
+                        rank[parv] -= rank[paru];
+                    }
+                }
+                res.push_back(flag);
+            }
+        }
+        return res;
+    }
+};
