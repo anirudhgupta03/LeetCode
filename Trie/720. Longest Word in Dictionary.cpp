64a14bcb22e97964ad4ee6fa0a11eb04a14250b8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-01-26 23:14:36 +0530

    Update 720. Longest Word in Dictionary.cpp

--- Trie/720. Longest Word in Dictionary.cpp
+++ Trie/720. Longest Word in Dictionary.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     struct TrieNode{
@@ -59,5 +60,71 @@ public:
         }
         
         return ans;
+    }
+};
+
+//Alter
+class Solution {
+public:
+    struct TrieNode{
+        bool isEnd;
+        TrieNode* child[26];
+        
+        TrieNode(){
+            isEnd = false;
+            for(int i = 0; i < 26; i++){
+                child[i] = NULL;
+            }
+        }
+    };
+    bool static cmp(string &s1, string &s2){
+        if(s1.size() == s2.size()){
+            return s1 < s2;
+        }
+        return s1.size() < s2.size();
+    }
+    bool helper(TrieNode* root, string &s){
+        
+        int n = s.size();
+        bool flag = true;
+        for(int i = 0; i < s.size() - 1; i++){
+            if(root -> child[s[i] - 'a'] == NULL){
+                flag = false;
+                root -> child[s[i] - 'a'] = new TrieNode();
+            }
+            root = root -> child[s[i] - 'a'];
+            if(!(root -> isEnd)){
+                flag = false;
+            }
+        }
+        if(root -> child[s[n - 1] - 'a'] == NULL){
+            root -> child[s[n - 1] - 'a'] = new TrieNode();
+        }
+        root = root -> child[s[n - 1] - 'a'];
+        root -> isEnd = true;
+        return flag;
+    }
+    string longestWord(vector<string>& words) {
+        
+        TrieNode* root = new TrieNode();
+        
+        sort(words.begin(), words.end(), cmp);
+        
+        int maxlen = 0;
+        string res;
+        for(int i = 0; i < words.size(); i++){
+            if(helper(root, words[i])){
+                if(maxlen < words[i].size()){
+                    maxlen = words[i].size();
+                    res = words[i];
+                }
+                else if(maxlen == words[i].size()){
+                    if(words[i] < res){
+                        res = words[i];
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
