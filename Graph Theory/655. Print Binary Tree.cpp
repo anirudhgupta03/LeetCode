c74b79d4b625062d9d7c9d0a2b776e4c54dce9ef
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-01-28 20:36:45 +0530

    Create 655. Print Binary Tree.cpp

---
+++ Graph Theory/655. Print Binary Tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    #define ptpii pair<TreeNode*,pair<int,int>>
+    vector<vector<string>> printTree(TreeNode* root) {
+        
+        int level = 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                TreeNode* curr = q.front();
+                q.pop();
+                
+                if(curr -> left) q.push(curr -> left);
+                if(curr -> right) q.push(curr -> right);
+            }
+            level++;
+        }
+        
+        int m = level, n = pow(2,level) - 1;
+        vector<vector<string>> res(m, vector<string>(n, ""));
+        
+        queue<ptpii> que;
+        que.push({root,{0, (n - 1)/2}});
+        
+        while(!que.empty()){
+            
+            ptpii p = que.front();
+            TreeNode* curr = p.first;
+            int r = p.second.first, c = p.second.second;
+            res[r][c] = to_string(curr -> val);
+            que.pop();
+            
+            if(curr -> left){
+                que.push({curr -> left,{r + 1, c - pow(2, level - r - 2)}});
+            }
+            if(curr -> right){
+                que.push({curr -> right,{r + 1, c + pow(2, level - r - 2)}});
+            }
+        }
+        return res;
+    }
+};
