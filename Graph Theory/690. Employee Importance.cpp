68e43d7bce429891828618c23fe9842cea1855be
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-01-29 15:33:17 +0530

    Create 690. Employee Importance.cpp

---
+++ Graph Theory/690. Employee Importance.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        
+        queue<int> q;
+        q.push(id);
+        
+        unordered_set<int> vis;
+        
+        int totalImportance = 0;
+        while(!q.empty()){
+          
+            int curr = q.front();
+            q.pop();
+            if(vis.find(curr) != vis.end()) continue;
+            vis.insert(curr);  
+            for(int i = 0; i < employees.size(); i++){
+                if(employees[i] -> id == curr){
+                    totalImportance += employees[i] -> importance;
+                    vector<int> v = employees[i] -> subordinates;
+                    for(int j = 0; j < v.size(); j++){
+                        if(vis.find(v[j]) == vis.end()){
+                            q.push(v[j]);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        return totalImportance;
+    }
+};
