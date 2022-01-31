5fc34f2c513c181347ce231fe16e3b421e9258a2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-01-31 11:17:21 +0530

    Create 1302. Deepest Leaves Sum.cpp

---
+++ Graph Theory/1302. Deepest Leaves Sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        
+        if(root == NULL){
+            return 0;
+        }
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int sum;
+        while(!q.empty()){
+            int sz = q.size();
+            sum = 0;
+            while(sz--){
+                TreeNode* curr  = q.front();
+                q.pop();
+                sum += curr -> val;
+                if(curr -> left) q.push(curr -> left);
+                if(curr -> right) q.push(curr -> right);
+            }
+        }
+        return sum;
+    }
+};
