0bc5fa0423fc1f924fe060c06e3689d59aa48be4
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-02-08 09:59:10 +0530

    Create 258. Add Digits.cpp

---
+++ Maths/258. Add Digits.cpp
@@ -0,0 +1,11 @@
+//Ref: https://www.youtube.com/watch?v=aOu5fgdNNzU
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num == 0){
+            return 0;
+        }
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+};
