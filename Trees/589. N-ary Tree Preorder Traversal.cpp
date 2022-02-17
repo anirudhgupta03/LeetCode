4bdc2e06a65aaf4cbca0ace85c2dc1f10e58785c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-02-17 10:58:27 +0530

    Create 589. N-ary Tree Preorder Traversal.cpp

---
+++ Trees/589. N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> res;
+    vector<int> preorder(Node* root) {
+        
+        res.clear();
+        if(root == NULL){
+            return res;
+        }
+        stack<Node*> st;
+        st.push(root);
+        
+        while(!st.empty()){
+            Node* curr = st.top();
+            st.pop();
+            res.push_back(curr -> val);
+            
+            for(int i = (curr -> children).size() - 1; i >= 0; i--){
+                st.push((curr -> children)[i]);
+            }
+        }
+        return res;
+    }
+};
