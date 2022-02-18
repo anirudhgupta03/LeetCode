ae9e4d4a006f6bb062b55c152037b27ffde098b7
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-02-18 16:58:09 +0530

    Create 856. Score of Parentheses.cpp

---
+++ Stack/856. Score of Parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        
+        stack<int> st;
+        
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                st.push(-1);
+            }
+            else if(s[i] == ')'){
+                if(st.top() == -1){
+                    st.pop();
+                    st.push(1);
+                }
+                else{
+                    int sum = 0;
+                    while(st.top() != -1){
+                        sum += st.top();
+                        st.pop();
+                    }
+                    st.pop();
+                    st.push(2*sum);
+                }
+            }
+        }
+        int res = 0;
+        while(st.size()){
+            res += st.top();
+            st.pop();
+        }
+        return res;
+    }
+};
