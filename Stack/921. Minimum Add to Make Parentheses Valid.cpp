9af8cf459a4b64b2ba9da6ce4be76718ce3f0c0b
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-02-24 16:18:55 +0530

    Update 921. Minimum Add to Make Parentheses Valid.cpp

--- Stack/921. Minimum Add to Make Parentheses Valid.cpp
+++ Stack/921. Minimum Add to Make Parentheses Valid.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//TC - O(N)
+//SC - O(N)
 class Solution {
 public:
     int minAddToMakeValid(string s) {
@@ -18,5 +21,31 @@ public:
             }
         }
         return st.size();
+    }
+};
+
+//Method - 2
+//TC - O(N)
+//SC - O(1)
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        
+        int open = 0, close = 0;
+        
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == ')'){
+                if(open){
+                    open--;
+                }
+                else{
+                    close++;
+                }
+            }
+            else{
+                open++;
+            }
+        }
+        return close + open;
     }
 };
