fd51e0302303b79b6f31317234b23a6def6b1fc5
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-03-12 22:17:59 +0530

    Update 138. Copy List with Random Pointer.cpp

--- Linked List/138. Copy List with Random Pointer.cpp
+++ Linked List/138. Copy List with Random Pointer.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//TC - O(n)
+//SC - O(1)
 /*
 // Definition for a Node.
 class Node {
@@ -57,6 +60,60 @@ public:
                 temp2 -> next = temp1 -> next;
                 temp2 = temp1 -> next; 
             }
+        }
+        return root;
+    }
+};
+
+//Method - 2
+//TC - O(n)
+//SC - O(n)
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == NULL){
+            return NULL;
+        }
+        Node* temp = head, *pre = NULL, *root = NULL;;
+        
+        unordered_map<Node*,Node*> umap;
+        
+        while(temp){
+            Node* node = new Node(temp -> val);
+            if(root == NULL){
+                root = node;
+                pre = root;
+            }
+            else{
+                pre -> next = node;
+                pre = node;
+            }
+            umap[temp] = node;
+            temp = temp -> next;
+        }
+        Node* temp1 = head, *temp2 = root;
+        while(temp1){
+            if(temp1 -> random){
+                temp2 -> random = umap[temp1 -> random];
+            }
+            temp1 = temp1 -> next;
+            temp2 = temp2 -> next;
         }
         return root;
     }
