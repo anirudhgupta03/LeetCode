5495914b62f104f8529f01f495b7b4fbdf927508
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-30 14:44:18 +0530

    Update 1300. Sum of Mutated Array Closest to Target.cpp

--- Binary Search/1300. Sum of Mutated Array Closest to Target.cpp
+++ Binary Search/1300. Sum of Mutated Array Closest to Target.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     int findBestValue(vector<int>& arr, int target) {
@@ -60,6 +61,74 @@ public:
             
             if(tsum > target) hi = mid - 1;
             else lo = mid + 1;
+        }
+        return ans;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int mutatedArraySum(int ele, vector<int> &prefix, vector<int> &arr){
+        
+        int lo = 0, hi = arr.size() - 1;
+        int ans = arr.size();
+        
+        while(lo <= hi){
+            int mid = (lo + hi)/2;
+            if(arr[mid] <= ele){
+                lo = mid + 1;
+            }
+            else{
+                ans = mid;
+                hi = mid - 1;
+            }
+        }
+        return prefix[ans] + (arr.size() - ans)*ele;
+    }
+    int findBestValue(vector<int>& arr, int target) {
+        
+        sort(arr.begin(), arr.end());
+        
+        int n = arr.size();
+        
+        vector<int> prefix(n + 1, 0);
+        
+        for(int i = 1; i <= n; i++){
+            prefix[i] = prefix[i - 1] + arr[i - 1];
+        }
+        
+        if(prefix[n] <= target){
+            return arr[n - 1];
+        }
+        
+        int lo = 0, hi = arr[n - 1], minDiff = INT_MAX, ans;
+        
+        while(lo <= hi){
+            
+            int mid = (lo + hi)/2;
+            int sum = mutatedArraySum(mid, prefix, arr);
+            
+            if(sum < target){
+                if(target - sum < minDiff){
+                    minDiff = target - sum;
+                    ans = mid;
+                }
+                else if(target - sum == minDiff){
+                    ans = min(ans, mid);
+                }
+                lo = mid + 1;
+            }
+            else{
+                if(sum - target < minDiff){
+                    minDiff = sum - target;
+                    ans = mid;
+                }
+                else if(sum - target == minDiff){
+                    ans = min(ans, mid);
+                }
+                hi = mid - 1;
+            }
         }
         return ans;
     }
