053f4c9ce1a04f849cc44235bb81c8cccdd96ed7
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-30 10:10:06 +0530

    Update 1170. Compare Strings by Frequency of the Smallest Character.cpp

--- Binary Search/1170. Compare Strings by Frequency of the Smallest Character.cpp
+++ Binary Search/1170. Compare Strings by Frequency of the Smallest Character.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
@@ -51,6 +52,63 @@ public:
             }
             
             res[i] = n - ans - 1;
+        }
+        return res;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        
+        int n = words.size();
+        
+        vector<int> freq(n);
+        
+        for(int i = 0; i < n; i++){
+            int alpha[26] = {0};
+            for(int j = 0; j < words[i].size(); j++){
+                alpha[words[i][j] - 'a']++;
+            }
+            
+            for(int j = 0; j < 26; j++){
+                if(alpha[j]){
+                    freq[i] = alpha[j];
+                    break;
+                }
+            }
+        }
+        sort(freq.begin(), freq.end());
+    
+        vector<int> res;
+        
+        for(int i = 0; i < queries.size(); i++){
+            int alpha[26] = {0};
+            for(int j = 0; j < queries[i].size(); j++){
+                alpha[queries[i][j] - 'a']++;
+            }
+            int currfreq = 0;
+            for(int j = 0; j < 26; j++){
+                if(alpha[j]){
+                    currfreq = alpha[j];
+                    break;
+                }
+            }
+            
+            int ans = n, lo = 0, hi = n - 1;
+            
+            while(lo <= hi){
+                int mid = (lo + hi)/2;
+                if(freq[mid] <= currfreq){
+                    lo = mid + 1;
+                }
+                else{
+                    ans = mid;
+                    hi = mid - 1;
+                }
+            }
+            res.push_back(n - ans);
         }
         return res;
     }
