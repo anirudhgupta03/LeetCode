aa857b40ee2a41ba7a4389f194d8758e212df51b
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-31 22:18:27 +0530

    Update 475. Heaters.cpp

--- Binary Search/475. Heaters.cpp
+++ Binary Search/475. Heaters.cpp
@@ -1,3 +1,54 @@
+//Method - 1
+class Solution {
+public:
+    #define ll long long
+    bool heaterCoversAllHouses(ll radius, vector<int> &houses, vector<int> &heaters){
+        
+        ll ptr1 = 0, ptr2 = 0;
+        
+        while(ptr1 < houses.size() && ptr2 < heaters.size()){
+            if(heaters[ptr2] >= houses[ptr1]){
+                if((ll)heaters[ptr2] - radius <= (ll)houses[ptr1]){
+                    ptr1++;
+                }
+                else{
+                    return 0;
+                }
+            }
+            else{
+                if((ll)heaters[ptr2] + radius >= (ll)houses[ptr1]){
+                    ptr1++;
+                }
+                else{
+                    ptr2++;
+                }
+            }
+        }
+        if(ptr1 == houses.size()) return 1;
+        return 0;
+    }
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+        
+        ll lo = 0, hi = 1e9 + 7, ans;
+        
+        while(lo <= hi){
+            ll mid = lo + (hi - lo)/2;
+            if(heaterCoversAllHouses(mid, houses, heaters)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//Method - 2
 class Solution {
 public:
     int findRadius(vector<int>& houses, vector<int>& heaters) {
