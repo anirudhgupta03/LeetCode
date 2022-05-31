2fc2a7747e8d067b184133e10f02f5df74ee0756
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-31 09:59:31 +0530

    Update 1838. Frequency of the Most Frequent Element.cpp

--- Binary Search/1838. Frequency of the Most Frequent Element.cpp
+++ Binary Search/1838. Frequency of the Most Frequent Element.cpp
@@ -1,3 +1,60 @@
+//Method - 1
+class Solution {
+public:
+    #define ll long long
+    bool isPossible(int freq, vector<int> &nums, ll k, vector<ll> &prefix){
+        
+        ll i = 0, ele = nums[i];
+        
+        while(i < nums.size() && ele == nums[i]){
+            i++;
+        }
+        if(i >= freq){
+            return true;
+        }
+        while(i < nums.size()){
+            ele = nums[i];
+            while(i < nums.size() && nums[i] == ele){
+                i++;
+            }
+            if(i - freq >= 0){
+                ll op = (ll)freq*nums[i - 1] - (prefix[i] - prefix[i - freq]);
+                if(op <= k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    int maxFrequency(vector<int>& nums, int k) {
+        
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        
+        vector<ll> prefix(n + 1, 0);
+        
+        for(int i = 1; i <= n; i++){
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+        
+        int lo = 1, hi = n, ans;
+        
+        while(lo <= hi){
+            int mid = (lo + hi)/2;
+            if(isPossible(mid, nums, k, prefix)){
+                ans = mid;
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//Method - 2
 //Ref: https://www.youtube.com/watch?v=nveGJc_oYAI
 class Solution {
 public:
