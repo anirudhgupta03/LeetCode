271f0023c264db3e6b799c420cd6f90b8534d6c2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-31 21:48:17 +0530

    Update 436. Find Right Interval.cpp

--- Binary Search/436. Find Right Interval.cpp
+++ Binary Search/436. Find Right Interval.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
@@ -37,6 +38,43 @@ public:
             }
             
             res[end[i].second] = ind;
+        }
+        return res;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> temp = intervals;
+        
+        for(int i = 0; i < temp.size(); i++){
+            temp[i].push_back(i);
+        }
+        sort(temp.begin(), temp.end());
+        
+        vector<int> res;
+        
+        for(int i = 0; i < intervals.size(); i++){
+            
+            int lo = 0, hi = temp.size() - 1, endi = intervals[i][1], ans = -1;
+            
+            while(lo <= hi){
+                
+                int mid = lo + (hi - lo)/2;
+                
+                if(temp[mid][0] >= endi){
+                    ans = temp[mid][2];
+                    hi = mid - 1;
+                } 
+                else{
+                    lo = mid + 1;
+                }
+            }
+            
+            res.push_back(ans);
         }
         return res;
     }
