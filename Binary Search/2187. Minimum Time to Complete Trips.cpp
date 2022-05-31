c48c956926f5d002f27dc590503c3618031a39da
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-31 18:20:50 +0530

    Update 2187. Minimum Time to Complete Trips.cpp

--- Binary Search/2187. Minimum Time to Complete Trips.cpp
+++ Binary Search/2187. Minimum Time to Complete Trips.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     #define ll long long
@@ -27,6 +28,40 @@ public:
             ll mid = (lo + hi)/2;
             
             if(isValid(mid, time, totalTrips)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    #define ll long long
+    bool isPossibleToComplete(ll t, vector<int> &time, ll totalTrips){
+        ll count = 0;
+        for(int i = 0; i < time.size(); i++){
+            count += t/time[i];
+            if(count >= totalTrips) return true;
+        }
+        return false;
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        
+        ll lo = 1, hi = (ll)*min_element(time.begin(), time.end())*totalTrips;
+        
+        ll ans;
+        
+        while(lo <= hi){
+            
+            ll mid = lo + (hi - lo)/2;
+            
+            if(isPossibleToComplete(mid, time, totalTrips)){
                 ans = mid;
                 hi = mid - 1;
             }
