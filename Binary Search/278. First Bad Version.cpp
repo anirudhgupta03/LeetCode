6465f35b377773bf16408622f9cfbb287ae10a41
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-05-31 18:54:26 +0530

    Update 278. First Bad Version.cpp

--- Binary Search/278. First Bad Version.cpp
+++ Binary Search/278. First Bad Version.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 // The API isBadVersion is defined for you.
 // bool isBadVersion(int version);
 
@@ -13,6 +14,32 @@ public:
         while(lo <= hi){
             
             ll mid = (lo + hi)/2;
+            
+            if(isBadVersion(mid)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//Method - 2
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        
+        int lo = 1, hi = n, ans;
+        
+        while(lo <= hi){
+            
+            int mid = lo + (hi - lo)/2;
             
             if(isBadVersion(mid)){
                 ans = mid;
