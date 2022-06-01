bcf2167bb8c959998110e1946d8dcae1ad7ee4ab
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-01 09:59:33 +0530

    Update 1060 Missing Element in Sorted Array.cpp

--- Binary Search/1060 Missing Element in Sorted Array.cpp
+++ Binary Search/1060 Missing Element in Sorted Array.cpp
@@ -1,9 +1,11 @@
-int KthMissingElement(int a[], int n, int k)
-{
+//https://www.codingninjas.com/codestudio/problems/kth-missing-element_893215?leftPanelTab=0
+//https://leetcode.ca/all/1060.html
+int missingK(vector < int > a, int n, int k) {
+    
     int lo = 0, hi = n - 1;
     int ans = -1;
+    int start = n - 1;
     
-    int start = -1;
     while(lo <= hi){
         
         int mid = (lo + hi)/2;
@@ -21,8 +23,5 @@ int KthMissingElement(int a[], int n, int k)
             lo = mid + 1;
         }
     } 
-    if(start == -1){
-        return -1;
-    }
     return a[start] + k - ans;
 }
