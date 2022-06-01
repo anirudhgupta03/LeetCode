1d4cb3babad0e3aa4d3ea97d277b51718768fe9c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-01 18:57:20 +0530

    Update 1802. Maximum Value at a Given Index in a Bounded Array.cpp

--- Binary Search/1802. Maximum Value at a Given Index in a Bounded Array.cpp
+++ Binary Search/1802. Maximum Value at a Given Index in a Bounded Array.cpp
@@ -1,5 +1,6 @@
 //Ref1: https://www.youtube.com/watch?v=MjpAAE3-hYg
 //Ref2: https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/discuss/1119755/Intuition-explained-or-Binary-search-or-Java-or-Heavily-commented-or-Easy-to-understand
+//Method - 1
 class Solution {
 public:
     #define ll long long
@@ -53,5 +54,53 @@ public:
         }
         
         return res;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    #define ll long long
+    bool isValid(ll maxValue, ll n, ll index, ll maxSum){
+        
+        ll lsum = 0, rsum = 0;
+        
+        if(maxValue - 1 <= index){
+            lsum += (maxValue*(maxValue + 1))/2;
+            lsum += index + 1 - maxValue;
+        }
+        else{
+            ll temp = maxValue - index - 1;
+            lsum += (maxValue*(maxValue + 1))/2 - (temp*(temp + 1))/2;
+        }
+        
+        if(n - index >= maxValue){
+            rsum += (maxValue*(maxValue + 1))/2;
+            rsum += n - index - maxValue;
+        }
+        else{
+            ll temp = maxValue - (n - index);
+            rsum += (maxValue*(maxValue + 1))/2 - (temp*(temp + 1))/2;
+        }
+        
+        return lsum + rsum - maxValue <= maxSum;
+    }
+    int maxValue(int n, int index, int maxSum) {
+        
+        ll lo = 1, hi = maxSum, ans;
+        
+        while(lo <= hi){
+            
+            ll mid = lo + (hi - lo)/2;
+      
+            if(isValid(mid, n, index, maxSum)){
+                ans = mid;
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
+        }
+        return ans;
     }
 };
