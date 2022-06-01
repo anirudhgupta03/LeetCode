cfb873e6fafcdec20e0db54db782001fa36add94
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-01 16:18:02 +0530

    Update 1658. Minimum Operations to Reduce X to Zero.cpp

--- Binary Search/1658. Minimum Operations to Reduce X to Zero.cpp
+++ Binary Search/1658. Minimum Operations to Reduce X to Zero.cpp
@@ -1,4 +1,49 @@
 //Ref: https://www.youtube.com/watch?v=7nzwrX4N_A0&t=40s
+//Method - 1
+//TC - O(N)
+//SC - O(N)
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        
+        int n = nums.size();
+        
+        int sum = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+        }
+        
+        sum -= x;
+        
+        if(sum == 0) return n;
+        
+        unordered_map<int,int> umap;
+        umap[0] = -1;
+        
+        int maxLenSubarray = -1;
+        
+        int tsum = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            tsum += nums[i];
+            
+            if(umap.find(tsum - sum) != umap.end()){
+                maxLenSubarray = max(maxLenSubarray, i - umap[tsum - sum]);
+            }
+           
+            if(umap.find(tsum) == umap.end()){
+                umap[tsum] = i;
+            }
+        }
+        if(maxLenSubarray == -1) return -1;
+        return n - maxLenSubarray;
+    }
+};
+
+//Method - 2
+//TC - O(N)
+//SC - O(1)
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
