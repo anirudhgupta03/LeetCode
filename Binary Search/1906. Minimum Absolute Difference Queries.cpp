b3a553aba8cd55ebfbb56574fb0e836c551397f1
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-01 22:19:58 +0530

    Update 1906. Minimum Absolute Difference Queries.cpp

--- Binary Search/1906. Minimum Absolute Difference Queries.cpp
+++ Binary Search/1906. Minimum Absolute Difference Queries.cpp
@@ -1,4 +1,5 @@
 //Ref: https://leetcode.com/problems/minimum-absolute-difference-queries/discuss/1284358/C%2B%2B-Binary-search
+//Method - 1
 class Solution {
 public:
     vector<int> minDifference(vector<int>& nums, vector<vector<int>>& queries) {
@@ -41,5 +42,46 @@ public:
             ans[i] = t;
         }
         return ans;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    vector<int> minDifference(vector<int>& nums, vector<vector<int>>& queries) {
+        
+        vector<int> res;
+        
+        int n = nums.size(), maxval = *max_element(nums.begin(), nums.end());
+        
+        vector<vector<int>> prefix(n + 1, vector<int>(maxval + 1, 0));
+        
+        for(int i = 1; i <= n; i++){
+            prefix[i] = prefix[i - 1];
+            prefix[i][nums[i - 1]] = 1 + prefix[i - 1][nums[i - 1]];
+        }
+        
+        for(int i = 0; i < queries.size(); i++){
+            
+            vector<int> v(maxval + 1);
+            
+            for(int j = 0; j <= maxval; j++){
+                v[j] = prefix[queries[i][1] + 1][j] - prefix[queries[i][0]][j];
+            }
+            
+            int minDiff = INT_MAX, pre = -1;
+            
+            for(int j = 1; j <= maxval; j++){
+                if(v[j]){
+                    if(pre != -1){
+                        minDiff = min(minDiff, j - pre);
+                    }
+                    pre = j;
+                }
+            }
+            if(minDiff == INT_MAX) res.push_back(-1);
+            else res.push_back(minDiff);
+        }
+        return res;
     }
 };
