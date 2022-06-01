919d4ed75addf42c116c981e7f368f3d49045a65
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-01 18:02:24 +0530

    Update 1760. Minimum Limit of Balls in a Bag.cpp

--- Binary Search/1760. Minimum Limit of Balls in a Bag.cpp
+++ Binary Search/1760. Minimum Limit of Balls in a Bag.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     bool isPossible(vector<int>& nums, int maxOperations, int penalty){
@@ -30,6 +31,41 @@ public:
             int mid = (lo + hi)/2;
             
             if(isPossible(nums, maxOperations, mid)){
+                ans = mid;
+                hi = mid - 1;
+            }
+            else{
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    bool isValid(int balls, vector<int> &nums, int maxOperations){
+        
+        int op = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            op += nums[i]/balls;
+            if(nums[i] % balls == 0) op--;
+        }
+        if(op <= maxOperations) return true;
+        return false;
+    }
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        
+        int lo = 1, hi = *max_element(nums.begin(), nums.end());
+        int ans;
+        
+        while(lo <= hi){
+            
+            int mid = lo + (hi - lo)/2;
+            
+            if(isValid(mid, nums, maxOperations)){
                 ans = mid;
                 hi = mid - 1;
             }
