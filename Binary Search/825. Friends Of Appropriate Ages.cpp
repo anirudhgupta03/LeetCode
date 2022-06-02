7e31a2fbab0ce237424ed9311e90598bf71e76cb
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-02 13:14:28 +0530

    Update 825. Friends Of Appropriate Ages.cpp

--- Binary Search/825. Friends Of Appropriate Ages.cpp
+++ Binary Search/825. Friends Of Appropriate Ages.cpp
@@ -1,3 +1,59 @@
+//Method - 1
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        
+        sort(ages.begin(), ages.end(), greater<int>());
+        
+        int count = 0;
+        
+        for(int i = 0; i < ages.size(); i++){
+            
+            int lo = i + 1, hi = ages.size() - 1, ans = i;
+            
+            while(lo <= hi){
+                
+                int mid = (lo + hi)/2;
+                
+                if(0.5*ages[i] + 7 < ages[mid]){
+                    ans = mid;
+                    lo = mid + 1;
+                }
+                else{
+                    hi = mid - 1;
+                }
+            }
+            
+            count += ans - i;
+        }
+        
+        sort(ages.begin(), ages.end());
+        
+        for(int i = 0; i< ages.size(); i++){
+            
+            int lo = i + 1, hi = ages.size() - 1, ans = i;
+            
+            while(lo <= hi){
+                
+                int mid = (lo + hi)/2;
+                
+                if(ages[mid] > ages[i]){
+                    hi = mid - 1;
+                }
+                else{
+                    ans = mid;
+                    lo = mid + 1;
+                }
+            }
+            if(0.5*ages[i] + 7 < ages[ans]){
+                count += ans - i;
+            }
+        }
+        return count;
+    }
+};
+
+//Method - 2
 class Solution {
 public:
     int numFriendRequests(vector<int>& ages) {
