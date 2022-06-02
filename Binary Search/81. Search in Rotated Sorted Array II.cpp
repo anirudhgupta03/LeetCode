99c0cc0a8f497c4a08629c4bcbe652bfdd0a1bc1
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-02 11:54:02 +0530

    Update 81. Search in Rotated Sorted Array II.cpp

--- Binary Search/81. Search in Rotated Sorted Array II.cpp
+++ Binary Search/81. Search in Rotated Sorted Array II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
@@ -27,6 +28,44 @@ public:
             else{
                 
                 if(target >= nums[lo] && target <= nums[mid]){
+                    hi = mid - 1;
+                }
+                else{
+                    lo = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        
+        int lo = 0, hi = nums.size() - 1;
+        
+        while(lo <= hi){
+            
+            int mid = lo + (hi - lo)/2;
+            
+            if(nums[mid] == target) return true;
+            
+            if(nums[mid] == nums[lo] && nums[mid] == nums[hi]){
+                lo++;
+                hi--;
+            }
+            else if(nums[mid] <= nums[hi]){
+                if(target > nums[mid] && target <= nums[hi]){
+                    lo = mid + 1;
+                }
+                else{
+                    hi = mid - 1;
+                }
+            }
+            else{
+                if(target < nums[mid] && target >= nums[lo]){
                     hi = mid - 1;
                 }
                 else{
