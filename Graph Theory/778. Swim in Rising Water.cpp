a925010b35034c5c33a4940413eed623d448b090
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-03 10:29:04 +0530

    Update 778. Swim in Rising Water.cpp

--- Graph Theory/778. Swim in Rising Water.cpp
+++ Graph Theory/778. Swim in Rising Water.cpp
@@ -45,3 +45,47 @@ public:
         return -1;
     }
 };
+
+//Method - 2
+class Solution {
+public:
+    #define pipii pair<int,pair<int,int>>
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    struct cmp{
+      bool operator()(pipii &p1, pipii &p2){
+          return p1.first > p2.first;
+      }  
+    };
+    int swimInWater(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        
+        priority_queue<pipii, vector<pipii>, cmp> pq;
+        
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        
+        pq.push({grid[0][0],{0,0}});
+        
+        while(!pq.empty()){
+            
+            int maxValueTillNow = pq.top().first, x = pq.top().second.first, y = pq.top().second.second;
+            pq.pop();
+            
+            if(x == n - 1 && y == n - 1){
+                return maxValueTillNow;
+            }
+            if(vis[x][y]) continue;
+            vis[x][y] = 1;
+            
+            for(int i = 0; i < 4; i++){
+                int xo = x + dx[i], yo = y + dy[i];
+                
+                if(xo >= 0 && yo >= 0 && xo < n && yo < n && vis[xo][yo] == 0){
+                    pq.push({max(maxValueTillNow, grid[xo][yo]), {xo, yo}});
+                }
+            }
+        }
+        return 0;
+    }
+};
