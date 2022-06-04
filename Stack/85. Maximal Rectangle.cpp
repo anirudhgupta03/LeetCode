c8b67e516e79fa1776110342ebaa30f2021582a8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-04 11:47:50 +0530

    Update 85. Maximal Rectangle.cpp

--- Stack/85. Maximal Rectangle.cpp
+++ Stack/85. Maximal Rectangle.cpp
@@ -1,4 +1,5 @@
 //Using Maximum Area Histogram
+//Method - 1
 class Solution {
 public:
     int mah(vector<int> height, int n){
@@ -110,5 +111,95 @@ public:
         }
         
         return maxarea;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int findMaxArea(vector<int> &v){
+        
+        int n = v.size();
+        
+        vector<int> nsl(n), nsr(n);
+        
+        stack<int> st;
+        
+        nsl[0] = -1;
+        st.push(0);
+        
+        for(int i = 1; i < n; i++){
+            if(v[i] > v[st.top()]){
+                nsl[i] = st.top();
+            }
+            else{
+                while(!st.empty() && v[st.top()] >= v[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    nsl[i] = -1;
+                }
+                else{
+                    nsl[i] = st.top();
+                }
+            }
+            st.push(i);
+        }
+        
+        while(!st.empty()){
+            st.pop();
+        }
+        
+        st.push(n - 1);
+        nsr[n - 1] = n;
+        
+        for(int i = n - 2; i >= 0; i--){
+            if(v[i] > v[st.top()]){
+                nsr[i] = st.top();
+            }
+            else{
+                while(!st.empty() && v[st.top()] >= v[i]){
+                    st.pop();
+                }
+                if(st.empty()){
+                    nsr[i] = n;
+                }
+                else{
+                    nsr[i] = st.top();
+                }
+            }
+            st.push(i);
+        }
+        
+        int maxArea = 0;
+        
+        for(int i = 0; i < n; i++){
+            maxArea = max(maxArea, (nsr[i] - nsl[i] - 1)*v[i]);
+        }
+        return maxArea;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        
+        int m = matrix.size(), n = matrix[0].size();
+        
+        vector<vector<int>> heights(m, vector<int>(n, 0));
+        
+        for(int j = 0; j < n; j++){
+            heights[0][j] = matrix[0][j] == '1' ? 1 : 0;
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 0; j < n; j++){
+                heights[i][j] = matrix[i][j] == '0' ? 0 : 1 + heights[i - 1][j];
+            }
+        }
+        
+        int maxArea = 0;
+        
+        for(int i = 0; i < m; i++){
+            maxArea = max(maxArea, findMaxArea(heights[i]));
+        }
+        
+        return maxArea;
     }
 };
