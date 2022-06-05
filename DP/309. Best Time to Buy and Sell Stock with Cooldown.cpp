8e3a73d821eb3efd704cb78caea770c289dbbf75
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-05 18:06:47 +0530

    Update 309. Best Time to Buy and Sell Stock with Cooldown.cpp

--- DP/309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ DP/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -39,6 +39,46 @@ public:
 };
 
 //Method - 2
+//Optimised Top Down
+class Solution {
+public:
+    int solve(int day, int buy, vector<vector<int>> &dp, vector<int> &prices){
+        
+        if(day >= prices.size()){
+            return 0;
+        }
+        
+        if(dp[day][buy] != -1){
+            return dp[day][buy];
+        }
+        
+        int maxProfit = 0;
+        
+        if(buy == 0){
+            for(int i = day; i < prices.size(); i++){
+                maxProfit = max(maxProfit, solve(i, 1, dp, prices));
+            }
+        }
+        else{
+            for(int i = day + 1; i < prices.size(); i++){
+                maxProfit = max(maxProfit, solve(i + 2, 0, dp, prices) + prices[i] - prices[day]);
+            }
+        }
+        return dp[day][buy] = maxProfit;
+    }
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+        
+        if(n == 1) return 0;
+        
+        vector<vector<int>> dp(n, vector<int>(2,-1));
+        
+        return solve(0, 0, dp, prices);
+    }
+};
+
+//Method - 3
 //Bottom Up
 //Ref: https://www.youtube.com/watch?v=GY0O57llkKQ&t=13s
 class Solution {
@@ -81,5 +121,26 @@ public:
             ocsp = ncsp;
         }
         return ossp;
+    }
+}; 
+
+//Method - 4
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = p rices.size();
+        int dp[n][3];
+        
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        dp[0][2] = 0;
+        
+        for(int i = 1; i < n; i++){
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i]);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1]);
+        }
+        return dp[n - 1][1];
     }
 };
