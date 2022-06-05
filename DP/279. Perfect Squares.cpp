ca9502abf3d14f15a7a5d32325b3f840871eead4
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-05 15:58:15 +0530

    Update 279. Perfect Squares.cpp

--- DP/279. Perfect Squares.cpp
+++ DP/279. Perfect Squares.cpp
@@ -1,3 +1,5 @@
+//Method - 1
+//Bottom Up
 //Ref: https://www.youtube.com/watch?v=aZuQXkO0-XA&t=5s
 class Solution {
 public:
@@ -17,5 +19,31 @@ public:
         }
         
         return dp[n];
+    }
+};
+
+//Method - 2
+//Top Down
+class Solution {
+public:
+    int solve(int n, vector<int> &dp){
+        
+        if(n == 0 || n == 1){
+            return n;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int count = INT_MAX;
+        
+        for(int i = 1; i <= sqrt(n); i++){
+            count = min(count, 1 + solve(n - i*i, dp));
+        }
+        return dp[n] = count;
+    }
+    int numSquares(int n) {
+        
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
     }
 };
