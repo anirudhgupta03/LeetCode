a5a552d7c8622a24c1412e4a23d7f615d2f5b731
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-05 13:41:27 +0530

    Update 221. Maximal Square.cpp

--- DP/221. Maximal Square.cpp
+++ DP/221. Maximal Square.cpp
@@ -2,31 +2,24 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         
-        int maxLen = 0;
         int m = matrix.size(), n = matrix[0].size();
         
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<vector<int>> dp(m, vector<int>(n));
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        int side = 0;
+        
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
                 
-                if(j == n - 1 || i == m - 1){
-                    if(matrix[i][j] == '1'){
-                        dp[i][j] = 1;
-                        maxLen = 1;
-                    }
+                if(i == m - 1 || j == n - 1){
+                    dp[i][j] = matrix[i][j] == '0' ? 0 : 1;
                 }
+                else if(matrix[i][j] == '1'){
+                    dp[i][j] = min({dp[i + 1][j + 1], dp[i + 1][j], dp[i][j + 1]}) + 1;
+                }
+                side = max(side, dp[i][j]);
             }
         }
-        
-        for(int i = m - 2; i >= 0; i--){
-            for(int j = n - 2; j >= 0; j--){
-                if(matrix[i][j] == '1'){
-                    dp[i][j] = min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]}) + 1;
-                }
-                maxLen = max(maxLen,dp[i][j]);
-            }
-        }
-        return maxLen*maxLen;
+        return side*side;
     }
 };
