7eaf2771a2c61e59bda1664b4a108b300e0e7ad7
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-05 11:34:23 +0530

    Update 198. House Robber.cpp

--- DP/198. House Robber.cpp
+++ DP/198. House Robber.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -28,5 +29,48 @@ public:
             }
         }
         return max(dp[n-1],dp[n-2]);
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n == 1) return nums[0];
+        
+        vector<int> dp(n);
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        
+        for(int i = 2; i < n; i++){
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        
+        return dp[n - 1];
+    }
+};
+
+//Method - 3
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        if(n == 1) return nums[0];
+        
+        int val1 = nums[0];
+        int val2 = max(nums[0], nums[1]);
+        
+        for(int i = 2; i < n; i++){
+            int val3 = max(val2, val1 + nums[i]);
+            val1 = val2;
+            val2 = val3;
+        }
+        return val2;
     }
 };
