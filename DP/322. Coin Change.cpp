4429f9da08825a4a2dfa045dfa5b9c712ab321b6
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-05 23:32:58 +0530

    Update 322. Coin Change.cpp

--- DP/322. Coin Change.cpp
+++ DP/322. Coin Change.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
@@ -28,5 +29,38 @@ public:
             return -1;
         }
         return dp[n][amount];
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        
+        int n = coins.size();
+        
+        int dp[n + 1][amount + 1];
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= amount; j++){
+                
+                if(j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(i == 0){
+                    dp[i][j] = INT_MAX;
+                }
+                else if(coins[i - 1] <= j){
+                    dp[i][j] = dp[i - 1][j];
+                    if(dp[i][j - coins[i - 1]] != INT_MAX){
+                        dp[i][j] = min(dp[i][j], dp[i][j - coins[i - 1]] + 1);
+                    }
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount] == INT_MAX ? -1 : dp[n][amount];
     }
 };
