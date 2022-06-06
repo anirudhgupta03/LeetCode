d3870904e57b5099e580f650ae2696e1f52ade9f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-06 15:53:38 +0530

    Update 403. Frog Jump.cpp

--- DP/403. Frog Jump.cpp
+++ DP/403. Frog Jump.cpp
@@ -1,26 +1,20 @@
 //Top-Down Approach
 class Solution {
 public:
-    bool solve(int ind, int k, vector<int> &stones, map<pair<int,int>,int> &dp){
+    map<pair<int,int>,bool> dp;
+    bool solve(int ind, int k, vector<int> &stones){
         
         if(ind == stones.size() - 1){
             return true;
         }
-        
-        if(dp.find({ind,k}) != dp.end()){
+        if(dp.find({ind, k}) != dp.end()){
             return dp[{ind,k}];
         }
         for(int i = ind + 1; i < stones.size(); i++){
-            if(stones[i] - stones[ind] == k - 1){
-                bool flag = solve(i, k - 1, stones, dp);
-                if(flag) return dp[{ind,k}] = true;
-            }
-            else if(stones[i] - stones[ind] == k){
-                bool flag = solve(i, k, stones, dp);
-                if(flag) return dp[{ind,k}] = true;
-            }
-            else if(stones[i] - stones[ind] == k + 1){
-                bool flag = solve(i, k + 1, stones, dp);
+            
+            int temp = stones[i] - stones[ind];
+            if(temp == k || temp == k - 1 || temp == k + 1){
+                bool flag = solve(i, temp, stones);
                 if(flag) return dp[{ind,k}] = true;
             }
         }
@@ -31,7 +25,7 @@ public:
         if(stones[1] > 1){
             return false;
         }
-        map<pair<int,int>,int> dp;
-        return solve(1, 1, stones, dp);
+        dp.clear();
+        return solve(1, 1, stones);
     }
 };
