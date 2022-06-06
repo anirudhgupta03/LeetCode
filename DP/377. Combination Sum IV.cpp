5d1d46eefc24b635afbb69bf66c9a6a8a6afb4dc
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-06 20:04:40 +0530

    Update 377. Combination Sum IV.cpp

--- DP/377. Combination Sum IV.cpp
+++ DP/377. Combination Sum IV.cpp
@@ -1,4 +1,24 @@
 //Ref: https://www.youtube.com/watch?v=GWqe_xfqxCA
+//Bottom Up
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        vector<long double> dp(target + 1, 0);
+        dp[0] = 1;
+        
+        for(int i = 1; i <= target; i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[j] <= i){
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+};
+
+//Top Down
 class Solution {
 public:
     int solve(int sum, vector<int> &nums, int target, vector<int> &dp){
