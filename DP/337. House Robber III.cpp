e220d2231942a787342a29004ef65be244cbc79d
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-06 09:41:00 +0530

    Update 337. House Robber III.cpp

--- DP/337. House Robber III.cpp
+++ DP/337. House Robber III.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 //Ref: https://www.youtube.com/watch?v=kTL5LhCTL1c
 /**
  * Definition for a binary tree node.
@@ -33,5 +34,47 @@ public:
         
           vector<int> ans = houseRobber(root);
           return max(ans[0],ans[1]);
+    }
+};
+
+//Method - 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    #define pii pair<int,int>
+    pii solve(TreeNode* root){
+        
+        if(root == NULL){
+            return {0,0};
+        }
+        if(root -> left == NULL && root -> right == NULL){
+            return {root -> val, 0};
+        }
+        
+        pii lt = solve(root -> left);
+        pii rt = solve(root -> right);
+        
+        int inc = root -> val + lt.second + rt.second;
+        int exc = lt.first + rt.first;
+        
+        return {max(inc, exc), exc};
+    }
+    int rob(TreeNode* root) {
+        
+        if(root == NULL) return 0;
+        
+        if(root -> left == NULL && root -> right == NULL) return root -> val;
+        
+        return solve(root).first;
     }
 };
