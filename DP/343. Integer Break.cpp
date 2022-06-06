b3a048a0d015ebc456b8a4774fdf7dc4d24dfcce
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-06 10:33:29 +0530

    Create 343. Integer Break.cpp

---
+++ DP/343. Integer Break.cpp
@@ -0,0 +1,57 @@
+//Method - 1
+class Solution {
+public:
+    int solve(int count, int n, vector<int> &dp){
+        
+        if(n == 0){
+            if(count >= 2) return 1;
+            return 0;
+        }
+        
+        if(dp[count]!= -1){
+            return dp[count];
+        }
+        int maxProd = 1;
+        
+        for(int i = 1; i <= n; i++){
+            maxProd = max(maxProd, i* solve(count + 1, n - i, dp));
+        }
+        return dp[count] = maxProd;
+    }
+    int integerBreak(int n) {
+        
+        int prod = 1, maxProd = 1;
+        
+        vector<int> dp(n + 1, -1);
+        
+        return solve(0, n,  dp);
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int integerBreak(int n) {
+        
+        if(n <= 2){
+            return 1;
+        }
+        
+        if(n == 3){
+            return 2;
+        }
+        
+        int p1 = n/3;
+        
+        if(n % 3 == 1){
+            p1--;
+        }
+        
+        int p2 = (n - p1*3)/2;
+        
+        int t1 = pow(3,p1);
+        int t2 = pow(2,p2);
+        
+        return t1*t2;
+    }
+};
