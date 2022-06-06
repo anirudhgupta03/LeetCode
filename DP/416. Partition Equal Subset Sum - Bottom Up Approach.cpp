913b2f1acc88f4d2447300fe87f73392a2568c35
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-06 16:57:25 +0530

    Update 416. Partition Equal Subset Sum - Bottom Up Approach.cpp

--- DP/416. Partition Equal Subset Sum - Bottom Up Approach.cpp
+++ DP/416. Partition Equal Subset Sum - Bottom Up Approach.cpp
@@ -1,42 +1,40 @@
+//TC: O(n*sum)
+//SC: O(n*sum)
 class Solution {
 public:
-    
     bool canPartition(vector<int>& nums) {
         
         int sum = 0;
-        for(auto x : nums){
+        
+        for(int x: nums){
             sum += x;
         }
         
         if(sum % 2 != 0){
             return false;
         }
-        else{
-            
-            vector<vector<bool> > dp(nums. size() + 1,vector<bool>(sum/2 +1));
-            
-            for(int j = 0; j < sum/2 + 1; j++){
-                dp[0][j] = false;
+        sum /= 2;
+        int n = nums.size();
+        
+        bool dp[n + 1][sum + 1];
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= sum; j++){
+                
+                if(j == 0){
+                    dp[i][j] = true;
+                }
+                else if(i == 0){
+                    dp[i][j] = false;
+                }
+                else if(nums[i - 1] <= j){
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
-            for(int i = 0; i < nums.size() + 1; i++){
-                dp[i][0] = true;
-            }
-            
-            for(int i = 1; i < nums.size() + 1; i++){
-                 for(int j = 1; j < sum/2 + 1; j++){
-
-                    if(nums[i-1] <= j){
-                        dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                    }
-                    else{
-                        dp[i][j] = dp[i-1][j];
-                    }
-                 }
-           }
-            
-            return dp[nums.size()][sum/2];
         }
+        return dp[n][sum];
     }
 };
-//Time Complexity: O(n*sum)
-//Space Complexity: O(n*sum)
