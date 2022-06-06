02267b327bc3b806334db90955b681f3504769d2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-06 13:26:26 +0530

    Update 354. Russian Doll Envelopes.cpp

--- DP/354. Russian Doll Envelopes.cpp
+++ DP/354. Russian Doll Envelopes.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//TLE
+//LIS
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
@@ -29,5 +32,47 @@ public:
             }
         }
         return max;
+    }
+};
+
+//Method - 2
+//Ref: https://www.youtube.com/watch?v=J0WQb_SffqY
+//Optimal Approach
+class Solution {
+public:
+    bool static cmp(vector<int> &v1, vector<int> &v2){
+        if(v1[0] == v2[0]){
+            return v1[1] > v2[1];
+        }
+        return v1[0] < v2[0];
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        
+        int n = envelopes.size();
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        sort(envelopes.begin(),envelopes.end(), cmp);
+        
+        vector<int> v;
+        v.push_back(envelopes[0][1]);
+        
+        int length = 1;
+        
+        for(int i = 1; i < envelopes.size(); i++){
+           
+            auto itr = lower_bound(v.begin(), v.end(), envelopes[i][1]);
+            
+            if(itr == v.end()){
+                length++;
+                v.push_back(envelopes[i][1]);
+            }
+            else{
+                *itr = envelopes[i][1];
+            }
+        }
+        return length;
     }
 };
