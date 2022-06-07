c825aa8d2f2da0c75872b82193d025a67dc195ed
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-07 18:37:58 +0530

    Update 650. 2 Keys Keyboard.cpp

--- DP/650. 2 Keys Keyboard.cpp
+++ DP/650. 2 Keys Keyboard.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     void solve(bool flag, int count, int copied, int steps, int &minsteps, int n){
@@ -29,7 +30,36 @@ public:
     }
 };
 
-//Alternate Approach
+//Method - 2
+class Solution {
+public:
+    int minSteps(int n) {
+        
+       int dp[n + 1];
+        
+        dp[1] = 0;
+        
+        for(int i = 2; i <= n; i++){
+            if(i % 2 == 0){
+                dp[i] = dp[i/2] + 2;
+            }
+            else{
+                int ans = i;
+                for(int j = 3; j <= sqrt(i); j += 2){
+                    if(i % j == 0){
+                        ans = min(ans, dp[j] + i/j);
+                        ans = min(ans, dp[i/j] + j);
+                    }
+                }
+                dp[i] = ans;
+            }
+        }
+        return dp[n];
+    }
+};
+
+//Method - 3
+//Optimal Approach
 //Ref: https://www.youtube.com/watch?v=anHoebBokmg
 class Solution {
 public:
