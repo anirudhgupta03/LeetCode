ffe63100c57309293cd5aa4ebb2130e47225f7a3
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-07 14:24:42 +0530

    Update 583. Delete Operation for Two Strings.cpp

--- DP/583. Delete Operation for Two Strings.cpp
+++ DP/583. Delete Operation for Two Strings.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     int minDistance(string word1, string word2) {
@@ -24,5 +25,32 @@ public:
             }
         }
         return dp[len1][len2];
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        
+        int len1 = word1.size(), len2 = word2.size();
+        
+        int dp[len1 + 1][len2 + 1];
+        
+        for(int i = 0; i <= len1; i++){
+            for(int j = 0; j <= len2; j++){
+                
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return len1 + len2 - 2*dp[len1][len2];
     }
 };
