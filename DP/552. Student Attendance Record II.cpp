7309e52c336d2105a490a21d77e18e57bcdea50e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-07 13:47:12 +0530

    Update 552. Student Attendance Record II.cpp

--- DP/552. Student Attendance Record II.cpp
+++ DP/552. Student Attendance Record II.cpp
@@ -1,4 +1,5 @@
 //Nice Question
+//Method - 1
 class Solution {
 public:
     #define ll long long
@@ -40,5 +41,41 @@ public:
             sum = (sum + dp[i][n])%mod;
         }
         return sum%mod;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    #define ll long long
+    #define mod 1000000007
+    int checkRecord(int n) {
+        
+        ll dp[n + 1][6];
+        
+        dp[1][0] = dp[1][1] = dp[1][5] = 1;
+        dp[1][2] = dp[1][3] = dp[1][4] = 0;
+        
+        ll pre = 3;
+        
+        for(int i = 2; i <= n; i++){
+            
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][3] + dp[i - 1][4] + dp[i - 1][5]) % mod;
+            
+            dp[i][1] = dp[i - 1][5] % mod;
+                
+            dp[i][2] = dp[i - 1][0] % mod;
+                
+            dp[i][3] = dp[i - 1][1] % mod;
+                
+            dp[i][4] = dp[i - 1][2] % mod;
+                
+            dp[i][5] = (dp[i - 1][1] + dp[i - 1][3] + dp[i - 1][5]) % mod;   
+        }
+        ll ans = 0;
+        for(ll j = 0; j < 6; j++){
+            ans = (ans + dp[n][j]) % mod;
+        }
+        return ans;
     }
 };
