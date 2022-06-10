94e4b9a766f31b32c7ea161ba89469767ab4ece5
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-10 16:03:01 +0530

    Update 894. All Possible Full Binary Trees.cpp

--- Trees/894. All Possible Full Binary Trees.cpp
+++ Trees/894. All Possible Full Binary Trees.cpp
@@ -12,30 +12,28 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> solve(int n){
+    vector<TreeNode*> allPossibleFBT(int n) {
         
         if(n == 1){
-            vector<TreeNode*> temp;
-            temp.push_back(new TreeNode(0));
-            return temp;
+            TreeNode* root = new TreeNode(0);
+            return {root};
         }
         
-        vector<TreeNode*> v;
+        vector<TreeNode*> res;
+        
         for(int i = 1; i < n; i += 2){
-            vector<TreeNode*> l = solve(i);
-            vector<TreeNode*> r = solve(n - i - 1);
+            vector<TreeNode*> l = allPossibleFBT(i);
+            vector<TreeNode*> r = allPossibleFBT(n - i - 1);
+            
             for(auto x: l){
                 for(auto y: r){
-                   TreeNode* curr = new TreeNode(0);
-                    curr -> left = x;
-                    curr -> right = y;
-                    v.push_back(curr);
+                    TreeNode* root = new TreeNode(0);
+                    root -> left = x;
+                    root -> right = y;
+                    res.push_back(root);
                 }
             }
         }
-        return v;
-    }
-    vector<TreeNode*> allPossibleFBT(int n) {
-        return solve(n);
+        return res;
     }
 };
