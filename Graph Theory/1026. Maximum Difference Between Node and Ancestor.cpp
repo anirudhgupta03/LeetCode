b216eb0c57ca3eba177f202daa6b09266efbd231
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-10 22:02:13 +0530

    Update 1026. Maximum Difference Between Node and Ancestor.cpp

--- Graph Theory/1026. Maximum Difference Between Node and Ancestor.cpp
+++ Graph Theory/1026. Maximum Difference Between Node and Ancestor.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -34,6 +35,47 @@ public:
         
         solve(root, maxDiff, INT_MAX, INT_MIN);
         
+        return maxDiff;
+    }
+};
+
+//Method - 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDiff;
+    pair<int,int> solve(TreeNode* root){
+        
+        if(root == NULL){
+            return {INT_MAX, INT_MIN};
+        }
+        
+        if(root -> left == NULL && root -> right == NULL){
+            return {root -> val, root -> val};
+        }
+        pair<int,int> l = solve(root -> left);
+        pair<int,int> r = solve(root -> right);
+        
+        int minVal = min(l.first, r.first);
+        int maxVal = max(l.second, r.second);
+        
+        maxDiff = max({maxDiff, abs(root -> val - minVal), abs(root -> val - maxVal)});
+        
+        return {min(minVal, root -> val), max(maxVal, root -> val)};
+    }
+    int maxAncestorDiff(TreeNode* root) {
+        maxDiff = INT_MIN;
+        solve(root);
         return maxDiff;
     }
 };
