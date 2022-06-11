511364a95c30615e67d47c3821bed6c2e5dd0a09
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-11 11:09:47 +0530

    Update 1101. The Earliest Moment When Everyone Become Friends.cpp

--- Graph Theory/1101. The Earliest Moment When Everyone Become Friends.cpp
+++ Graph Theory/1101. The Earliest Moment When Everyone Become Friends.cpp
@@ -1,5 +1,6 @@
 //https://www.codingninjas.com/codestudio/problems/the-earliest-moment-when-everyone-become-friends_1376604?leftPanelTab=0
 //Ref: https://www.youtube.com/watch?v=xW2hjWl19is
+//Method - 1
 #include<bits/stdc++.h>
 using namespace std;
 bool cmp(vector<int> &v1, vector<int> &v2){
@@ -39,4 +40,48 @@ int minTime(vector<vector<int>> &logs, int n)
         }
     }
     return -1;
+}
+
+//Method - 2
+#include<bits/stdc++.h>
+using namespace std;
+bool cmp(vector<int> &v1, vector<int> &v2){
+    return v1[0] < v2[0];
+}
+void unionP(int node1, int node2, vector<int> &par){
+    par[node2] = node1;
+}
+int findP(int node, vector<int> &par){
+    if(par[node] == -1){
+        return node;
+    }
+    return par[node] = findP(par[node], par);
+}
+int minTime(vector<vector<int>> &logs, int n)
+{
+	sort(logs.begin(), logs.end(), cmp);
+    
+    int ans = 0;
+    
+    vector<int> par(n, -1);
+    
+    for(int i = 0; i < logs.size(); i++){
+        int t = logs[i][0], u = logs[i][1], v = logs[i][2];
+        
+        int paru = findP(u, par);
+        int parv = findP(v, par);
+
+        if(paru != parv){
+            ans = t;
+            unionP(paru, parv, par);
+        }
+    }
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(par[i] == -1){
+            count++;
+        }
+    }
+    if(count > 1) return -1;
+    return ans;
 }
