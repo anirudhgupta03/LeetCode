3ab93a32f4b4fa3888187c5e7550a6ea6568d7d9
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-11 15:12:07 +0530

    Update 1202. Smallest String With Swaps.cpp

--- Graph Theory/1202. Smallest String With Swaps.cpp
+++ Graph Theory/1202. Smallest String With Swaps.cpp
@@ -1,4 +1,5 @@
 //Ref: https://www.youtube.com/watch?v=po4XuLCT9EA
+//Method - 1
 class Solution {
 public:
     int findp(int node, vector<int> &par){
@@ -56,5 +57,54 @@ public:
             mp[par[i]].pop_back();
         }
         return res;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    void dfs(int node, int parent, vector<int> &par, vector<int> &vis, vector<char> &v, string &s, vector<int> al[]){
+        
+        par[node] = parent;
+        v.push_back(s[node]);
+        vis[node] = 1;
+        
+        for(int i = 0; i < al[node].size(); i++){
+            int child = al[node][i];
+            if(vis[child] == 0){
+                dfs(child, parent, par, vis, v, s, al);
+            }
+        }
+    }
+    string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+        
+        int n = s.size();
+        
+        vector<int> al[n];
+        for(auto &x: pairs){
+            al[x[0]].push_back(x[1]);
+            al[x[1]].push_back(x[0]);
+        }
+        
+        vector<vector<char>> temp(n);
+        vector<int> par(n), vis(n, 0), ind(n, -1);
+        
+        for(int i = 0; i < n; i++){
+            if(vis[i] == 0){
+                vector<char> v;
+                dfs(i, i, par, vis, v, s, al);
+                sort(v.begin(), v.end());
+                temp[i] = v;
+                ind[i] = 0;
+            }
+        }
+        
+        string ans;
+        for(int i = 0; i < n; i++){
+            int parent = par[i];
+            ans.push_back(temp[parent][ind[parent]]);
+            ind[par[i]]++;
+        }
+        return ans;
     }
 };
