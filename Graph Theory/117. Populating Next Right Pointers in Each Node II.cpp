361dce836d5a23be7d9243d0e1d1c132909bc0ea
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-11 14:25:41 +0530

    Update 117. Populating Next Right Pointers in Each Node II.cpp

--- Graph Theory/117. Populating Next Right Pointers in Each Node II.cpp
+++ Graph Theory/117. Populating Next Right Pointers in Each Node II.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /*
 // Definition for a Node.
 class Node {
@@ -51,6 +52,56 @@ public:
             curr -> next = NULL;
             if(curr -> left) q.push(curr -> left);
             if(curr -> right) q.push(curr -> right);
+        }
+        return root;
+    }
+};
+
+//Method - 2
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+        if(root == NULL){
+            return NULL;
+        }
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int sz = q.size();
+            Node* pre = NULL;
+            while(sz--){
+                Node* curr = q.front();
+                q.pop();
+                
+                if(pre != NULL){
+                    pre -> next = curr;
+                }
+                pre = curr;
+                if(curr -> left) q.push(curr -> left);
+                if(curr -> right) q.push(curr -> right);
+            }
+            pre -> next = NULL;
         }
         return root;
     }
