c4356247ad7b27e288fcc43ce996c53228cbe41a
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-12 17:50:23 +0530

    Update 1293. Shortest Path in a Grid with Obstacles Elimination.cpp

--- Graph Theory/1293. Shortest Path in a Grid with Obstacles Elimination.cpp
+++ Graph Theory/1293. Shortest Path in a Grid with Obstacles Elimination.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     #define ppiipii pair<pair<int,int>,pair<int,int>>
@@ -49,6 +50,62 @@ public:
                                 }
                             }
                         }
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+//Method - 2
+//Ref: https://www.youtube.com/watch?v=ID9YJXy3OJk
+class Solution {
+public:
+    #define pii pair<int,int>
+    #define ppp pair<pii,pii>
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    struct cmp{
+      bool operator()(ppp &p1, ppp &p2){
+          return p1.first.first > p2.first.first;
+      }  
+    };
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        
+        int m = grid.size(), n = grid[0].size();
+        
+        priority_queue<ppp, vector<ppp>, cmp> pq;
+        
+        pq.push({{0,0},{0,0}});
+        
+        int vis[m][n][k + 1];
+        memset(vis,0,sizeof(vis));
+        
+        while(!pq.empty()){
+            
+            int steps = pq.top().first.first, count = pq.top().first.second;
+            int x = pq.top().second.first, y = pq.top().second.second;
+            
+            pq.pop();
+            
+            if(x == m - 1 && y == n - 1){
+                return steps;
+            }
+            
+            if(vis[x][y][count]) continue;
+            vis[x][y][count] = 1;
+            
+            for(int i = 0; i < 4; i++){
+                int xo = x + dx[i], yo = y + dy[i];
+                
+                if(xo >= 0 && yo >= 0 && xo < m && yo < n){
+                    if(grid[xo][yo] == 0 && vis[xo][yo][count] == 0){
+                        pq.push({{steps + 1, count},{xo, yo}});
+                    }
+                    else if(count + 1 <= k && vis[xo][yo][count + 1] == 0){
+                            pq.push({{steps + 1, count + 1},{xo, yo}});
+                        }
+                    }
                 }
             }
         }
