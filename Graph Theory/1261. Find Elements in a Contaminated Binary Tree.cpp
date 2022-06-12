d7352da315732ce1d853c3a8df41536a542f07c6
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-12 13:09:38 +0530

    Update 1261. Find Elements in a Contaminated Binary Tree.cpp

--- Graph Theory/1261. Find Elements in a Contaminated Binary Tree.cpp
+++ Graph Theory/1261. Find Elements in a Contaminated Binary Tree.cpp
@@ -1,3 +1,5 @@
+//Method - 1
+//Using BFS
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -41,6 +43,47 @@ public:
         if(us.find(target) != us.end()){
             return true;
         }
+        return false;
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
+
+//Method - 2
+//Using DFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class FindElements {
+public:
+    unordered_set<int> us;
+    void solve(int val, TreeNode* root){
+        if(root == NULL){
+            return;
+        }
+        us.insert(val);
+        solve(2*val + 1, root -> left);
+        solve(2*val + 2, root -> right);
+    }
+    FindElements(TreeNode* root) {
+        us.clear();
+        solve(0, root);
+    }
+    
+    bool find(int target) {
+        if(us.find(target) != us.end()) return true;
         return false;
     }
 };
