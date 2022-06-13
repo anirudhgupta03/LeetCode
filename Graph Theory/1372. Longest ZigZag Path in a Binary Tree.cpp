0f1fec3a7b49b2d70078fff7f13c4c3eec163271
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-13 16:59:44 +0530

    Update 1372. Longest ZigZag Path in a Binary Tree.cpp

--- Graph Theory/1372. Longest ZigZag Path in a Binary Tree.cpp
+++ Graph Theory/1372. Longest ZigZag Path in a Binary Tree.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -37,5 +38,47 @@ public:
         int maxLen = 1;
         solve(root, -1, maxLen);
         return maxLen - 1;
+    }
+};
+
+//Method - 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans;
+    pair<int,int> solve(TreeNode* root){
+        if(root == NULL){
+            return {-1,-1};
+        }
+        if(root -> left == NULL && root -> right == NULL){
+            return {0,0};
+        }
+        pair<int,int> l = solve(root -> left);
+        pair<int,int> r = solve(root -> right);
+        
+        int lval = l.second + 1;
+        int rval = r.first + 1;
+        ans = max({ans,lval, rval});
+        
+        return {lval, rval};
+    }
+    int longestZigZag(TreeNode* root) {
+        
+        if(root == NULL){
+            return 0;
+        }
+        ans = 0;
+        solve(root);
+        return ans;
     }
 };
