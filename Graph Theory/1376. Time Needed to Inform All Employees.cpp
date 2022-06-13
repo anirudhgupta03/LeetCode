00ee8d57373f9c932a4504e0b6d5a602c585fbce
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-13 17:35:11 +0530

    Update 1376. Time Needed to Inform All Employees.cpp

--- Graph Theory/1376. Time Needed to Inform All Employees.cpp
+++ Graph Theory/1376. Time Needed to Inform All Employees.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     #define pii pair<int,int>
@@ -40,6 +41,55 @@ public:
                 if(vis[child] == 0 && dist[curr] + al[curr][i].second < dist[child]){
                     dist[child] = dist[curr] + al[curr][i].second;
                     pq.push({child, dist[child]});
+                }
+            }
+        }
+        return minTime;
+    }
+};
+
+//Method - 2
+//dist array not required
+class Solution {
+public:
+    #define pii pair<int,int>
+    struct cmp{
+      bool operator()(pii &p1, pii &p2){
+          return p1.second > p2.second;
+      }  
+    };
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        
+        vector<int> al[n];
+        
+        for(int i = 0; i < n; i++){
+            if(manager[i] == -1) continue;
+            al[manager[i]].push_back(i);
+        }
+        
+        int minTime = INT_MIN;
+        
+        vector<int> vis(n, 0);
+        
+        priority_queue<pii, vector<pii>, cmp> pq;
+        pq.push({headID, 0});
+        
+        int curr, ttime;
+        
+        while(!pq.empty()){
+            
+            curr = pq.top().first, ttime = pq.top().second;
+            pq.pop();
+            
+            if(vis[curr]) continue;
+            vis[curr] = 1;
+            
+            minTime = max(minTime, ttime);
+            
+            for(int i = 0; i < al[curr].size(); i++){
+                int subordinate = al[curr][i];
+                if(vis[subordinate] == 0){
+                    pq.push({subordinate, ttime + informTime[curr]});
                 }
             }
         }
