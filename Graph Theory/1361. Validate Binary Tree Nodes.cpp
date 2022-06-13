cd55e918af169216e39bbc60e13a50b12a60a491
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-13 11:24:33 +0530

    Update 1361. Validate Binary Tree Nodes.cpp

--- Graph Theory/1361. Validate Binary Tree Nodes.cpp
+++ Graph Theory/1361. Validate Binary Tree Nodes.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 //Ref: https://leetcode.com/problems/validate-binary-tree-nodes/discuss/899238/Simple-C%2B%2B-and-Python-solution-with-explanation
 class Solution {
 public:
@@ -53,5 +54,60 @@ public:
         }
         
         return (seen.size()==n);
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    bool dfs(int node, vector<int> al[], vector<int> &vis){
+        
+        vis[node] = 1;
+        
+        for(int i = 0; i < al[node].size(); i++){
+            int child = al[node][i];
+            if(vis[child] == 0){
+                bool flag = dfs(child, al, vis);
+                if(flag) return true;
+            }
+            else if(vis[child] == 1){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        
+        vector<int> al[n], indegree(n, 0);
+        
+        for(int i = 0; i < n; i++){
+            if(leftChild[i] != -1){
+                al[i].push_back(leftChild[i]);
+                indegree[leftChild[i]]++;
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(rightChild[i] != -1){
+                al[i].push_back(rightChild[i]);
+                indegree[rightChild[i]]++;
+            }
+        }
+        
+        vector<int> vis(n, 0);
+        int count = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                count++;
+                bool flag = dfs(i, al, vis);
+                if(flag) return false;
+            }
+        }
+        if(count == 0 || count >= 2) return false;
+        for(int i = 0; i < n; i++){
+            if(vis[i] == 0) return false;
+        }
+        return true;
     }
 };
