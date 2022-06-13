043d4b989732a20912d266e1d44a953920fb4285
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-13 08:55:10 +0530

    Update 1315. Sum of Nodes with Even-Valued Grandparent.cpp

--- Graph Theory/1315. Sum of Nodes with Even-Valued Grandparent.cpp
+++ Graph Theory/1315. Sum of Nodes with Even-Valued Grandparent.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -36,6 +37,49 @@ public:
             if(par[x.second] == NULL) continue;
             if((par[x.second] -> val)%2 == 0){
                 sum += (x.first) -> val;
+            }
+        }
+        return sum;
+    }
+};
+
+//Method - 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumEvenGrandparent(TreeNode* root) {
+        
+        if(root == NULL){
+            return 0;
+        }
+        
+        queue<pair<TreeNode*,pair<TreeNode*,TreeNode*>>> q;
+        
+        q.push({root,{NULL, NULL}});
+        
+        int sum = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                
+                TreeNode* curr = q.front().first, *parent = q.front().second.first, *grandparent = q.front().second.second;
+                q.pop();
+                
+                if(grandparent && (grandparent -> val) % 2 == 0){
+                    sum += curr -> val;
+                }
+                if(curr -> left) q.push({curr -> left,{curr, parent}});
+                if(curr -> right) q.push({curr -> right, {curr, parent}});
             }
         }
         return sum;
