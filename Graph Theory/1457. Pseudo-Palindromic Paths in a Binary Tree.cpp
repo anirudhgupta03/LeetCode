0e5d11598911db3920a521e16470e3e1baf3d374
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-14 13:39:04 +0530

    Update 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp

--- Graph Theory/1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
+++ Graph Theory/1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -56,6 +57,50 @@ public:
         vector<int> num(10, 0);
         int count = 0, len = 0;
         dfs(root, len, num, count);
+        return count;
+    }
+};
+
+//Method - 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count;
+    void solve(TreeNode* root, vector<int> &path){
+        if(root == NULL){
+            return;
+        }
+        if(root -> left == NULL && root -> right == NULL){
+            path[root -> val]++;
+            int odd = 0;
+            for(int i = 1; i <= 9; i++){
+                if(path[i] % 2 != 0){
+                    odd++;
+                }
+            }
+            if(odd <= 1) count++;
+            path[root -> val]--;
+            return;
+        }
+        path[root -> val]++;
+        solve(root -> left, path);
+        solve(root -> right, path);
+        path[root -> val]--;
+    }
+    int pseudoPalindromicPaths (TreeNode* root) {
+        count = 0;
+        vector<int> path(10,0);
+        solve(root, path);
         return count;
     }
 };
