72d958ce4e08215646cd8802c7437ec204e1a57b
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-14 11:55:30 +0530

    Update 1443. Minimum Time to Collect All Apples in a Tree.cpp

--- Graph Theory/1443. Minimum Time to Collect All Apples in a Tree.cpp
+++ Graph Theory/1443. Minimum Time to Collect All Apples in a Tree.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     int dfs(int node, int &mintime, vector<int> al[], vector<bool> &hasApple, vector<int> &vis){
@@ -49,5 +50,36 @@ public:
         vector<int> vis(n, 0);
         dfs(0, mintime, al, hasApple, vis);
         return mintime;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int solve(int node, int par, vector<int> al[], vector<bool> &hasApple){
+        
+        int sum = 0;
+        
+        for(int i = 0; i < al[node].size(); i++){
+            int child = al[node][i];
+            if(child != par){
+                sum += solve(child, node, al, hasApple);
+            }
+        }
+        if(hasApple[node] || sum) sum += 2;
+        return sum;
+    }
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        
+        vector<int> al[n];
+        
+        for(int i = 0; i < edges.size(); i++){
+            al[edges[i][0]].push_back(edges[i][1]);
+            al[edges[i][1]].push_back(edges[i][0]);
+        }
+        
+        int ans = solve(0, -1, al, hasApple);
+        if(ans) ans -= 2; 
+        return ans;
     }
 };
