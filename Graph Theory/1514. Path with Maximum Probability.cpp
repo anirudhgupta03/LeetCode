6155531db83223ec629fd3f4c161aa313c5632df
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-15 00:00:04 +0530

    Update 1514. Path with Maximum Probability.cpp

--- Graph Theory/1514. Path with Maximum Probability.cpp
+++ Graph Theory/1514. Path with Maximum Probability.cpp
@@ -1,56 +1,45 @@
-//Dijkstra's Algo
 class Solution {
 public:
-    #define pid pair<int, double>
+    #define pid pair<int,double>
     struct cmp{
-      bool operator()(pid p1, pid p2){
+      bool operator()(pid &p1, pid &p2){
           return p1.second < p2.second;
       }  
     };
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
         
-        double maxProb = 0;
-        
-        vector<pair<int,double>> al[n];
+        vector<pid> al[n];
         
         for(int i = 0; i < edges.size(); i++){
-            int a = edges[i][0], b = edges[i][1];
-            al[a].push_back({b,succProb[i]});
-            al[b].push_back({a,succProb[i]});
+            al[edges[i][0]].push_back({edges[i][1], succProb[i]});
+            al[edges[i][1]].push_back({edges[i][0], succProb[i]});
         }
+        
+        priority_queue<pid, vector<pid>, cmp> pq;
+        pq.push({start, 1.0});
         
         vector<int> vis(n, 0);
         
-        priority_queue<pid, vector<pid>, cmp> pq;
-        
-        pq.push({start, -1});
-        
         while(!pq.empty()){
+            
             int curr = pq.top().first;
             double prob = pq.top().second;
+            
             pq.pop();
             
-            if(vis[curr]){
-                continue;
+            if(curr == end){
+                return prob;
             }
+            
+            if(vis[curr]) continue;
             vis[curr] = 1;
             
-            if(curr == end){
-                maxProb = prob;
-            }
-            
             for(int i = 0; i < al[curr].size(); i++){
-                int child = al[curr][i].first;
-                if(vis[child] == 0){
-                    if(prob == -1){
-                        pq.push({child, al[curr][i].second});
-                    }
-                    else{
-                        pq.push({child, prob*al[curr][i].second});
-                    }
+                if(vis[al[curr][i].first] == 0){
+                    pq.push({al[curr][i].first, prob*al[curr][i].second});
                 }
             }
         }
-        return maxProb;
+        return 0;
     }
 };
