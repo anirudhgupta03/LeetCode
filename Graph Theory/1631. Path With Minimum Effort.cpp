b128dbbe44df22917cbb52bbdb586f08bc2cb6d3
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-15 23:09:09 +0530

    Update 1631. Path With Minimum Effort.cpp

--- Graph Theory/1631. Path With Minimum Effort.cpp
+++ Graph Theory/1631. Path With Minimum Effort.cpp
@@ -46,3 +46,46 @@ public:
         return 0;
     }
 };
+
+//Method - 2
+class Solution {
+public:
+    #define pipii pair<int,pair<int,int>>
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    struct cmp{
+      bool operator()(pipii &p1, pipii &p2){
+          return p1.first > p2.first;
+      }  
+    };
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        
+        int m = heights.size(), n = heights[0].size();
+        
+        priority_queue<pipii, vector<pipii>, cmp> pq;
+        
+        pq.push({0,{0,0}});
+        
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        
+        while(!pq.empty()){
+            
+            int diff = pq.top().first, x = pq.top().second.first, y = pq.top().second.second;
+            pq.pop();
+            
+            if(x == m - 1 && y == n - 1){
+                return diff;
+            }
+            if(vis[x][y]) continue;
+            vis[x][y] = 1;
+            
+            for(int i = 0; i < 4; i++){
+                int xo = x + dx[i], yo = y + dy[i];
+                if(xo >= 0 && yo >= 0 && xo < m && yo < n && vis[xo][yo] == 0){
+                    pq.push({max(diff, abs(heights[xo][yo] - heights[x][y])),{xo,yo}});
+                }
+            }
+        }
+        return 0;
+    }
+};
