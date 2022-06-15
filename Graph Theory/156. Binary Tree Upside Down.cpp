40594e54ed88425d58d379b3d82e4bd0c52ea919
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-15 11:50:28 +0530

    Update 156. Binary Tree Upside Down.cpp

--- Graph Theory/156. Binary Tree Upside Down.cpp
+++ Graph Theory/156. Binary Tree Upside Down.cpp
@@ -1,4 +1,6 @@
+//Method - 1
 //https://www.lintcode.com/problem/binary-tree-upside-down/description
+//https://www.codingninjas.com/codestudio/problems/upside-down-binary-tree_1281853?leftPanelTab=0
 /**
  * Definition of TreeNode:
  * class TreeNode {
@@ -52,5 +54,57 @@ public:
         solve(root, flag, start);
 
         return start;
+    }
+};
+
+//Method - 2
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: the root of binary tree
+     * @return: new root
+     */
+    TreeNode* upsideDownBinaryTree(TreeNode *root) {
+        
+        if(root == NULL) return root;
+        stack<TreeNode*> st;
+        TreeNode* head = NULL;
+
+        while(root){
+            st.push(root);
+            root = root -> left;
+        }
+         
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+
+            if(head == NULL){
+                head = curr;
+            }
+
+            if(!st.empty()){
+                TreeNode* par = st.top();
+                TreeNode* rt = par -> right;
+                curr -> left = rt;
+                par -> left = NULL;
+                par -> right = NULL;
+                curr -> right = par;
+            }
+        }
+        return head;
     }
 };
