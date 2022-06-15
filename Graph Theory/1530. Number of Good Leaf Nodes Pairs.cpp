ac058564c8cff07a0fc8824ff14d0ca7d425d35c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-15 10:18:49 +0530

    Update 1530. Number of Good Leaf Nodes Pairs.cpp

--- Graph Theory/1530. Number of Good Leaf Nodes Pairs.cpp
+++ Graph Theory/1530. Number of Good Leaf Nodes Pairs.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -54,6 +55,64 @@ public:
             unordered_set<TreeNode*> vis;
             flag = false;
             dfs(0, curr, vis, distance);
+        }
+        return count/2;
+    }
+};
+
+//Method - 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count;
+    void dfs(TreeNode* root, TreeNode* parent, unordered_map<TreeNode*, TreeNode*> &par, vector<TreeNode*> &leaf){
+        par[root] = parent;
+        if(root == NULL){
+            return;
+        }
+        if(root -> left == NULL && root -> right == NULL){
+            leaf.push_back(root);
+        }
+        dfs(root -> left, root, par, leaf);
+        dfs(root -> right, root, par, leaf);
+    }
+    void dfs(TreeNode* node, TreeNode* parent, unordered_map<TreeNode*, TreeNode*> &par, int distance){
+        
+        if(node == NULL){
+            return;
+        }
+        if(node -> left == NULL && node -> right == NULL){
+            if(distance >= 0){
+                count++;
+            }
+        }
+        
+        if(par[node] != parent) dfs(par[node], node, par, distance - 1);
+        
+        if(node -> left && (node -> left) != parent) dfs(node -> left, node, par, distance - 1);
+        
+        if(node -> right && (node -> right) != parent) dfs(node -> right, node, par, distance - 1);
+    }
+    int countPairs(TreeNode* root, int distance) {
+        
+        unordered_map<TreeNode*,TreeNode*> par;
+        vector<TreeNode*> leaf;
+        dfs(root, NULL, par, leaf);
+        count = 0;
+        
+        for(auto &x: leaf){
+            dfs(x, NULL, par, distance);
+            count--;
         }
         return count/2;
     }
