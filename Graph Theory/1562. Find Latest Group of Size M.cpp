d40319cde5e1af6bd01446f8aef14feed0a5c836
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-15 14:32:39 +0530

    Update 1562. Find Latest Group of Size M.cpp

--- Graph Theory/1562. Find Latest Group of Size M.cpp
+++ Graph Theory/1562. Find Latest Group of Size M.cpp
@@ -1,5 +1,6 @@
 //Disjoint Set Union-Find
 //Ref: https://www.youtube.com/watch?v=ZJFP49Tholc
+//Method - 1
 class Solution {
 public:
     int find(int x, vector<int> &par){
@@ -49,5 +50,64 @@ public:
             step++;
         }
         return seen;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    #define pii pair<int,int>
+    int findP(int node, vector<int> &par){
+        if(par[node] == -1){
+            return node;
+        }
+        return par[node] = findP(par[node], par);
+    }
+    int findLatestStep(vector<int>& arr, int m) {
+        
+        int n = arr.size();
+        
+        vector<int> par(n, -1), size(n, 0), v(n, 0);
+        
+        int ans = -1, count = 0, lpar, rpar, lsize, rsize, ind, tsize;
+        bool flag;
+        
+        for(int i = 0; i < n; i++){ 
+            
+            ind = arr[i] - 1;
+            flag = false;
+            tsize = 0;
+            v[ind] = 1;
+            
+            if(ind + 1 < n && v[ind + 1]){
+                rpar = findP(ind + 1, par);
+                rsize = size[rpar];
+                tsize = rsize + 1;
+                if(rsize == m) count--;
+                par[rpar] = ind;
+                size[ind]= tsize;
+                flag = true;
+            }
+            
+            if(ind - 1 >= 0 && v[ind - 1]){
+                lpar = findP(ind - 1, par);
+                lsize = size[lpar];
+                tsize += lsize;
+                if(!flag) tsize++;
+                if(lsize == m) count--;
+                par[ind] = lpar;
+                size[lpar] = tsize;
+                flag = true;
+            }
+            
+            if(!flag){
+                size[ind] = 1;
+                tsize = 1;
+            }
+            
+            if(tsize == m) count++;
+            if(count) ans = i + 1;
+        }
+        return ans;
     }
 };
