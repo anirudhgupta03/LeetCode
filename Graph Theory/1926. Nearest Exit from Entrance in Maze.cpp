9ec18d6d38910a47858feff88771b20258027909
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-16 13:31:29 +0530

    Update 1926. Nearest Exit from Entrance in Maze.cpp

--- Graph Theory/1926. Nearest Exit from Entrance in Maze.cpp
+++ Graph Theory/1926. Nearest Exit from Entrance in Maze.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     int dx[4] = {0,0,-1,1};
@@ -46,5 +47,49 @@ public:
             return -1;
         }
         return minsteps;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        
+        int m = maze.size(), n = maze[0].size();
+        
+        queue<pair<int,int>> q;
+        
+        q.push({entrance[0], entrance[1]});
+        
+        int steps = 0;
+        
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                
+                int x = q.front().first, y = q.front().second;
+                q.pop();
+                
+                if(x == 0 || y == 0 || x == m - 1 || y == n - 1){
+                    if(x != entrance[0] || y != entrance[1])
+                    return steps;
+                }
+                if(vis[x][y]) continue;
+                vis[x][y] = 1;
+                
+                for(int i = 0; i < 4; i++){
+                    int xo = x + dx[i], yo = y + dy[i];
+                    if(xo >= 0 && yo >= 0 && xo < m && yo < n && vis[xo][yo] == 0 && maze[xo][yo] == '.'){
+                        q.push({xo, yo});
+                    }
+                }
+            }
+            steps++;
+        }
+        return -1;
     }
 };
