1b95d7327b68b90e159f175d30a704052271ddc0
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-16 09:39:06 +0530

    Update 1706. Where Will the Ball Fall.cpp

--- Graph Theory/1706. Where Will the Ball Fall.cpp
+++ Graph Theory/1706. Where Will the Ball Fall.cpp
@@ -1,4 +1,5 @@
 //Ref: https://www.youtube.com/watch?v=qCKXRsIItG8
+//Method - 1
 class Solution {
 public:
     vector<int> findBall(vector<vector<int>>& grid) {
@@ -20,5 +21,46 @@ public:
             ans[j] = cpos;
         }
         return ans;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& grid) {
+        
+        int m = grid.size(), n = grid[0].size();
+        
+        vector<int> res(n,-1);
+        
+        for(int i = 0; i < n; i++){
+
+            int x = 0, y = i;
+            
+            while(x < m && y >= 0 && y < n){
+                if(grid[x][y] == 1){
+                    if(y + 1 < n && grid[x][y + 1] == -1){
+                        break;
+                    }
+                    else{
+                        x++;
+                        y++;
+                    }
+                }
+                else{
+                    if(y - 1 >= 0 && grid[x][y - 1] == 1){
+                        break;
+                    }
+                    else{
+                        x++;
+                        y--;
+                    }
+                }
+            }
+            if(x == m && y >= 0 && y < n){
+                res[i] = y;
+            }
+        }
+        return res;
     }
 };
