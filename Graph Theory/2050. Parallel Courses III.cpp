3fd4d29a712a8bf6a00eaefdc73ef0a0d1e610dd
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-16 17:44:05 +0530

    Update 2050. Parallel Courses III.cpp

--- Graph Theory/2050. Parallel Courses III.cpp
+++ Graph Theory/2050. Parallel Courses III.cpp
@@ -1,4 +1,5 @@
 //Ref: https://leetcode.com/problems/parallel-courses-iii/discuss/1537479/C%2B%2BPython-Topology-Sort-O(M%2B-N)-Clean-and-Concise
+//Method - 1
 class Solution {
 public:
     int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
@@ -44,5 +45,46 @@ public:
             }
         }
         return *max_element(dist.begin(),dist.end());
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        
+        vector<int> al[n + 1], indegree(n + 1, 0);
+        
+        for(auto &x: relations){
+            al[x[0]].push_back(x[1]);
+            indegree[x[1]]++;
+        }
+        
+        queue<int> q;
+        vector<int> ttime(n + 1, INT_MIN);
+        
+        for(int i = 1; i <= n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+                ttime[i] = time[i - 1];
+            }
+        }
+        
+        int minTime = INT_MIN;
+        
+        while(!q.empty()){
+           
+            int curr = q.front();
+            q.pop();
+            
+            for(int i = 0; i < al[curr].size(); i++){
+                int child = al[curr][i];
+                if(ttime[curr] + time[child - 1] > ttime[child]){
+                    ttime[child] = ttime[curr] + time[child - 1];
+                    q.push(child);
+                }
+            }
+        }
+        return *max_element(ttime.begin(), ttime.end());
     }
 };
