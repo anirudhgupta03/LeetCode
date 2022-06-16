65ef3cb8b580d2b2e4ce4f5c74fca93060acc658
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-16 13:50:36 +0530

    Update 1970. Last Day Where You Can Still Cross.cpp

--- Graph Theory/1970. Last Day Where You Can Still Cross.cpp
+++ Graph Theory/1970. Last Day Where You Can Still Cross.cpp
@@ -65,3 +65,64 @@ public:
     }
     
 };
+
+//Binary Search + BFS
+class Solution {
+public:
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    bool isPossibleToCross(int day, vector<vector<int>> &cells, int row, int col){
+        
+        vector<vector<int>> grid(row, vector<int>(col, 0));
+        
+        for(int i = 0; i <= day; i++){
+            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
+        }
+        
+        queue<pair<int,int>> q;
+        
+        for(int j = 0; j < col; j++){
+            if(grid[0][j] == 0){
+                q.push({0, j});
+            }
+        }
+        
+        while(!q.empty()){
+            
+            int x = q.front().first, y = q.front().second;
+            q.pop();
+            
+            if(x == row - 1){
+                return true;
+            }
+            if(grid[x][y]) continue;
+            grid[x][y] = 1;
+            
+            for(int i = 0; i < 4; i++){
+                int xo = x + dx[i], yo = y + dy[i];
+                if(xo >= 0 && yo >= 0 && xo < row && yo < col && grid[xo][yo] == 0){
+                    q.push({xo, yo});
+                }
+            }
+        }
+        return false;
+    }
+    int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
+        
+        int lo = 0, hi = cells.size() - 1, ans;
+        
+        while(lo <= hi){
+            
+            int mid = (lo + hi)/2;
+            
+            if(isPossibleToCross(mid, cells, row, col)){
+                ans = mid;
+                lo = mid + 1;
+            }
+            else{
+                hi = mid - 1;
+            }
+        }
+        return ans + 1;
+    }
+};
