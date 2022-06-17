0c090171332fda0475f4240c2c5df50c9407c10e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-17 18:06:30 +0530

    Update 269. Alien Dictionary.cpp

--- Graph Theory/269. Alien Dictionary.cpp
+++ Graph Theory/269. Alien Dictionary.cpp
@@ -39,3 +39,57 @@ class Solution{
         return res;
     }
 };
+
+//https://www.codingninjas.com/codestudio/problems/alien-dictionary_630423?leftPanelTab=0
+#include<bits/stdc++.h>
+using namespace std;
+vector<char> getAlienLanguage(string* dictionary, int n)
+{
+    vector<vector<int>> adj(26, vector<int>(26,0));
+    vector<int> indegree(26, 0), freq(26, 0);
+    
+    for(int i = 1; i < n; i++){
+        string prev = dictionary[i - 1], curr = dictionary[i];
+        int ptr1 = 0, ptr2 = 0;
+        
+        while(ptr1 < prev.size() && ptr2 < curr.size()){
+            if(prev[ptr1] != curr[ptr2]){
+                freq[prev[ptr1] - 'a'] = 1;
+                freq[curr[ptr2] - 'a'] = 1;
+                if(adj[prev[ptr1] - 'a'][curr[ptr2] - 'a'] == 0){
+                    adj[prev[ptr1] - 'a'][curr[ptr2] - 'a'] = 1;
+                    indegree[curr[ptr2] - 'a']++;
+                }
+                break;
+            }
+            else{
+                freq[prev[ptr1] - 'a'] = 1;
+                ptr1++;
+                ptr2++;
+            }
+        }
+    }
+    
+    queue<int> q;
+    for(int i = 0; i < 26; i++){
+        if(indegree[i] == 0 && freq[i]){
+            q.push(i);
+        }
+    }
+    vector<char> res;
+    while(!q.empty()){
+        int curr = q.front();
+        q.pop();
+        res.push_back((char)(curr + 'a'));
+        
+        for(int i = 0; i < 26; i++){
+            if(adj[curr][i]){
+                indegree[i]--;
+                if(indegree[i] == 0){
+                    q.push(i);
+                }
+            }
+        }
+    }
+    return res;
+}
