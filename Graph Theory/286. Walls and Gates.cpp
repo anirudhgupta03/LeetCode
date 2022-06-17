d5f73b46abd9370a9fca1f28d6b913ea95ebf86d
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-17 19:19:46 +0530

    Update 286. Walls and Gates.cpp

--- Graph Theory/286. Walls and Gates.cpp
+++ Graph Theory/286. Walls and Gates.cpp
@@ -1,3 +1,5 @@
+//Multi-Source BFS
+//https://www.lintcode.com/problem/663/
 class Solution {
 public:
     /**
@@ -5,37 +7,31 @@ public:
      * @return: nothing
      */
     int dx[4] = {-1,1,0,0};
-    int dy[4] = {0,0,-1,1};
+    int dy[4] = {0,0,-1,1}; 
     void wallsAndGates(vector<vector<int>> &rooms) {
         
         int m = rooms.size(), n = rooms[0].size();
-
         queue<pair<int,int>> q;
-
-        for(int i = 0; i < rooms.size(); i++){
-            for(int j = 0; j < rooms[i].size(); j++){
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
                 if(rooms[i][j] == 0){
-                    q.push({i,j});
+                    q.push({i, j});
                 }
             }
         }
 
-        vector<vector<int>> vis(m, vector<int>(n, 0));
         while(!q.empty()){
-            pair<int,int> p = q.front();
+
+            int x = q.front().first, y = q.front().second;
             q.pop();
-
-            int x = p.first, y = p.second;
-            if(vis[x][y]) continue;
-
-            vis[x][y] = 1;
 
             for(int i = 0; i < 4; i++){
                 int xo = x + dx[i], yo = y + dy[i];
-                if(xo >= 0 && yo >= 0 && xo < m && yo < n){
-                    if(vis[xo][yo] == 0 && rooms[x][y] + 1 < rooms[xo][yo]){
+                if(xo >= 0 && yo >= 0 && xo < m && yo < n && rooms[xo][yo] != -1){
+                    if(rooms[x][y] + 1 < rooms[xo][yo]){
                         rooms[xo][yo] = rooms[x][y] + 1;
-                        q.push({xo,yo});
+                        q.push({xo, yo});
                     }
                 }
             }
