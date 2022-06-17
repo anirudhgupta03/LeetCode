335f44c9bcab02c1b7caa8b314b15efef91db711
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-17 16:12:01 +0530

    Update 2115. Find All Possible Recipes from Given Supplies.cpp

--- Graph Theory/2115. Find All Possible Recipes from Given Supplies.cpp
+++ Graph Theory/2115. Find All Possible Recipes from Given Supplies.cpp
@@ -44,6 +44,50 @@ public:
 };
 
 //Method - 2
+//Optimised Method - 1
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        
+        int n = recipes.size();
+        
+        vector<int> vis(n, 0);
+        
+        unordered_map<string,int> available;
+        
+        for(auto &x: supplies){
+            available[x] = 1;
+        }
+        
+        while(1){
+            bool oflag = false;
+            for(int i = 0; i < n; i++){
+                if(vis[i] == 0){
+                    bool flag = true;
+                    for(auto &x: ingredients[i]){
+                        if(available[x] == 0){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag){
+                        oflag = true;
+                        vis[i] = 1;
+                        available[recipes[i]] = 1;
+                    }
+                }
+            }
+            if(!oflag) break;
+        }
+        vector<string> res;
+        for(int i = 0; i < n; i++){
+            if(vis[i]) res.push_back(recipes[i]);
+        }
+        return res;
+    }
+};
+
+//Method - 3
 //Topological Sorting
 class Solution {
 public:
@@ -94,5 +138,45 @@ public:
             }
         }
         return ans;
+    }
+};
+
+//Method - 4
+//Ref: https://www.youtube.com/watch?v=yKi8TIZcQoU
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        
+        unordered_map<string, vector<string>> al;
+        unordered_map<string,int> indegree;
+        
+        for(int i = 0; i < recipes.size(); i++){
+            for(int j = 0; j < ingredients[i].size(); j++){
+                al[ingredients[i][j]].push_back(recipes[i]);
+                indegree[recipes[i]]++;
+            }
+        }
+        
+        queue<string> q;
+        
+        for(int i = 0; i < supplies.size(); i++){
+            q.push(supplies[i]);
+        }
+        
+        vector<string> res;
+        
+        while(!q.empty()){
+            string curr = q.front();
+            q.pop();
+            
+            for(auto &x: al[curr]){
+                indegree[x]--;
+                if(indegree[x] == 0){
+                    q.push(x);
+                    res.push_back(x);
+                }
+            }
+        }
+        return res;
     }
 };
