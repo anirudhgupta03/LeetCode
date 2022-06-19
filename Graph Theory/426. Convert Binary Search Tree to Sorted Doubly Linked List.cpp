3407f0df9d1114a048c944b12e3d392a44b56684
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-19 16:01:37 +0530

    Update 426. Convert Binary Search Tree to Sorted Doubly Linked List.cpp

--- Graph Theory/426. Convert Binary Search Tree to Sorted Doubly Linked List.cpp
+++ Graph Theory/426. Convert Binary Search Tree to Sorted Doubly Linked List.cpp
@@ -1,4 +1,5 @@
 //https://www.lintcode.com/problem/1534/
+//Method - 1
 /**
  * Definition of TreeNode:
  * class TreeNode {
@@ -65,5 +66,54 @@ public:
         start -> left = end;
         end -> right = start;
         return start;
+    }
+};
+
+//Method - 2
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: root of a tree
+     * @return: head node of a doubly linked list
+     */
+    TreeNode* head, *pre;
+    void inorder(TreeNode* root){
+        if(root == NULL){
+            return;
+        }
+        inorder(root -> left);
+        if(head == NULL){
+            head = root;
+        }
+        else{
+            pre -> right = root;
+            root -> left = pre;
+        }
+        pre = root;
+        inorder(root -> right);
+    }
+    TreeNode * treeToDoublyList(TreeNode * root) {
+        if(root == NULL){
+            return NULL;
+        }
+        head = NULL;
+        pre = NULL;
+        inorder(root);
+        head -> left = pre;
+        pre -> right = head;
+        return head;
     }
 };
