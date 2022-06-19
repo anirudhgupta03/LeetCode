0c6013d5057713c2b7d62a002f2e5630a1f3f463
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-19 17:02:36 +0530

    Update 437. Path Sum III.cpp

--- Graph Theory/437. Path Sum III.cpp
+++ Graph Theory/437. Path Sum III.cpp
@@ -1,4 +1,5 @@
 //Ref: https://www.youtube.com/watch?v=UQzXYDN49cs
+//Method - 1
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -32,5 +33,44 @@ public:
         int ans = 0;
         solve(root, sum, ans);
         return ans;
+    }
+};
+
+//Method - 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<long ,long> umap;
+    int count;
+    void solve(TreeNode* root, long sum, long targetSum){
+        
+        if(root == NULL){
+            return;
+        }
+        if(umap[sum + root -> val - targetSum]){
+            count += umap[sum + root -> val - targetSum];
+        }
+        
+        umap[sum + root -> val]++;
+        solve(root -> left, sum + root -> val, targetSum);
+        solve(root -> right, sum + root -> val, targetSum);
+        umap[sum + root -> val]--;
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        umap.clear();
+        umap[0] = 1;
+        count = 0;
+        solve(root, 0, targetSum);
+        return count;
     }
 };
