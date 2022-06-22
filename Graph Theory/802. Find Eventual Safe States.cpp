b3688d90321a3dd96135be059fb7995c5dd70e04
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-22 11:23:39 +0530

    Update 802. Find Eventual Safe States.cpp

--- Graph Theory/802. Find Eventual Safe States.cpp
+++ Graph Theory/802. Find Eventual Safe States.cpp
@@ -1,3 +1,5 @@
+//Method - 1
+//Using DFS
 class Solution {
 public:
     bool dfs(vector<vector<int>> &graph, int node, vector<int> &vis, unordered_set<int> &safe_nodes, unordered_set<int> &cycle_nodes){
@@ -40,5 +42,58 @@ public:
             if(flag) ans.push_back(i);
         }
         return ans;
+    }
+};
+
+//Method - 2
+//Using Topological Sort
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        
+        vector<int> indegree(n, 0), al[n];
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < graph[i].size(); j++){
+                al[graph[i][j]].push_back(i);
+                indegree[i]++;
+            }
+        }
+        
+        queue<int> q;
+        
+        vector<int> res;
+        
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int sz = q.size();
+            bool flag = false;
+            while(sz--){
+                
+                int curr = q.front();
+                q.pop();
+                
+                res.push_back(curr);
+                
+                for(int i = 0; i < al[curr].size(); i++){
+                    int child = al[curr][i];
+                    indegree[child]--;
+                    if(indegree[child] == 0){
+                        q.push(child);
+                        flag = true;
+                    }
+                }
+            }
+            if(!flag) break;
+        }
+        sort(res.begin(),res.end());
+        return res;
     }
 };
