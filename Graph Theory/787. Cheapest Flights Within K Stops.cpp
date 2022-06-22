a20ce3380822046bdfa35d872b3a9b8cac38b4dc
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-22 10:20:58 +0530

    Update 787. Cheapest Flights Within K Stops.cpp

--- Graph Theory/787. Cheapest Flights Within K Stops.cpp
+++ Graph Theory/787. Cheapest Flights Within K Stops.cpp
@@ -1,3 +1,5 @@
+//Method - 1
+//Using Bellman Ford Algorithm
 //Ref: https://www.youtube.com/watch?v=5eIK3zUdYmE
 class Solution {
 public:
@@ -20,5 +22,52 @@ public:
             return -1;
         }
         return prices[dst];
+    }
+};
+
+//Method - 2
+//Using Dijkstra's Algorithm
+class Solution {
+public:
+    #define pipii pair<int,pair<int,int>>
+    struct cmp{
+      bool operator()(pipii &p1, pipii &p2){
+          return p1.second.second > p2.second.second;
+      }  
+    };
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        
+        vector<pair<int,int>> al[n];
+        
+        for(auto &x: flights){
+            al[x[0]].push_back({x[1], x[2]});
+        }
+        
+        priority_queue<pipii, vector<pipii>, cmp> pq;
+        pq.push({src,{-1,0}});
+        
+        set<pipii> vis;
+        
+        while(!pq.empty()){
+            
+            int curr = pq.top().first, stops = pq.top().second.first, cost = pq.top().second.second;
+            pq.pop();
+            
+            if(curr == dst){
+                return cost;
+            }
+            
+            if(vis.find({curr,{stops, cost}}) != vis.end()) continue;
+            vis.insert({curr,{stops, cost}});
+            
+            for(int i = 0; i < al[curr].size(); i++){
+                int child = al[curr][i].first;
+                
+                if(stops + 1 <= k){
+                    pq.push({child,{stops + 1, cost + al[curr][i].second}});
+                }
+            }
+        }
+        return -1;
     }
 };
