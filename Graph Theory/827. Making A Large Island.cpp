9a0d2922ae97166626039dfd506bd01c2061a619
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-06-23 10:08:52 +0530

    Update 827. Making A Large Island.cpp

--- Graph Theory/827. Making A Large Island.cpp
+++ Graph Theory/827. Making A Large Island.cpp
@@ -156,3 +156,91 @@ public:
         return maxSize;
     }
 };
+
+//Union-Find
+class Solution {
+public:
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    void dfs(int abparent, int x, int y, int n, vector<vector<int>> &grid, vector<vector<int>> &vis, vector<int> &par, int &count){
+        
+        par[n*x + y] = abparent;
+        vis[x][y] = 1;
+        count++;
+        
+        for(int i = 0; i < 4; i++){
+            int xo = x + dx[i], yo = y + dy[i];
+            if(xo >= 0 && yo >= 0 && xo < n && yo < n && vis[xo][yo] == 0 && grid[xo][yo]){
+                dfs(abparent, xo, yo, n, grid, vis, par, count);
+            }
+        }
+    }
+    
+    int findP(int node, vector<int> &par){
+        if(par[node] == node){
+            return node;
+        }
+        return findP(par[node], par);
+    }
+    
+    void unionP(int node1, int node2, vector<int> &par){
+        par[node2] = node1;
+    }
+    
+    int largestIsland(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        
+        vector<int> par(n*n), size(n*n, 0);
+        
+        for(int i = 0; i < n*n; i++){
+            par[i] = i;
+        }
+        
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
+                    int count = 0;
+                    dfs(n*i + j, i, j, n, grid, vis, par, count);
+                    size[n*i + j] = count;
+                }
+            }
+        }
+        
+        int largestSize = 0;
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                
+                if(grid[i][j] == 0){
+                    
+                    int count = 1;
+                    int parc = par[n*i + j];
+                    vector<int> temp(4,-1);
+                    
+                    for(int k = 0; k < 4; k++){
+                        int io = i + dx[k], jo = j + dy[k];
+                        if(io >= 0 && jo >= 0 && io < n && jo < n && grid[io][jo]){
+                            int park = findP(n*io + jo, par);
+                            temp[k] = park;
+                            if(park != parc){
+                                count += size[park];
+                                unionP(parc, park, par);
+                            }
+                        }
+                    }
+                    
+                    largestSize = max(largestSize, count);
+                    for(int k = 0; k < 4; k++){
+                        if(temp[k] != -1){
+                            par[temp[k]] = temp[k];
+                        }
+                    }
+                }
+            }
+        }
+        return largestSize == 0 ? n*n : largestSize;
+    }
+};
