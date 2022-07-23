b53d92aa4f625317af4538b404f5c208838fbfb2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-07-23 22:49:07 +0530

    Create 1547. Minimum Cost to Cut a Stick.cpp

---
+++ DP/1547. Minimum Cost to Cut a Stick.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
+        
+        if(i > j){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int mini = INT_MAX;
+        
+        for(int ind = i; ind <= j; ind++){
+            int cost = cuts[j + 1] - cuts[i - 1] + solve(i, ind - 1, cuts, dp) + solve(ind + 1, j, cuts, dp);
+            mini = min(mini, cost);
+        }
+        return dp[i][j] = mini;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        
+        cuts.insert(cuts.begin(), 0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
+        n = cuts.size();
+        vector<vector<int>> dp(n, vector<int>(n, - 1));
+        return solve(1, n - 2, cuts, dp);
+    }
+};
