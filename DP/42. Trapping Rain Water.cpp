f134bf65023c8ea7d634cb8613a411027705cdfe
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-06 17:31:31 +0530

    Update 42. Trapping Rain Water.cpp

--- DP/42. Trapping Rain Water.cpp
+++ DP/42. Trapping Rain Water.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//TC: O(2*N)
+//SC: O(2*N)
 class Solution {
 public:
     int trap(vector<int>& height) {
@@ -27,5 +30,36 @@ public:
         }
         
         return waterTrapped;
+    }
+};
+
+//Method - 2
+//TC: O(N)
+//SC: O(1)
+//Ref: https://www.youtube.com/watch?v=m18Hntz4go8
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+        int n = height.size();
+        
+        int lo = 0, hi = n - 1;
+        
+        int sum = 0, lmax = INT_MIN, rmax = INT_MIN;
+        
+        while(lo <= hi){
+            
+            if(height[lo] < height[hi]){
+                lmax = max(lmax, height[lo]);
+                sum += lmax - height[lo];
+                lo++;
+            }
+            else{
+                rmax = max(rmax, height[hi]);
+                sum += rmax - height[hi];
+                hi--;
+            }
+        }
+        return sum;
     }
 };
