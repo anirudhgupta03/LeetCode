48507be5ad4aea5e78ced7e0e8ca3fc3ef50c1a5
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-07 00:03:28 +0530

    Update 72. Edit Distance.cpp

--- DP/72. Edit Distance.cpp
+++ DP/72. Edit Distance.cpp
@@ -1,3 +1,37 @@
+//Top-Down
+//TC: O(len1*len2)
+//SC: O(len1*len2) + Auxilliary Stack Space
+class Solution {
+public:
+    int solve(int i, int j, string &word1, string &word2, vector<vector<int>> &dp){
+        
+        if(i == 0){
+            return j;
+        }
+        if(j == 0){
+            return i;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(word1[i - 1] == word2[j - 1]){
+            return dp[i][j] = solve(i - 1, j - 1, word1, word2, dp);
+        }
+        return dp[i][j] = 1 + min({solve(i - 1, j - 1, word1, word2, dp), solve(i - 1, j, word1, word2, dp), solve(i, j - 1, word1, word2, dp)});
+    }
+    int minDistance(string word1, string word2) {
+        
+        int len1 = word1.size(), len2 = word2.size();
+        
+        vector<vector<int>> dp(len1 + 1, vector<int>(len2 + 1, -1));
+        
+        return solve(len1, len2, word1, word2, dp);
+    }
+};
+
+//Bottom-Up
+//TC: O(len1*len2)
+//SC: O(len1*len2)
 class Solution {
 public:
     int minDistance(string word1, string word2) {
@@ -25,5 +59,39 @@ public:
         }
         
         return dp[l1][l2];
+    }
+};
+
+//Bottom-Up
+//TC: O(len1*len2)
+//SC: O(len2)
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        
+        int len1 = word1.size(), len2 = word2.size();
+        
+        vector<int> pre(len2 + 1, 0);
+        
+        for(int j = 0; j <= len2; j++){
+            pre[j] = j;
+        }
+        
+        for(int i = 1; i <= len1; i++){
+            vector<int> curr(len2 + 1);
+            for(int j = 0; j <= len2; j++){
+                if(j == 0){
+                    curr[j] = i;
+                }
+                else if(word1[i - 1] == word2[j - 1]){
+                    curr[j] = pre[j - 1];
+                }
+                else{
+                    curr[j] = min({pre[j - 1], pre[j], curr[j - 1]}) + 1;
+                }
+            }
+            pre = curr;
+        }
+        return pre[len2];
     }
 };
