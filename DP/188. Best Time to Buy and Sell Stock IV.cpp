ef6d2b29de0d1e54585eda525f726a3a935b5e89
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-07 19:41:36 +0530

    Update 188. Best Time to Buy and Sell Stock IV.cpp

--- DP/188. Best Time to Buy and Sell Stock IV.cpp
+++ DP/188. Best Time to Buy and Sell Stock IV.cpp
@@ -1,3 +1,95 @@
+//Top-Down
+//TC: O(n*2*k)
+//SC: O(n*2*k) + Auxilliary Stack Space
+class Solution {
+public:
+    int solve(int ind, int flag, int k, vector<int> &prices, vector<vector<vector<int>>> &dp){
+        
+        if(ind == prices.size() || k == 0){
+            return 0;
+        }
+        if(dp[ind][flag][k] != -1){
+            return dp[ind][flag][k];
+        }
+        
+        if(flag){
+            return dp[ind][flag][k] = max(solve(ind + 1, 0, k, prices, dp) - prices[ind], solve(ind + 1, 1, k, prices, dp));
+        }
+        else{
+            return dp[ind][flag][k] = max(solve(ind + 1, 1, k - 1, prices, dp) + prices[ind], solve(ind + 1, 0, k, prices, dp));
+        }
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int n = prices.size();
+        
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+        
+        return solve(0, 1, k, prices, dp);
+    }
+};
+
+//Bottom-Up
+//TC: O(n*2*k)
+//SC: O(n*2*k)
+class Solution {
+public:
+    int maxProfit(int tk, vector<int>& prices) {
+        
+        int n = prices.size();
+        
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(tk + 1)));
+        
+        for(int ind = n; ind >= 0; ind--){
+            for(int flag = 0; flag <= 1; flag++){
+                for(int k = 0; k <= tk; k++){
+                    if(ind == n || k == 0){
+                        dp[ind][flag][k] = 0;
+                    }
+                    else if(flag){
+                        dp[ind][flag][k] = max(dp[ind + 1][0][k] - prices[ind], dp[ind + 1][1][k]);
+                    }
+                    else{
+                        dp[ind][flag][k] = max(dp[ind + 1][1][k - 1] + prices[ind], dp[ind + 1][0][k]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][tk];
+    }
+};
+
+//Bottom-Up
+//TC: O(n*2*k)
+//SC: O(2*k)
+class Solution {
+public:
+    int maxProfit(int tk, vector<int>& prices) {
+        
+        int n = prices.size();
+        
+        vector<vector<int>> pre(2, vector<int>(tk + 1, 0)), curr(2, vector<int>(tk + 1));
+        
+        for(int ind = n - 1; ind >= 0; ind--){
+            for(int flag = 0; flag <= 1; flag++){
+                for(int k = 0; k <= tk; k++){
+                    if(ind == n || k == 0){
+                        curr[flag][k] = 0;
+                    }
+                    else if(flag){
+                        curr[flag][k] = max(pre[0][k] - prices[ind], pre[1][k]);
+                    }
+                    else{
+                        curr[flag][k] = max(pre[1][k - 1] + prices[ind], pre[0][k]);
+                    }
+                }
+            }
+            pre = curr;
+        }
+        return pre[1][tk];
+    }
+};
+
 //Approach - 1
 class Solution {
 public:
