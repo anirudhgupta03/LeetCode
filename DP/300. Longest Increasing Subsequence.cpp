ebd1e70d29fa3f3384e00d504d34da4856e948a2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-08 00:49:01 +0530

    Update 300. Longest Increasing Subsequence.cpp

--- DP/300. Longest Increasing Subsequence.cpp
+++ DP/300. Longest Increasing Subsequence.cpp
@@ -1,3 +1,5 @@
+//TC: O(N*N)
+//SC: O(N)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -17,5 +19,28 @@ public:
             maxLen = max(maxLen,dp[i]);
         }
         return maxLen;
+    }
+};
+
+//TC: O(NlogN)
+//SC: O(N)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+        vector<int> v;
+        
+        v.push_back(nums[0]);
+        
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] > v.back()){
+                v.push_back(nums[i]);
+            }
+            else{
+                int ind = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+                v[ind] = nums[i];
+            }
+        }
+        return v.size();
     }
 };
