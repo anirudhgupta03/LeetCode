be280f4f7a0061f9c1097e156e96bfac5ec013ff
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-07 18:36:42 +0530

    Update 132. Palindrome Partitioning II.cpp

--- DP/132. Palindrome Partitioning II.cpp
+++ DP/132. Palindrome Partitioning II.cpp
@@ -40,41 +40,49 @@ public:
 //Optimal Approach
 class Solution {
 public:
-    int dp[2001];
-    int solve(int ind, string &A, vector<vector<int>> &isPalindrome){
-        if(ind == A.size()){
+    int solve(int ind, string &s, vector<vector<bool>> &isPalindrome, vector<int> &dp){
+        
+        if(ind == s.size()){
             return -1;
         }
         if(dp[ind] != -1){
             return dp[ind];
         }
         int minCuts = INT_MAX;
-        for(int i = ind; i < A.size(); i++){
+        for(int i = ind; i < s.size(); i++){
             if(isPalindrome[ind][i]){
-                minCuts = min(minCuts, solve(i + 1, A, isPalindrome) + 1);
+                minCuts = min(minCuts, 1 + solve(i + 1, s, isPalindrome, dp));
             }
         }
         return dp[ind] = minCuts;
     }
     int minCut(string s) {
-        memset(dp,-1,sizeof(dp));
-        int n = s.size();
-        vector<vector<int>> isPalindrome(n, vector<int>(n,0));
-        for(int i = 0; i < n; i++){
-            isPalindrome[i][i] = 1; 
+        
+        int len = s.size();
+        
+        vector<vector<bool>> isPalindrome(len, vector<bool>(len));
+        
+        for(int i = 0; i < len; i++){
+            isPalindrome[i][i] = true;
         }
-        for(int i = 0; i < n - 1; i++){
+        
+        for(int i = 0; i < len - 1; i++){
             if(s[i] == s[i + 1]){
-                isPalindrome[i][i + 1] = 1;
+                isPalindrome[i][i + 1] = true;
             }
         }
-        for(int i = n - 3; i >= 0; i--){
-            for(int j = i + 2; j < n; j++){
-                if(s[i] == s[j]){
-                    isPalindrome[i][j] = isPalindrome[i + 1][j - 1];
+        
+        for(int i = len - 3; i >= 0; i--){
+            for(int j = i + 2; j < len; j++){
+                if(s[i] == s[j] && isPalindrome[i + 1][j - 1]){
+                    isPalindrome[i][j] = true;
+                }
+                else{
+                    isPalindrome[i][j] = false;
                 }
             }
         }
-        return solve(0,s,isPalindrome);
+        vector<int> dp(len, -1);
+        return solve(0, s, isPalindrome, dp);
     }
 };
