d683435b0c8fe3fb99260cf22ed6b4fdc48cd3d8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-08 01:20:46 +0530

    Update 309. Best Time to Buy and Sell Stock with Cooldown.cpp

--- DP/309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ DP/309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -1,85 +1,63 @@
-//Method - 1
-//Top Down
+//Top-Down
+//TC: O(n*2)
+//SC: O(n*2) + Auxilliary Stack Space
 class Solution {
 public:
-    int solve(int day, int buy, int sold, vector<vector<vector<int>>> &dp, vector<int> &prices){
+    int solve(int ind, int flag, vector<int> &prices, vector<vector<int>> &dp){
         
-        if(day >= prices.size()){
+        if(ind >= prices.size()){
             return 0;
         }
         
-        if(dp[day][buy][sold] != -1){
-            return dp[day][buy][sold];
+        if(dp[ind][flag] != -1){
+            return dp[ind][flag];
         }
         
-        int maxProfit = 0;
-        
-        if(buy == 0){
-            for(int i = day; i < prices.size(); i++){
-                maxProfit = max(maxProfit, solve(i, 1, 0, dp, prices));
-            }
-        }
-        else if(sold == 0){
-            for(int i = day + 1; i < prices.size(); i++){
-                maxProfit = max(maxProfit, solve(i + 2, 0, 1, dp, prices) + prices[i] - prices[day]);
-            }
-        }
-        return dp[day][buy][sold] = maxProfit;
-    }
-    int maxProfit(vector<int>& prices) {
-        
-        int n = prices.size();
-        
-        if(n == 1) return 0;
-        
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2, -1)));
-        
-        return solve(0, 0, 0, dp, prices);
-    }
-};
-
-//Method - 2
-//Optimised Top Down
-class Solution {
-public:
-    int solve(int day, int buy, vector<vector<int>> &dp, vector<int> &prices){
-        
-        if(day >= prices.size()){
-            return 0;
-        }
-        
-        if(dp[day][buy] != -1){
-            return dp[day][buy];
-        }
-        
-        int maxProfit = 0;
-        
-        if(buy == 0){
-            for(int i = day; i < prices.size(); i++){
-                maxProfit = max(maxProfit, solve(i, 1, dp, prices));
-            }
+        if(flag){
+            return dp[ind][flag] = max(solve(ind + 1, 0, prices, dp) - prices[ind], solve(ind + 1, 1, prices, dp));
         }
         else{
-            for(int i = day + 1; i < prices.size(); i++){
-                maxProfit = max(maxProfit, solve(i + 2, 0, dp, prices) + prices[i] - prices[day]);
-            }
+            return dp[ind][flag] = max(solve(ind + 2, 1, prices, dp) + prices[ind], solve(ind + 1, 0, prices, dp));
         }
-        return dp[day][buy] = maxProfit;
     }
     int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
         
-        if(n == 1) return 0;
-        
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-        
-        return solve(0, 0, dp, prices);
+        return solve(0, 1, prices, dp);
     }
 };
 
-//Method - 3
-//Bottom Up
+//Bottom-Up
+//TC: O(n*2)
+//SC: O(n*2)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2));
+        
+        for(int ind = n; ind >= 0; ind--){
+            for(int flag = 0; flag <= 1; flag++){
+                if(ind == n){
+                    dp[ind][flag] = 0;
+                }
+                else if(flag){
+                    dp[ind][flag] = max(dp[ind + 1][0] - prices[ind], dp[ind + 1][1]);
+                }
+                else{
+                    int val = (ind + 2 <= n) ? dp[ind + 2][1] : 0;
+                    dp[ind][flag] = max(val + prices[ind], dp[ind + 1][0]);
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
+
+//Intuitive
 //Ref: https://www.youtube.com/watch?v=GY0O57llkKQ&t=13s
 class Solution {
 public:
@@ -124,7 +102,7 @@ public:
     }
 }; 
 
-//Method - 4
+//Intuitive
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -142,61 +120,5 @@ public:
             dp[i][2] = max(dp[i - 1][2], dp[i - 1][1]);
         }
         return dp[n - 1][1];
-    }
-};
-
-//Top-Down 
-class Solution {
-public:
-    int dp[5001][2];
-    int solve(int ind, int flag, vector<int> &prices){
-        
-        if(ind >= prices.size()){
-            return 0;
-        }
-        if(dp[ind][flag] != -1){
-            return dp[ind][flag];
-        }
-        
-        if(flag == 0){
-            int profit = max(solve(ind + 1, 1, prices) - prices[ind], solve(ind + 1, 0, prices));
-            return dp[ind][flag] = profit;
-        }
-        else{
-            int profit = max(solve(ind + 2, 0, prices) + prices[ind], solve(ind + 1, 1, prices));
-            return dp[ind][flag] = profit;
-        }
-    }
-    int maxProfit(vector<int>& prices) {
-        
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, prices);
-    }
-};
-
-//Bottom-Up
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, prices);
-        
-        int n = prices.size();
-        
-        int dp[n + 2][2];
-        memset(dp, 0, sizeof(dp));
-        
-        for(int ind = n - 1; ind >= 0; ind--){
-            for(int flag = 0; flag <= 1; flag++){
-                if(flag == 0){
-                    dp[ind][flag] = max(dp[ind + 1][1] - prices[ind], dp[ind + 1][0]);
-                }
-                else{
-                    dp[ind][flag] = max(solve(ind + 2, 0, prices) + prices[ind], solve(ind + 1, 1, prices));
-                }
-            }
-        }
-        return dp[0][0];
     }
 };
