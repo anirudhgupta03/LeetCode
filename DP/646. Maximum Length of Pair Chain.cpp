55fcb9507ee69872eeaf3cc467dff8d76f4d261f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-10 16:25:11 +0530

    Update 646. Maximum Length of Pair Chain.cpp

--- DP/646. Maximum Length of Pair Chain.cpp
+++ DP/646. Maximum Length of Pair Chain.cpp
@@ -1,3 +1,32 @@
+//DP
+//TC: O(n*n)
+//SC: O(n)
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        
+        sort(pairs.begin(), pairs.end());
+        
+        int n = pairs.size();
+        
+        vector<int> dp(n, 1);
+        int ans = 1;
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(pairs[j][1] < pairs[i][0]){
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
+
+//Greedy
+//TC: O(nlogn)
+//SC: O(1)
 class Solution {
 public:
     bool static cmp(vector<int> &v1, vector<int> &v2){
