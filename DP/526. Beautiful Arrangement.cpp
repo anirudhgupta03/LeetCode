ab8da6d95839f9092a56b71fb00d544b987c5e96
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-10 23:02:59 +0530

    Update 526. Beautiful Arrangement.cpp

--- DP/526. Beautiful Arrangement.cpp
+++ DP/526. Beautiful Arrangement.cpp
@@ -1,3 +1,38 @@
+//Top-Down
+//DP + Bitmasking
+class Solution {
+public:
+    int solve(int ind, int n, int &mask, vector<vector<int>> &dp){
+        
+        if(mask == (1 << n) - 1){
+            return 1;
+        }
+        
+        if(dp[ind][mask] != -1){
+            return dp[ind][mask];
+        }
+        int count = 0;
+        
+        for(int i = 0; i < n; i++){
+            if((mask & (1 << i)) == 0){
+                if(ind % (i + 1) == 0 || (i + 1) % ind == 0){
+                    mask |= (1 << i);
+                    count += solve(ind + 1, n, mask, dp);
+                    mask &= (~(1 << i));
+                }
+            }
+        }
+        return dp[ind][mask] = count;
+    }
+    int countArrangement(int n) {
+        
+        int mask = 0;
+        int temp = (1 << n);
+        vector<vector<int>> dp(n + 1, vector<int>(temp, -1));
+        return solve(1, n, mask, dp);
+    }
+};
+
 //Ref: https://www.youtube.com/watch?v=xf8qAkqDr8Y
 class Solution {
 public:
