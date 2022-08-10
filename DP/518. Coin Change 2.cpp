5185cac335977500df91aa61ca806061c1490805
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-10 12:10:47 +0530

    Update 518. Coin Change 2.cpp

--- DP/518. Coin Change 2.cpp
+++ DP/518. Coin Change 2.cpp
@@ -1,3 +1,38 @@
+//Top-Down
+//TC: O(n*amount)
+//SC: O(n*amount) + Auxilliary Stack Space
+class Solution {
+public:
+    int solve(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp){
+        
+        if(amount == 0){
+            return 1;
+        }
+        if(ind == coins.size()){
+            return 0;
+        }
+        if(dp[ind][amount] != -1){
+            return dp[ind][amount];
+        }
+        int count = 0;
+        if(coins[ind] <= amount){
+            count += solve(ind, amount - coins[ind], coins, dp);
+        }
+        count += solve(ind + 1, amount, coins, dp);
+        return dp[ind][amount] = count;
+    }
+    int change(int amount, vector<int>& coins) {
+        
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+        
+        return solve(0, amount, coins, dp);
+    }
+};
+
+//Bottom-Up
+//TC: O(n*amount)
+//SC: O(n*amount)
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
@@ -26,5 +61,35 @@ public:
             }
         }
         return dp[n][amount];
+    }
+};
+
+//Bottom-Up
+//TC: O(n*amount)
+//SC: O(amount)
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        
+        int n = coins.size();
+        
+        vector<int> pre(amount + 1, 0), curr(amount + 1);
+        pre[0] = 1;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= amount; j++){
+                if(j == 0){
+                    curr[j] = 1;
+                }
+                else if(coins[i - 1] <= j){
+                    curr[j] = curr[j - coins[i - 1]] + pre[j];
+                }
+                else{
+                    curr[j] = pre[j];
+                }
+            }
+            pre = curr;
+        }
+        return curr[amount];
     }
 };
