0ac52143e4660eb4544b4d8f5f5271ba41cb3fa6
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-10 10:50:39 +0530

    Update 494. Target Sum.cpp

--- DP/494. Target Sum.cpp
+++ DP/494. Target Sum.cpp
@@ -1,3 +1,4 @@
+//Intuition
 int findTargetSumWaysTopDown(vector<int>& nums, int S) {
         
 	int n = nums.size();
@@ -53,3 +54,47 @@ int findTargetSumWaysTopDown(vector<int>& nums, int S) {
         */
         return (pow(2, countOfZeroes)*dp[n][s]);
     }
+
+//Method 
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        
+        int zeros = 0, sum = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+        }
+        
+        if(target > sum || target < -sum){
+            return 0;
+        }
+        
+        if((sum + target) % 2 != 0){
+            return 0;
+        }
+        int s = (sum + target)/2;
+        
+        int dp[n + 1][s + 1];
+        
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= s; j++){
+                if(j == 0){
+                    dp[i][j] = 1;
+                }
+                else if(i == 0){
+                    dp[i][j] = 0;
+                }
+                else if(nums[i - 1] == 0 || nums[i - 1] > j){
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][s]*(1 << zeros);
+    }
+};
