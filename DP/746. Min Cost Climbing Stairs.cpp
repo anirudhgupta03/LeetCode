f24415f165abc6afb84b432352ce20a4bc5c009e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-11 11:57:18 +0530

    Update 746. Min Cost Climbing Stairs.cpp

--- DP/746. Min Cost Climbing Stairs.cpp
+++ DP/746. Min Cost Climbing Stairs.cpp
@@ -1,3 +1,44 @@
+//Top-Down
+//TC: O(n)
+//SC: O(n) + Auxilliary Stack Space
+class Solution {
+public:
+    int solve(int ind, vector<int> &cost, vector<int> &dp){
+        
+        if(ind >= cost.size()){
+            return 0;
+        }
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+        return dp[ind] = min(solve(ind + 1, cost, dp) + cost[ind], solve(ind + 2, cost, dp) + cost[ind]);
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int n = cost.size();
+        vector<int> dp(n, -1);
+        return min(solve(0, cost, dp), solve(1, cost, dp));
+    }
+};
+
+//Bottom-Up
+//TC: O(n)
+//SC: O(n)
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+        int n = cost.size();
+        vector<int> dp(n + 2, 0);
+        
+        for(int ind = n - 1; ind >= 0; ind--){
+            dp[ind] = min(dp[ind + 1] + cost[ind], dp[ind + 2] + cost[ind]);
+        }
+        return min(dp[0], dp[1]);
+    }
+};
+
+//Iterative
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
