cf1879071b301a09e69b3e67db30a13c90806435
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2022-08-14 23:17:49 +0530

    Update 1824. Minimum Sideway Jumps.cpp

--- DP/1824. Minimum Sideway Jumps.cpp
+++ DP/1824. Minimum Sideway Jumps.cpp
@@ -50,3 +50,118 @@ public:
         return solve(2,0,obstacles,dp);
     }
 };
+
+//Top-Down
+//TC: O(3*n)
+//SC: O(3*n) + Auxilliary Stack Space
+class Solution {
+public:
+    int solve(int lane, int ind, vector<int> &obstacles, vector<vector<int>> &dp){
+        
+        if(ind == obstacles.size() - 1){
+            return 0;
+        }
+        if(dp[lane][ind] != -1){
+            return dp[lane][ind];
+        }
+        
+        int minSteps = INT_MAX;
+        if(obstacles[ind + 1] != lane){
+            minSteps = min(minSteps, solve(lane, ind + 1, obstacles, dp));
+        }
+        
+        int otherlane1, otherlane2;
+        
+        if(lane == 1){
+            otherlane1 = 2;
+            otherlane2 = 3;
+        }
+        if(lane == 2){
+            otherlane1 = 1;
+            otherlane2 = 3;
+        }
+        if(lane == 3){
+            otherlane1 = 1;
+            otherlane2 = 2;
+        }
+        
+        if(obstacles[ind] != otherlane1 && obstacles[ind + 1] != otherlane1){
+            int temp = solve(otherlane1, ind + 1, obstacles, dp);
+            if(temp != INT_MAX){
+                minSteps = min(minSteps, temp + 1);
+            }
+        }
+        
+        if(obstacles[ind] != otherlane2 && obstacles[ind + 1] != otherlane2){
+            int temp = solve(otherlane2, ind + 1, obstacles, dp);
+            if(temp != INT_MAX){
+                minSteps = min(minSteps, temp + 1);
+            }
+        }
+        return dp[lane][ind] = minSteps;
+    }
+    int minSideJumps(vector<int>& obstacles) {
+        
+        int n = obstacles.size();
+        vector<vector<int>> dp(4, vector<int>(n, -1));
+        return solve(2, 0, obstacles, dp);
+    }
+};
+
+//Bottom-Up
+//TC: O(3*n)
+//SC: O(3*n)
+class Solution {
+public:
+    int minSideJumps(vector<int>& obstacles) {
+        
+        int n = obstacles.size();
+        vector<vector<int>> dp(4, vector<int>(n));
+        
+        for(int ind = n - 1; ind >= 0; ind--){
+            for(int lane = 1; lane <= 3; lane++){
+                if(ind == n - 1){
+                    dp[lane][ind] = 0;
+                }
+                else{
+                    int minSteps = INT_MAX;
+
+                    if(obstacles[ind + 1] != lane){
+                        minSteps = min(minSteps, dp[lane][ind + 1]);
+                    }
+
+                    int otherlane1, otherlane2;
+
+                    if(lane == 1){
+                        otherlane1 = 2;
+                        otherlane2 = 3;
+                    }
+                    if(lane == 2){
+                        otherlane1 = 1;
+                        otherlane2 = 3;
+                    }
+                    if(lane == 3){
+                        otherlane1 = 1;
+                        otherlane2 = 2;
+                    }
+
+                    if(obstacles[ind] != otherlane1 && obstacles[ind + 1] != otherlane1){
+                        int temp = dp[otherlane1][ind + 1];
+                        if(temp != INT_MAX){
+                            minSteps = min(minSteps, temp + 1);
+                        }
+                    }
+
+                    if(obstacles[ind] != otherlane2 && obstacles[ind + 1] != otherlane2){
+                        int temp = dp[otherlane2][ind + 1];
+                        if(temp != INT_MAX){
+                            minSteps = min(minSteps, temp + 1);
+                        }
+                    }
+                    dp[lane][ind] = minSteps;
+                }
+            }
+        }
+        return dp[2][0];
+    }
+};
