9b0d3da51054667dcb39b71f9fe29d3bf858b20e
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-23 00:05:34 +0530

    Update 125. Valid Palindrome.cpp

--- String/125. Valid Palindrome.cpp
+++ String/125. Valid Palindrome.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     bool isPalindrome(string s) {
@@ -23,6 +24,32 @@ public:
             lo++;hi--;
         }
         
+        return true;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    bool isAlphaNumeric(char ch){
+        if(ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch >= '0' && ch <= '9'){
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int lo = 0, hi = s.length() - 1;
+        while(lo < hi){
+            while(lo < hi && !isAlphaNumeric(s[lo])) lo++;
+            while(lo < hi && !isAlphaNumeric(s[hi])) hi--;
+            if(lo < hi){
+                if(tolower(s[lo]) != tolower(s[hi])){
+                    return false;
+                }
+                lo++;
+                hi--;
+            }
+        }
         return true;
     }
 };
