632a153ba34a01504a5a935bcf20bbe2ee876fd3
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-26 15:02:26 +0530

    Update 189. Rotate Array.cpp

--- Arrays/189. Rotate Array.cpp
+++ Arrays/189. Rotate Array.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
         int n = nums.size();
-        
-        reverse(nums.begin(),nums.begin() + n - k % n);
-        
-        reverse(nums.begin() + n - k % n, nums.end());
-        
-        reverse(nums.begin(),nums.end());
+        k = k % n;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
