ce8f1a13cb344b5c1285e6dc3bcd4301cc945682
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-26 18:16:10 +0530

    Create 136. Single Number.cpp

---
+++ Arrays/136. Single Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans = 0;
+
+        for(int num: nums){
+            ans = ans^num;
+        }
+
+        return ans;
+    }
+};
