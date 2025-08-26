a635df2e0e13f6bf16fcaee90511200eeb4cc733
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-26 14:23:06 +0530

    Update 498. Diagonal Traverse.cpp

--- Arrays/498. Diagonal Traverse.cpp
+++ Arrays/498. Diagonal Traverse.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        
         int m = mat.size(), n = mat[0].size();
         int dir = 0, i = 0, j = 0;
         vector<int> res;
 
         while(res.size() != m*n){
-
             if(dir == 0){
                 while(i >= 0 && j < n){
                     res.push_back(mat[i][j]);
                     i--;
                     j++;
                 }
-                if(i < 0 && j >= n){
+                if(j >= n){
                     i = i + 2;
                     j = n - 1;
                 }
                 else if(i < 0){
                     i = 0;
-                }
-                else if(j >= n){
-                    i = i + 2;
-                    j = n - 1;
                 }
             }
             else{
@@ -32,16 +26,12 @@ public:
                     i++;
                     j--;
                 }
-                if(i >= m && j < 0){
+                if(i >= m){
                     j = j + 2;
                     i = m - 1;
                 }
                 else if(j < 0){
                     j = 0;
-                }
-                else if(i >= m){
-                    j = j + 2;
-                    i = m - 1;
                 }
             }
             dir = !dir;
