8c729342f1ef188f189e2542dc676f033ae61a0a
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-26 15:50:40 +0530

    Update 88. Merge Sorted Array.cpp

--- Arrays/88. Merge Sorted Array.cpp
+++ Arrays/88. Merge Sorted Array.cpp
@@ -1,3 +1,6 @@
+//Method - 1
+//TC: O(M + N)
+//SC: O(M + N)
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -29,5 +32,26 @@ public:
         
         nums1.clear();
         nums1 = res;
+    }
+};
+
+//Method - 2
+//Ref: https://www.youtube.com/watch?v=C4oBXLr3zos
+//TC: O(M + N)
+//SC: O(1)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
     }
 };
