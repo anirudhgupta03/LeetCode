3e51127b5d0a425d2c28f0ea77e89165e3f1b8b9
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-30 20:21:33 +0530

    Update 329. Longest Increasing Path in a Matrix.cpp

--- DP/329. Longest Increasing Path in a Matrix.cpp
+++ DP/329. Longest Increasing Path in a Matrix.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 class Solution {
 public:
     int dx[4] = {0,0,-1,1};
@@ -34,5 +35,64 @@ public:
             }
         }
         return lip;
+    }
+};
+
+//Method - 2
+//Using Topological Sorting
+//Ref: https://www.youtube.com/watch?v=aN5EZa6cypU
+#define pii pair<int,int>
+class Solution {
+public:
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size(), n = matrix[0].size();
+
+        vector<vector<int>> indegree(m, vector<int>(n,0));
+        vector<vector<vector<pii>>> al(m, vector<vector<pii>>(n));
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int di = -1, dj = -1;
+                for(int k = 0; k < 4; k++){
+                    di = i + dx[k];
+                    dj = j + dy[k];
+                    if(di >= 0 && dj >= 0 && di < m && dj < n && matrix[di][dj] > matrix[i][j]){
+                        al[i][j].push_back({di, dj});
+                        indegree[di][dj]++;
+                    }
+                }
+            }
+        }
+
+        queue<pii> q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(indegree[i][j] == 0){
+                    q.push({i, j});
+                }
+            }
+        }
+
+        vector<pair<int,int>> path;
+        int ans = 0;
+        while(!q.empty()){
+            int sz = q.size();
+            ans++;
+            while(sz--){
+                pii p = q.front();
+                q.pop();
+                vector<pii> tp = al[p.first][p.second];
+                for(int i = 0; i < tp.size(); i++){
+                    indegree[tp[i].first][tp[i].second]--;
+                    if(indegree[tp[i].first][tp[i].second] == 0){
+                        q.push({tp[i].first, tp[i].second});
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
