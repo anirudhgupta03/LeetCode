047a7843f148387a27abeca2ecd40f332472adb2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-30 09:09:01 +0530

    Update 75. Sort Colors.cpp

--- Arrays/75. Sort Colors.cpp
+++ Arrays/75. Sort Colors.cpp
@@ -1,7 +1,11 @@
-//Ref: https://www.youtube.com/watch?v=tp8JIuCXBaU&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB
+//Ref: //https://youtu.be/tp8JIuCXBaU?list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&t=451
 //Optimal Solution
 //Dutch National Flag Algorithm
-//https://youtu.be/tp8JIuCXBaU?list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&t=451
+//                                             Unsorted
+//0......0             1.........1          ...............          2..........2
+//0      lo-1          lo        mid - 1    mid(0/1/2)    hi         hi + 1     n - 1
+//TC: O(N)
+//SC: O(1)
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
