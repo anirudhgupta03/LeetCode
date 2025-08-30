53f3466788b3e7d3582df578c57633a218d18d33
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-08-30 08:43:23 +0530

    Update 75. Sort Colors.cpp

--- Arrays/75. Sort Colors.cpp
+++ Arrays/75. Sort Colors.cpp
@@ -1,22 +1,25 @@
+//Ref: https://www.youtube.com/watch?v=tp8JIuCXBaU&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB
+//Optimal Solution
+//Dutch National Flag Algorithm
+//00000...
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         
-        int lo = 0, mid = 0, hi = nums.size() - 1;
-        
-        while(mid <= hi){
-            
-            if(nums[mid] == 0){
-                swap(nums[mid],nums[lo]);
-                mid++;
-                lo++;
+        int n = nums.size();
+        int i = 0, j = n - 1, k = 0;
+        while(k <= j){
+            if(nums[k] == 0){
+                swap(nums[i], nums[k]);
+                i++;
+                k++;
             }
-            else if(nums[mid] == 2){
-                swap(nums[mid],nums[hi]);
-                hi--;
+            else if(nums[k] == 2){
+                swap(nums[j], nums[k]);
+                j--;
             }
             else{
-                mid++;
+                k++;
             }
         }
     }
