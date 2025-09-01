ad9e0d9ddc45751d78faf6447dc2d441e2b927a2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-01 13:17:19 +0530

    Update 128. Longest Consecutive Sequence.cpp

--- Arrays/128. Longest Consecutive Sequence.cpp
+++ Arrays/128. Longest Consecutive Sequence.cpp
@@ -1,3 +1,32 @@
+//Ref: https://www.youtube.com/watch?v=oO5uLE7EUlM&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&index=13
+//Method - 1
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0){
+            return 0;
+        }
+        unordered_set<int> us;
+        for(auto &x: nums){
+            us.insert(x);
+        }
+        int maxLen = 1;
+        for(auto it: us){
+            if(us.find(it - 1) == us.end()){
+                int currLen = 1;
+                int currNum = it;
+                while(us.find(currNum + 1) != us.end()){
+                    currLen++;
+                    currNum++;
+                }
+                maxLen = max(maxLen,currLen);
+            }
+        }
+        return maxLen;
+    }
+};
+
+//Method - 2
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
