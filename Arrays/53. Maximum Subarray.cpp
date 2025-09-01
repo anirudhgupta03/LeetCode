f8c1b45d57f6b63bb65ac3c5c49f2e526085ec21
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-01 09:30:01 +0530

    Update 53. Maximum Subarray.cpp

--- Arrays/53. Maximum Subarray.cpp
+++ Arrays/53. Maximum Subarray.cpp
@@ -1,10 +1,9 @@
+//Ref: https://www.youtube.com/watch?v=AHZpyENo7k4&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&index=8
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
         int n = nums.size();
         int sum = 0, maxSum = INT_MIN;
-
         for(int ele: nums){
             sum += ele;
             maxSum = max(maxSum, sum);
