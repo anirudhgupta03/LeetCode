fcf90750cc7dbb5de05e30c8d62f1aa4211eedaa
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-01 09:46:11 +0530

    Update 2149. Rearrange Array Elements by Sign.cpp

--- Arrays/2149. Rearrange Array Elements by Sign.cpp
+++ Arrays/2149. Rearrange Array Elements by Sign.cpp
@@ -1,12 +1,10 @@
+//Ref: https://www.youtube.com/watch?v=h4aBagy4Uok&list=PLgUwDviBIf0rENwdL0nEH0uGom9no0nyB&index=9
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        
         int n = nums.size();
         vector<int> ans(n, 0);
-
         int indexpos = 0, indexneg = 1;
-
         for(int ele: nums){
             if(ele > 0){
                 ans[indexpos] = ele;
