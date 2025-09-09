992860a917ffb52a931e73dc9ab5947bbb8fa761
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-09 11:07:15 +0530

    Update 704. Binary Search.cpp

--- Binary Search/704. Binary Search.cpp
+++ Binary Search/704. Binary Search.cpp
@@ -1,4 +1,5 @@
 //Ref: https://www.youtube.com/watch?v=MHf6awe89xw&list=PLF6ChxadzFf8vjafLIxxbKUfarW4V4IOh
+//Iterative
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -19,5 +20,20 @@ public:
             }
         }
         return -1;
+    }
+};
+
+//Recursive
+class Solution {
+public:
+    int binarySearch(int lo, int hi,vector<int> &nums, int target){
+        if(lo > hi) return -1;
+        int mid = lo + (hi - lo)/2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] > target) return binarySearch(lo, mid - 1, nums, target);
+        return binarySearch(mid + 1, hi, nums, target);
+    }
+    int search(vector<int>& nums, int target) {
+        return binarySearch(0, nums.size() - 1, nums, target);
     }
 };
