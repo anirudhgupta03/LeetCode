b29bccdf852a4e254e6015874ff262535b5622f4
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-11 11:30:37 +0530

    Update 1482. Minimum Number of Days to Make m Bouquets.cpp

--- Binary Search/1482. Minimum Number of Days to Make m Bouquets.cpp
+++ Binary Search/1482. Minimum Number of Days to Make m Bouquets.cpp
@@ -1,16 +1,12 @@
+//Ref: https://www.youtube.com/watch?v=TXAuxeYBTdg&list=PLF6ChxadzFf8vjafLIxxbKUfarW4V4IOh&index=13
 //Excellent Question
 //Method - 1
 class Solution {
 public:
-    
     bool isPossible(int day, vector<int> &bloomDay, int m, int k){
-        
         int count = 0;
-        int ans = INT_MIN;
         int b_count = 0;
-        
         for(int i = 0; i < bloomDay.size(); i++){
-            
             if(bloomDay[i] <= day){
                 count++;
             }
@@ -21,7 +17,6 @@ public:
                 count = 0;
             }
         }
-        
         if(count >= k){
             b_count += count/k;
         }
@@ -31,18 +26,13 @@ public:
         return false;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        
         int lo = INT_MAX, hi = INT_MIN;
-        
         for(int i = 0; i < bloomDay.size(); i++){
             lo = min(lo,bloomDay[i]);
             hi = max(hi,bloomDay[i]);
         }
-        
         int ans = -1;
-        
         while(lo <= hi){
-            
             int mid = (lo + hi)/2;
             if(isPossible(mid,bloomDay,m,k)){
                 hi = mid - 1;
@@ -58,58 +48,45 @@ public:
 
 //Method - 2
 //Optimal Method 
+#define ll long long
 class Solution {
 public:
-    #define ll long long
-    int numberofBouquets(int days, vector<int> &bloomDay, int k){
-        
-        ll count = 0, temp = 0;
-        
+    bool isPossibleToMakeMBouquets(int day, vector<int>& bloomDay, int m, int k){
+        int ans = 0, count = 0;
         for(int i = 0; i < bloomDay.size(); i++){
-            
-            if(bloomDay[i] <= days){
-                temp++;
+            if(bloomDay[i] <= day){
+                count++;
             }
             else{
-                temp = 0;
+                count = 0;
             }
-            
-            if(temp == k){
-                count++;
-                temp = 0;
+            if(count == k){
+                count = 0;
+                ans++;
             }
         }
-        return count;
+        return ans >= m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        
-        int n = bloomDay.size();
-        
-        if(k*m > n){
+        if((ll)m*k > bloomDay.size()){
             return -1;
         }
-        
-        int lo = 1, hi = INT_MIN;
-        
-        for(int i = 0; i < bloomDay.size(); i++){
-            hi = max(hi, bloomDay[i]);
-        }   
-        
+        int lo = INT_MAX, hi = INT_MIN;
+        for(int &day: bloomDay){
+            lo = min(lo, day);
+            hi = max(hi, day);
+        }
         int ans = -1;
-        
         while(lo <= hi){
-            
-            int mid = (lo + hi)/2;
-            int c = numberofBouquets(mid,bloomDay,k);
-                                     
-            if(c >= m){
+            int mid = lo + (hi - lo)/2;
+            if(isPossibleToMakeMBouquets(mid, bloomDay, m, k)){
                 ans = mid;
                 hi = mid - 1;
             }
             else{
                 lo = mid + 1;
             }
-        }   
+        }
         return ans;
     }
 };
