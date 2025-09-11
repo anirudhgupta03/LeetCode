9bad7ec80cde97f0e3f60c62ff63accd6c3e45ed
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-11 11:52:57 +0530

    Update 1011. Capacity To Ship Packages Within D Days.cpp

--- Binary Search/1011. Capacity To Ship Packages Within D Days.cpp
+++ Binary Search/1011. Capacity To Ship Packages Within D Days.cpp
@@ -1,44 +1,35 @@
+//Ref: https://www.youtube.com/watch?v=MG-Ac4TAvTY&list=PLF6ChxadzFf8vjafLIxxbKUfarW4V4IOh&index=15
 class Solution {
 public:
-    int daysTaken(int cap, vector<int> &weights){
-        
-        int count = 1;
-        
-        int sum = 0;
-        
-        for(int i = 0; i < weights.size(); i++){
-            
-            sum += weights[i];
-            if(sum > cap){
-                count++;
-                sum = weights[i];
+    bool isPossibleToShipPackagesWithinDDays(int shipCapacity, vector<int>& weights, int days){
+        int daysTaken = 1, cap = 0;
+        for(int &weight: weights){
+            cap += weight;
+            if(cap > shipCapacity){
+                daysTaken++;
+                cap = weight;
             }
         }
-        return count;
+        return daysTaken <= days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        
+        int n = weights.size();
         int lo = INT_MIN, hi = 0;
-        
-        for(int i = 0; i < weights.size(); i++){
-            hi += weights[i];
-            lo = max(lo,weights[i]);
+        for(int &weight: weights){
+            lo = max(lo, weight);
+            hi += weight;
         }
-        
         int ans;
         while(lo <= hi){
-            
-            int mid = (lo + hi)/2;
-            
-            if(daysTaken(mid,weights) <= days){
-                ans = mid;
-                hi = mid - 1;
+            int shipCapacity = lo + (hi - lo)/2;
+            if(isPossibleToShipPackagesWithinDDays(shipCapacity, weights, days)){
+                ans = shipCapacity;
+                hi = shipCapacity - 1;
             }
             else{
-                lo = mid + 1;
+                lo = shipCapacity + 1;
             }
         }
-        
         return ans;
     }
 };
