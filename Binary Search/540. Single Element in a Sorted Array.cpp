c51a98a8c4479d709e935b3f935a78fab4d5b4e2
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-11 09:29:58 +0530

    Update 540. Single Element in a Sorted Array.cpp

--- Binary Search/540. Single Element in a Sorted Array.cpp
+++ Binary Search/540. Single Element in a Sorted Array.cpp
@@ -1,87 +1,29 @@
-//Method - 1
+//Ref: https://www.youtube.com/watch?v=AZOmHuHadxQ&list=PLF6ChxadzFf8vjafLIxxbKUfarW4V4IOh&index=8
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         
         int n = nums.size();
-        
-        if(n == 1){
-            return nums[0];
-        }
-        
         int lo = 0, hi = n - 1;
-        
-        while(lo <= hi){
-            
-            int mid = (lo + hi)/2;
-            
-            if(mid % 2 == 0){
-                if(mid == 0){
-                    if(nums[mid] != nums[mid+1]){
-                        return nums[mid];
-                    }
-                }
-                else if(mid == n - 1){
-                    if(nums[mid] != nums[mid-1]){
-                        return nums[mid];
-                    }
-                }
-                else if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid + 1]){
-                    return nums[mid];
-                }
-                else if(nums[mid] == nums[mid-1]){
-                    hi = mid - 1;
-                }
-                else{
-                    lo = mid + 1;
-                }
-            }
-            else{
-                if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid + 1]){
-                    return nums[mid];
-                }
-                else if(nums[mid] == nums[mid - 1]){
-                    lo = mid + 1;
-                }
-                else{
-                    hi = mid - 1;
-                }
-            }
-        }
-        return 0;
-    }
-};
 
-//Method - 2
-class Solution {
-public:
-    int singleNonDuplicate(vector<int>& nums) {
-        
-        if(nums.size() == 1) return nums[0];
-        
-        int n = nums.size();
-        
-        int lo = 0, hi = nums.size() - 1;
-        
         while(lo <= hi){
-            
             int mid = lo + (hi - lo)/2;
-            
-            if(mid == 0 && nums[mid] != nums[mid + 1]) return nums[mid];
-            
-            else if(mid == n - 1 && nums[mid] != nums[mid - 1]) return nums[mid];
-            
-            else if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
-            
-            else if(mid % 2 == 0){
-                if(nums[mid] == nums[mid - 1]) hi = mid - 1;
-                else lo = mid + 1;
+            int preEle = (mid - 1 >= 0) ? nums[mid - 1] : -1;
+            int nextEle = (mid + 1 < n) ? nums[mid + 1] : -1;
+            if(nums[mid] != preEle && nums[mid] != nextEle){
+                return nums[mid];
             }
             else{
-                if(nums[mid] == nums[mid - 1]) lo = mid + 1;
-                else hi = mid - 1;
-            }
+                if(nums[mid] == preEle){
+                    if(mid % 2 == 0) hi = mid - 1;
+                    else lo = mid + 1;
+                }
+                else{
+                    if(mid % 2 == 0) lo = mid + 1;
+                    else hi = mid - 1;
+                }
+            } 
         }
-        return 0;
+        return -1;
     }
 };
