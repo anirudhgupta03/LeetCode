15d1873df2f4d93cbdc40a6ac193eaceb82a4b31
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-11 10:44:39 +0530

    Update 875. Koko Eating Bananas.cpp

--- Binary Search/875. Koko Eating Bananas.cpp
+++ Binary Search/875. Koko Eating Bananas.cpp
@@ -1,3 +1,4 @@
+//Ref: https://www.youtube.com/watch?v=qyfekrNni90&list=PLF6ChxadzFf8vjafLIxxbKUfarW4V4IOh&index=12
 class Solution {
 public:
     #define ll long long
@@ -12,10 +13,10 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         
         int lo = 1, hi = INT_MIN;
-        
         for(int i = 0; i < piles.size(); i++){
             hi = max(hi,piles[i]);
         }
+        
         int ans;
         while(lo <=  hi){
             int mid = (lo + hi)/2;
