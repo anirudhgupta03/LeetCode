cf54f86ed67669fb7e636b0061e00afc47d8684a
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-09-27 12:52:05 +0530

    Update 328. Odd Even Linked List.cpp

--- Linked List/328. Odd Even Linked List.cpp
+++ Linked List/328. Odd Even Linked List.cpp
@@ -1,4 +1,41 @@
 //Ref: https://www.youtube.com/watch?v=qf6qp7GzD5Q&list=PLgUwDviBIf0rAuz8tVcM0AymmhTRsfaLU&index=7
+
+//Brute Force Solution
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    void fetchElements(ListNode* head, vector<int> &arr){
+        while(head != NULL && head -> next != NULL){
+            arr.push_back(head -> val);
+            head = head -> next -> next;
+        }
+        if(head) arr.push_back(head -> val);
+    }
+    ListNode* oddEvenList(ListNode* head) {
+        vector<int> arr;
+        fetchElements(head, arr);
+        fetchElements(head -> next, arr);
+        ListNode* ptr = head;
+        int ind = 0;
+        while(ptr != NULL){
+            ptr -> val = arr[ind];
+            ptr = ptr -> next;
+            ind++;
+        }
+        return head;
+    }
+};
+
+//Optimal Solution
 /**
  * Definition for singly-linked list.
  * struct ListNode {
