7476bdf52f1caa7d43263dcdbbb6dbbb58f0534c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-09 18:11:06 +0530

    Update 994. Rotting Oranges.cpp

--- Graph Theory/994. Rotting Oranges.cpp
+++ Graph Theory/994. Rotting Oranges.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 //Ref: https://www.youtube.com/watch?v=Dq3dGS_0Z6o
 class Solution {
 public:
@@ -53,5 +54,56 @@ public:
             }
         }
         return minTime - 1;
+    }
+};
+
+//Method - 2
+//Ref: https://www.youtube.com/watch?v=yf3oUhkvqA0&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=10
+class Solution {
+public:
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+
+        int freshOranges = 0;
+
+        vector<vector<int>> vis = grid;
+
+        queue<pair<pair<int,int>,int>> q;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 2){
+                    q.push({{i, j},0});
+                }
+                else if(grid[i][j] == 1){
+                    freshOranges++;
+                }
+            }
+        }
+        if(freshOranges == 0) return 0;
+
+        int ans = INT_MIN;
+        while(!q.empty()){
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            ans = max(ans, t);
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == 1 && vis[nx][ny] == 1){
+                    q.push({{nx, ny}, t + 1});
+                    vis[nx][ny] = 2;
+                    freshOranges--;
+                }
+            }
+        }
+        return freshOranges == 0 ? ans : -1;
     }
 };
