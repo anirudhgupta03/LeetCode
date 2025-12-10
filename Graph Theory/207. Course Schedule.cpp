4ca60a5f04a4bc5323cf4bba4da6a6bd05da2c70
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-10 18:51:06 +0530

    Update 207. Course Schedule.cpp

--- Graph Theory/207. Course Schedule.cpp
+++ Graph Theory/207. Course Schedule.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        vector<int> res;
-        
-        vector<int> in(numCourses,0);
-        
-        vector<int> al[numCourses];
-        
-        for(int i = 0; i < prerequisites.size(); i++){
-            
-            int a = prerequisites[i][0], b = prerequisites[i][1]; 
-            al[a].push_back(b);
-            in[b]++;
+        vector<int> adj[numCourses];
+        vector<int> indegree(numCourses, 0);
+        for(auto &prerequisite: prerequisites){
+            adj[prerequisite[1]].push_back(prerequisite[0]);
+            indegree[prerequisite[0]]++;
         }
-        
         queue<int> q;
         for(int i = 0; i < numCourses; i++){
-            if(in[i] == 0){
+            if(indegree[i] == 0){
                 q.push(i);
             }
         }
-        
+        int coursesFinished = 0;
         while(!q.empty()){
-            
             int curr = q.front();
             q.pop();
-            res.push_back(curr);
-            
-            for(int i = 0; i < al[curr].size(); i++){
-                in[al[curr][i]]--;
-                if(in[al[curr][i]] == 0){
-                    q.push(al[curr][i]);
+            coursesFinished++;
+            for(auto &adjNode: adj[curr]){
+                indegree[adjNode]--;
+                if(indegree[adjNode] == 0){
+                    q.push(adjNode);
                 }
             }
         }
-        
-        if(res.size() == numCourses){
-            return true;
-        }
-        return false;
+        return coursesFinished == numCourses;
     }
 };
