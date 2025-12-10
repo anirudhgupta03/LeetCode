46cf8c70b616ff73de4f1600793bc460e6147c61
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-10 07:35:25 +0530

    Update 130. Surrounded Regions.cpp

--- Graph Theory/130. Surrounded Regions.cpp
+++ Graph Theory/130. Surrounded Regions.cpp
@@ -2,42 +2,37 @@ class Solution {
 public:
     int dx[4] = {-1,1,0,0};
     int dy[4] = {0,0,-1,1};
-    void change(int x, int y, vector<vector<char>> &board){
-        
-        board[x][y] = '*';
-        for(int i = 0; i < 4; i++){
-            int xo = x + dx[i], yo = y + dy[i];
-            
-            if(xo >= 0 && xo < board.size() && yo >= 0 && yo < board[0].size() && board[xo][yo] == 'O'){
-                change(xo,yo,board);
-            }
-        }
-    }
     void solve(vector<vector<char>>& board) {
-        
         int m = board.size(), n = board[0].size();
-        
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        queue<pair<int,int>> q;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(i == 0 || i == m - 1 || j == 0 || j == n - 1){
                     if(board[i][j] == 'O'){
-                        change(i,j,board);
+                        q.push({i, j});
+                        vis[i][j] = 1;
                     }
                 }
             }
         }
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == 'O'){
-                    board[i][j] = 'X';
+        while(!q.empty()){
+            auto[x,y] = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i], ny = y + dy[i];
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n){
+                    if(board[nx][ny] == 'O' && vis[nx][ny] == 0){
+                        vis[nx][ny] = 1;
+                        q.push({nx, ny});
+                    }
                 }
             }
         }
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(board[i][j] == '*'){
-                    board[i][j] = 'O';
+                if(board[i][j] == 'O' && vis[i][j] == 0){
+                    board[i][j] = 'X';
                 }
             }
         }
