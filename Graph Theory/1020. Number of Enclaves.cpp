b052828e24300fe6b6ff2a8784300258e57f54af
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-10 07:55:05 +0530

    Update 1020. Number of Enclaves.cpp

--- Graph Theory/1020. Number of Enclaves.cpp
+++ Graph Theory/1020. Number of Enclaves.cpp
@@ -1,3 +1,47 @@
+//BFS Solution
+class Solution {
+public:
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    int numEnclaves(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        queue<pair<int,int>> q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || i == m - 1 || j == 0 || j == n - 1){
+                    if(board[i][j] == 1){
+                        q.push({i, j});
+                        vis[i][j] = 1;
+                    }
+                }
+            }
+        }
+        while(!q.empty()){
+            auto[x,y] = q.front();
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int nx = x + dx[i], ny = y + dy[i];
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n){
+                    if(board[nx][ny] == 1 && vis[nx][ny] == 0){
+                        vis[nx][ny] = 1;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 1 && vis[i][j] == 0){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 //Method - 1
 class Solution {
 public:
