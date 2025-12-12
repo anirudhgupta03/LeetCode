ced7334cc8735ea22afd66889959aaeabbcb3728
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-12 07:04:01 +0530

    Update 787. Cheapest Flights Within K Stops.cpp

--- Graph Theory/787. Cheapest Flights Within K Stops.cpp
+++ Graph Theory/787. Cheapest Flights Within K Stops.cpp
@@ -27,96 +27,41 @@ public:
 
 //Method - 2
 //Using Dijkstra's Algorithm
-class Solution {
-public:
-    #define pipii pair<int,pair<int,int>>
-    struct cmp{
-      bool operator()(pipii &p1, pipii &p2){
-          return p1.second.second > p2.second.second;
-      }  
-    };
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        
-        vector<pair<int,int>> al[n];
-        
-        for(auto &x: flights){
-            al[x[0]].push_back({x[1], x[2]});
-        }
-        
-        priority_queue<pipii, vector<pipii>, cmp> pq;
-        pq.push({src,{-1,0}});
-        
-        set<pipii> vis;
-        
-        while(!pq.empty()){
-            
-            int curr = pq.top().first, stops = pq.top().second.first, cost = pq.top().second.second;
-            pq.pop();
-            
-            if(curr == dst){
-                return cost;
-            }
-            
-            if(vis.find({curr,{stops, cost}}) != vis.end()) continue;
-            vis.insert({curr,{stops, cost}});
-            
-            for(int i = 0; i < al[curr].size(); i++){
-                int child = al[curr][i].first;
-                
-                if(stops + 1 <= k){
-                    pq.push({child,{stops + 1, cost + al[curr][i].second}});
-                }
-            }
-        }
-        return -1;
+#define ppi pair<pair<int,int>,int>
+struct cmp{
+    bool operator()(ppi &p1, ppi &p2){
+        return p1.first.first > p2.first.first;
     }
 };
-
-//Method - 3
-//Using Dijkstra's Algorithm
 class Solution {
 public:
-    #define pipii pair<int,pair<int,int>>
-    struct cmp{
-      bool operator()(pipii &p1, pipii &p2){
-          return p1.second.first > p2.second.first;
-      }  
-    };
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         
-        vector<pair<int,int>> al[n];
-        
-        for(auto &x: flights){
-            al[x[0]].push_back({x[1], x[2]});
+        vector<pair<int,int>> adj[n];
+
+        for(auto &flight: flights){
+            int source = flight[0], destination = flight[1], price = flight[2];
+            adj[source].push_back({destination, price});
         }
-        
-        priority_queue<pipii, vector<pipii>, cmp> pq;
-        
-        vector<vector<int>> vis(n, vector<int>(n + 1, 0));
-        
-        pq.push({src,{0, -1}});
-        
+
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        priority_queue<ppi, vector<ppi>, cmp> pq;
+        pq.push({{-1, 0},src});
+
         while(!pq.empty()){
-            
-            int curr = pq.top().first, cost = pq.top().second.first, moves = pq.top().second.second;
+            int stops = pq.top().first.first;
+            int price = pq.top().first.second;
+            int place = pq.top().second;
             pq.pop();
-            
-            if(curr == dst){
-                return cost;
-            }
-            
-            if(vis[curr][moves + 1]) continue;
-            vis[curr][moves + 1] = 1;
-            
-            for(auto &x: al[curr]){
-                if(moves + 1 == k && x.first == dst){
-                    pq.push({x.first, {cost + x.second, moves + 1}});
-                }
-                else if(moves + 1 < k){
-                    pq.push({x.first,{cost + x.second, moves + 1}});
+            for(auto &adjPlace: adj[place]){
+                if(price + adjPlace.second < dist[adjPlace.first] && (stops + 1 <= k)){
+                    dist[adjPlace.first] = price + adjPlace.second;
+                    pq.push({{stops + 1, dist[adjPlace.first]}, adjPlace.first});
                 }
             }
         }
-        return -1;
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
 };
