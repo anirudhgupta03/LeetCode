6a0cadc12ec94b178cbd2eea3619e3aa5e5329eb
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-12 14:53:18 +0530

    Update 1976. Number of Ways to Arrive at Destination.cpp

--- Graph Theory/1976. Number of Ways to Arrive at Destination.cpp
+++ Graph Theory/1976. Number of Ways to Arrive at Destination.cpp
@@ -1,60 +1,44 @@
 //Dijkstra's Algo
 //Ref: https://www.youtube.com/watch?v=rBmg3dg_MVc
+#define ll long long
+#define pll pair<ll,ll>
 class Solution {
 public:
-    #define ll long long
-    #define pll pair<ll,ll>
-    struct cmp{
-        bool operator()(pll p1, pll p2){
-            return p1.second > p2.second;
-        }
-    };
     int countPaths(int n, vector<vector<int>>& roads) {
         
-        vector<pair<int,int>> al[n];
-        
-        for(int i = 0; i < roads.size(); i++){
-            int ui = roads[i][0], vi = roads[i][1], timei = roads[i][2];
-            al[ui].push_back({vi, timei});
-            al[vi].push_back({ui, timei});
-        }
-        
         int mod = 1e9 + 7;
-        
-        vector<ll> timetaken(n, LLONG_MAX), ways(n, 0), vis(n, 0);
-        
+
+        vector<pair<int,int>> adj[n];
+
+        for(auto &road: roads){
+            adj[road[0]].push_back({road[1], road[2]});
+            adj[road[1]].push_back({road[0], road[2]});
+        }
+
+        priority_queue<pll, vector<pll>, greater<pll>> pq;
+        pq.push({0, 0});
+
+        vector<bool> vis(n, false);
+        vector<ll> dist(n, LLONG_MAX);
+        vector<ll> ways(n, 0);
+
         ways[0] = 1;
-        priority_queue<pll,vector<pll>,cmp> pq;
-        pq.push({0,0});
-        
+        dist[0] = 0;
         while(!pq.empty()){
-            
-            pll p = pq.top();
+            auto[t, city] = pq.top();
             pq.pop();
-            
-            ll curr = p.first, temptime = p.second;
-            
-            if(vis[curr]){
-                continue;
-            }
-            vis[curr] = 1;
-            
-            if(curr == n - 1){
-                return ways[curr];
-            }
-            
-            for(int i = 0; i < al[curr].size(); i++){
-                int child = al[curr][i].first;
-                if(vis[child] == 0){
-                    if(temptime + al[curr][i].second < timetaken[child]){
-                        timetaken[child] = temptime + al[curr][i].second;
-                        ways[child] = ways[curr];
-                        pq.push({child, timetaken[child]});
-                    }
-                    else if(temptime + al[curr][i].second == timetaken[child]){
-                        ways[child] = (ways[child] + ways[curr])%mod;
-                        
-                    }
+
+            if(vis[city]) continue;
+            vis[city] = true;
+
+            for(auto &[ncity, timei]: adj[city]){
+                if(t + timei < dist[ncity]){
+                    dist[ncity] = t + timei;
+                    ways[ncity] = ways[city];
+                    pq.push({dist[ncity], ncity});
+                }
+                else if(t + timei == dist[ncity]){
+                    ways[ncity] = (ways[ncity] + ways[city])%mod;
                 }
             }
         }
