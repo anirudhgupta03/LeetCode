ee06cbc56ed2db8559f2e941aac36f45fe68a401
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-13 11:52:39 +0530

    Update 721. Accounts Merge.cpp

--- Graph Theory/721. Accounts Merge.cpp
+++ Graph Theory/721. Accounts Merge.cpp
@@ -1,116 +1,45 @@
-//Method - 1
-//Iterative
 class Solution {
 public:
+    int findPar(int node, vector<int>& par){
+        if(par[node] == node){
+            return node;
+        }
+        return par[node] = findPar(par[node], par);
+    }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        
         int n = accounts.size();
-        
-        vector<int> visited(n, 0);
-        
-        vector<vector<string>> res;
-        
-        vector<unordered_map<string,int>> help(n);
-        
-        for(int i = 0; i < accounts.size(); i++){
-            for(int j = 1; j < accounts[i].size(); j++){
-                help[i][accounts[i][j]] = 1;
-            }
-        }
-        
+        unordered_map<string,int> umap;
+        vector<int> par(n, -1);
         for(int i = 0; i < n; i++){
-            if(visited[i]) continue;
-            
-            unordered_set<string> vis;
-            vector<string> acc;
-            acc.push_back(accounts[i][0]);
-            
-            for(int j = 1; j < accounts[i].size(); j++){
-                if(vis.find(accounts[i][j]) == vis.end()){
-                    acc.push_back(accounts[i][j]);
-                    vis.insert(accounts[i][j]);
+            par[i] = i;
+        }
+        for(int i = 0; i < n; i++){
+            vector<string> emails = accounts[i];
+            for(int j = 1; j < emails.size(); j++){
+                if(umap.find(emails[j]) == umap.end()){
+                    umap[emails[j]] = i;
+                }
+                else{
+                    int paru = findPar(par[i], par);
+                    int parv = findPar(umap[emails[j]], par);
+                    par[parv] = paru;
                 }
             }
-            int ind = 1;
-            
-            while(ind < acc.size()){
-                
-                for(int j = i + 1; j < n; j++){
-                    if(visited[j]) continue;
-                    
-                    if(accounts[j][0] == accounts[i][0]){
-                        bool flag = false;
-                        if(help[j].find(acc[ind]) != help[j].end()){
-                            visited[j] = 1;
-                            for(int k = 1; k < accounts[j].size(); k++){
-                                if(vis.find(accounts[j][k]) == vis.end()){
-                                    acc.push_back(accounts[j][k]);
-                                    vis.insert(accounts[j][k]);
-                                }
-                            }
-                        }
-                    }
-                }
-                ind++;
-            }
-            sort(acc.begin() + 1, acc.end());
-            res.push_back(acc);
         }
-        return res;
-    }
-};
-
-//Method - 2
-//Using DFS
-//Recursive
-class Solution {
-public:
-    void dfs(string email, vector<string> &emailList, unordered_map<string,bool> &visited, unordered_map<string, vector<string>> &graph){
-        
-        visited[email] = true;
-        emailList.push_back(email);
-        
-        for(string emails: graph[email]){
-            if(!visited[emails]){
-                dfs(emails, emailList, visited, graph);
-            }
+        for(int i = 0; i < n; i++){
+            par[i] = findPar(i, par);
         }
-    }
-    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        
-        unordered_map<string,bool> visited;
-        unordered_map<string, vector<string>> graph;
-        unordered_map<string,string> emailNameRelation;
-        
-        for(int i = 0; i < accounts.size(); i++){
-            int n = accounts[i].size();
-            string name = accounts[i][0];
-            emailNameRelation[accounts[i][1]] = name;
-            visited[accounts[i][1]] = false;
-            
-            for(int j = 2; j < n; j++){
-                visited[accounts[i][j]] = false;
-                emailNameRelation[accounts[i][j]] = name;
-                graph[accounts[i][j]].push_back(accounts[i][j-1]);
-                graph[accounts[i][j-1]].push_back(accounts[i][j]);
-            }
-        }
-        
+        unordered_map<int,vector<string>> mp;
         vector<vector<string>> res;
-        
-        for(int i = 0; i < accounts.size(); i++){
-            for(int j = 1; j < accounts[i].size(); j++){
-                
-                if(!visited[accounts[i][j]]){
-                    vector<string> emailList;
-                    dfs(accounts[i][j], emailList, visited, graph);
-                    emailList.push_back(emailNameRelation[accounts[i][j]]);
-                    
-                    reverse(emailList.begin(),emailList.end());
-                    sort(emailList.begin() + 1, emailList.end());
-                    res.push_back(emailList);
-                }
-            }
+        for(auto &x: umap){
+            auto[str, u] = x;
+            mp[par[u]].push_back(str);
+        }
+        for(auto &x: mp){
+            vector<string> vec = x.second;
+            sort(vec.begin(), vec.end());
+            vec.insert(vec.begin(), accounts[x.first][0]);
+            res.push_back(vec);
         }
         return res;
     }
