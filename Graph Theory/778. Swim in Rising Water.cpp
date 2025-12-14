9e0e691d9334d8fd67140fd185888139e86128e3
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-14 10:56:31 +0530

    Update 778. Swim in Rising Water.cpp

--- Graph Theory/778. Swim in Rising Water.cpp
+++ Graph Theory/778. Swim in Rising Water.cpp
@@ -1,52 +1,4 @@
-//Path With Minimum Maximum Value
-class Solution {
-public:
-    int dx[4] = {-1,1,0,0};
-    int dy[4] = {0,0,-1,1};
-    #define pipii pair<int,pair<int,int>>
-    struct cmp{
-        bool operator()(pipii p1, pipii p2){
-            return p1.first > p2.first;
-        }
-    };
-    int swimInWater(vector<vector<int>>& grid) {
-        
-        int m = grid.size(), n = grid[0].size();
-        
-        vector<vector<int>> vis(m, vector<int>(n, 0));
-        
-        priority_queue<pipii, vector<pipii>, cmp> pq;
-        
-        pq.push({grid[0][0],{0,0}});
-        
-        int ans = INT_MIN;
-        
-        while(!pq.empty()){
-            
-            int currval = pq.top().first, x = pq.top().second.first, y = pq.top().second.second;
-            pq.pop();
-            
-            ans = max(ans, currval);
-            
-            if(x == m - 1 && y == n - 1){
-                return ans;
-            }
-            
-            for(int i = 0; i < 4; i++){
-                int xo = x + dx[i], yo = y + dy[i];
-                if(xo >= 0 && yo >= 0 && xo < m && yo < n){
-                    if(vis[xo][yo] == 0){
-                        vis[xo][yo] = 1;
-                        pq.push({grid[xo][yo],{xo,yo}});
-                    }
-                }
-            }
-        }
-        return -1;
-    }
-};
-
-//Method - 2
+//Method - 1
 class Solution {
 public:
     #define pipii pair<int,pair<int,int>>
@@ -87,5 +39,35 @@ public:
             }
         }
         return 0;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({grid[0][0], 0, 0});
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        while(!pq.empty()){
+            auto x = pq.top();
+            pq.pop();
+            int t = x[0], i = x[1], j = x[2];
+            if(i == n - 1 && j == n - 1){
+                return t;
+            }
+            if(vis[i][j]) continue;
+            vis[i][j] = 1;
+            for(int dir = 0; dir < 4; dir++){
+                int di = i + dx[dir], dj = j + dy[dir];
+                if(di >= 0 && dj >= 0 && di < n && dj < n && !vis[di][dj]){
+                    pq.push({max(grid[di][dj], t), di, dj});
+                }
+            }
+        }
+        return -1;
     }
 };
