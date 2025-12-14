522b42a8e3ea328077f61566ee9f51adda001f53
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-14 12:05:53 +0530

    Update 947. Most Stones Removed with Same Row or Column.cpp

--- Graph Theory/947. Most Stones Removed with Same Row or Column.cpp
+++ Graph Theory/947. Most Stones Removed with Same Row or Column.cpp
@@ -1,3 +1,4 @@
+//Method - 1
 //Ref: https://www.youtube.com/watch?v=beOCN7G4h-M
 class Solution {
 public:
@@ -36,5 +37,50 @@ public:
             }
         }
         return n - cc_count;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int findPar(int node, vector<int>& par){
+        if(par[node] == node){
+            return node;
+        }
+        return par[node] = findPar(par[node], par);
+    }
+    void merge(int nodeRow, int nodeCol, vector<int>& par){
+        int parNodeRow = findPar(nodeRow, par);
+        int parNodeCol = findPar(nodeCol, par);
+        par[parNodeCol] = parNodeRow;
+    }
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int maxRow = 0, maxCol = 0;
+        for(auto &it: stones){
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+        vector<int> par(maxRow + maxCol + 2, -1);
+        vector<int> size(maxRow + maxCol + 2, 0);
+        
+        for(int i = 0; i < maxRow + maxCol + 2; i++){
+            par[i] = i;
+        }
+        unordered_set<int> stoneNodes;
+        for(auto &it: stones){
+            int nodeRow = it[0];
+            int nodeCol = maxRow + it[1] + 1;
+            merge(nodeRow, nodeCol, par);
+            stoneNodes.insert(nodeRow);
+            stoneNodes.insert(nodeCol);
+        }
+        int cnt = 0;
+        for(auto &node: stoneNodes){
+            if(findPar(node, par) == node){
+                cnt++;
+            }
+        }
+        return n - cnt;
     }
 };
