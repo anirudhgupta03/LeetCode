44e647c3c117f25393ef2e047695b5000ba0f450
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-15 10:14:57 +0530

    Update 815. Bus Routes.cpp

--- Graph Theory/815. Bus Routes.cpp
+++ Graph Theory/815. Bus Routes.cpp
@@ -1,3 +1,5 @@
+//Method - 1
+//Gives TLE
 class Solution {
 public:
     #define pii pair<int,int>
@@ -43,6 +45,47 @@ public:
                 if(vis.find({x.first, x.second}) != vis.end()) continue;
                 if(x.first == bus) pq.push({count, {x.second, x.first}});
                 else               pq.push({count + 1,{x.second, x.first}});
+            }
+        }
+        return -1;
+    }
+};
+
+//Method - 2
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        
+        unordered_map<int,vector<int>> busesInCity;
+
+        for(int i = 0; i < routes.size(); i++){
+            for(auto &city: routes[i]){
+                busesInCity[city].push_back(i);
+            }
+        }
+
+        queue<pair<int,int>> q;
+        unordered_map<int,bool> visBus, visBusStop;
+        q.push({source, 0});
+        visBusStop[source] = true;
+
+        while(!q.empty()){
+            int sz = q.size();
+            while(sz--){
+                auto[cityNo, busesTaken] = q.front();
+                q.pop();
+
+                if(cityNo == target) return busesTaken;
+
+                for(auto &bus: busesInCity[cityNo]){
+                    if(visBus[bus]) continue;
+                    visBus[bus] = true;
+                    for(auto &busStops: routes[bus]){
+                        if(visBusStop[busStops]) continue;
+                        q.push({busStops, busesTaken + 1});
+                        visBusStop[busStops] = true;
+                    }
+                }
             }
         }
         return -1;
