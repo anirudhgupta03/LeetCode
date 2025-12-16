beb8a437b92ab290f7829a9cdbb14e5926dbe502
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-16 17:52:51 +0530

    Update 198. House Robber.cpp

--- DP/198. House Robber.cpp
+++ DP/198. House Robber.cpp
@@ -1,71 +1,44 @@
-//Method - 1
+//Recursion + Memoization
 class Solution {
 public:
+    int maxRob(int ind, vector<int>& nums, vector<int>& dp){
+        if(ind >= nums.size()) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        return dp[ind] = max(maxRob(ind + 2, nums, dp) + nums[ind], maxRob(ind + 1, nums, dp));
+    }
     int rob(vector<int>& nums) {
-        
         int n = nums.size();
-        
-        if(n == 1){
-            return nums[0];
-        }
-        
-        int dp[n];
-        
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        
-        int max1 = dp[0];
-        int max2 = max(dp[0],dp[1]);
-        
-        for(int i = 2; i < n; i++){
-            
-            if(i % 2 == 0){
-                dp[i] = max1 + nums[i];
-                max1 = max(max2,dp[i]);
-            }
-            else{
-                dp[i] = max2 + nums[i];
-                max2 = max(max1,dp[i]);
-            }
-        }
-        return max(dp[n-1],dp[n-2]);
+        vector<int> dp(n, -1);
+        return maxRob(0, nums, dp);
     }
 };
 
-//Method - 2
+//Tabulation
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
         int n = nums.size();
-        
         if(n == 1) return nums[0];
-        
+        int val1 = nums[0];
+        int val2 = max(nums[0], nums[1]);
         vector<int> dp(n);
-        
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        
+        dp[0] = val1;
+        dp[1] = val2;
         for(int i = 2; i < n; i++){
             dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
         }
-        
         return dp[n - 1];
     }
 };
 
-//Method - 3
+//Tabluation + Space Optimization
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
         int n = nums.size();
-        
         if(n == 1) return nums[0];
-        
         int val1 = nums[0];
         int val2 = max(nums[0], nums[1]);
-        
         for(int i = 2; i < n; i++){
             int val3 = max(val2, val1 + nums[i]);
             val1 = val2;
