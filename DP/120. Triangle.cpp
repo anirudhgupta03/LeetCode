ae5634981c4ccf69d80d8b19da1bf26b5fa6fbb8
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-17 10:23:31 +0530

    Update 120. Triangle.cpp

--- DP/120. Triangle.cpp
+++ DP/120. Triangle.cpp
@@ -20,19 +20,13 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
-        dp[0][0] = triangle[0][0];
-        for(int i = 1; i < n; i++){
-            for(int j = 1; j < i; j++){
-                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
+        for(int j = 0; j < n; j++) dp[n - 1][j] = triangle[n - 1][j];
+        for(int i = n - 2; i >= 0; i--){
+            for(int j = i; j >= 0; j--){
+                dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
             }
-            dp[i][0] = dp[i - 1][0] + triangle[i][0];
-            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
         }
-        int minSum = INT_MAX;
-        for(int j = 0; j < n; j++){
-            minSum = min(minSum, dp[n - 1][j]);
-        }
-        return minSum;
+        return dp[0][0];
     }
 };
 
@@ -42,20 +36,14 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<int> dp(n, 0);
-        dp[0] = triangle[0][0];
-        for(int i = 1; i < n; i++){
+        for(int j = 0; j < n; j++) dp[j] = triangle[n - 1][j];
+        for(int i = n - 2; i >= 0; i--){
             vector<int> currdp(n, 0);
-            for(int j = 1; j < i; j++){
-                currdp[j] = min(dp[j], dp[j - 1]) + triangle[i][j];
+            for(int j = i; j >= 0; j--){
+                currdp[j] = min(dp[j], dp[j + 1]) + triangle[i][j];
             }
-            currdp[0] = dp[0] + triangle[i][0];
-            currdp[i] = dp[i - 1] + triangle[i][i];
             dp = currdp;
         }
-        int minSum = INT_MAX;
-        for(int j = 0; j < n; j++){
-            minSum = min(minSum, dp[j]);
-        }
-        return minSum;
+        return dp[0];
     }
 };
