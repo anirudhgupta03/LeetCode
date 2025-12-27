cfa32c78aa4aea71b5c368560d8f6cac32c75149
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-27 17:53:22 +0530

    Update 1143. Longest Common Subsequence.cpp

--- DP/1143. Longest Common Subsequence.cpp
+++ DP/1143. Longest Common Subsequence.cpp
@@ -36,14 +36,7 @@ public:
         int l1 = text1.length();
         int l2 = text2.length();
         
-        int dp[l1+1][l2+1];
-        
-        for(int i = 0; i < l1 + 1; i++){
-            for(int j = 0; j < l2 + 1; j++){
-                dp[i][j] = 0;
-            }
-        }
-        
+        vector<vector<int>> dp(l1+1, vector<int>(l2+1,0));
         for(int i = 1; i < l1 + 1; i++){
             for(int j = 1; j < l2 + 1; j++){
                 if(text1[i-1] == text2[j-1]){
