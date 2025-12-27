645ff8accf3840249c1c9157f0eabc32ad960cbc
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-27 14:18:29 +0530

    Update 518. Coin Change 2.cpp

--- DP/518. Coin Change 2.cpp
+++ DP/518. Coin Change 2.cpp
@@ -3,30 +3,23 @@
 //SC: O(n*amount) + Auxilliary Stack Space
 class Solution {
 public:
-    int solve(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp){
+    int findWaysToChange(int ind, int amount, vector<int>& coins, vector<vector<int>>& dp){
+        if(amount == 0) return 1;
+        if(ind == 0) return 0;
         
-        if(amount == 0){
-            return 1;
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+        
+        int take = 0;
+        if(coins[ind - 1] <= amount){
+            take = findWaysToChange(ind, amount - coins[ind - 1], coins, dp);
         }
-        if(ind == coins.size()){
-            return 0;
-        }
-        if(dp[ind][amount] != -1){
-            return dp[ind][amount];
-        }
-        int count = 0;
-        if(coins[ind] <= amount){
-            count += solve(ind, amount - coins[ind], coins, dp);
-        }
-        count += solve(ind + 1, amount, coins, dp);
-        return dp[ind][amount] = count;
+        int notTake = findWaysToChange(ind - 1, amount, coins, dp);
+        return dp[ind][amount] = take + notTake;
     }
     int change(int amount, vector<int>& coins) {
-        
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        
-        return solve(0, amount, coins, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        return findWaysToChange(n, amount, coins, dp);
     }
 };
 
@@ -38,58 +31,53 @@ public:
     int change(int amount, vector<int>& coins) {
         
         int n = coins.size();
-        
-        int dp[n+1][amount+1];
-        
-        for(int j = 1; j < amount + 1; j++){
-            dp[0][j] = 0;
-        }
-        
-        for(int i = 0; i < n + 1; i++){
-            dp[i][0] = 1;
-        }
-        
-        for(int i = 1; i < n + 1; i++){
-            for(int j = 1; j < amount + 1; j++){
-                
-                if(coins[i-1] <= j){
-                    dp[i][j] = dp[i][j-coins[i-1]]+dp[i-1][j];
-                }
+        vector<vector<double>> dp(n + 1, vector<double>(amount + 1, 0));
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= amount; j++){
+                if(j == 0) dp[i][j] = 1;
+                else if(i == 0) dp[i][j] = 0;
                 else{
-                    dp[i][j] = dp[i-1][j];
+                    double take = 0;
+                    if(coins[i - 1] <= j){
+                        take = dp[i][j - coins[i - 1]];
+                    }
+                    double notTake = dp[i - 1][j];
+                    dp[i][j] = take + notTake;
                 }
             }
         }
-        return dp[n][amount];
+        return (int)dp[n][amount];
     }
 };
 
-//Bottom-Up
+//Bottom-Up (Tabulation + Space Optimization)
 //TC: O(n*amount)
 //SC: O(amount)
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        
         int n = coins.size();
-        
-        vector<int> pre(amount + 1, 0), curr(amount + 1);
-        pre[0] = 1;
-        
-        for(int i = 1; i <= n; i++){
+        vector<double> prevDP(amount + 1, 0);
+        for(int i = 0; i <= n; i++){
+            vector<double> currDP(amount + 1, 0);
             for(int j = 0; j <= amount; j++){
                 if(j == 0){
-                    curr[j] = 1;
+                    currDP[j] = 1;
                 }
-                else if(coins[i - 1] <= j){
-                    curr[j] = curr[j - coins[i - 1]] + pre[j];
+                else if(i == 0){
+                    currDP[j] = 0;
                 }
                 else{
-                    curr[j] = pre[j];
+                    double take = 0;
+                    if(coins[i - 1] <= j){
+                        take = currDP[j - coins[i - 1]];
+                    }
+                    double notTake = prevDP[j];
+                    currDP[j] = take + notTake;
                 }
             }
-            pre = curr;
+            prevDP = currDP;
         }
-        return curr[amount];
+        return (int)prevDP[amount];
     }
 };
