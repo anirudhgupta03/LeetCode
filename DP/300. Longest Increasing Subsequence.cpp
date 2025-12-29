1319964a4286ad0605ccef06a6e36d69fcb7dd9c
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-29 17:59:44 +0530

    Update 300. Longest Increasing Subsequence.cpp

--- DP/300. Longest Increasing Subsequence.cpp
+++ DP/300. Longest Increasing Subsequence.cpp
@@ -1,3 +1,73 @@
+//Top-Down
+//TC: O(N*N)
+//SC: O(N*N) + Auxilliary Stack Space
+class Solution {
+public:
+    int findLengthOfLIS(int ind, int preInd, vector<int>& nums, vector<vector<int>> &dp){
+        if(ind == nums.size() + 1) return 0;
+        if(dp[ind][preInd] != -1) return dp[ind][preInd];
+        int take = 0;
+        if(preInd == 0 || nums[ind - 1] > nums[preInd - 1]){
+            take = findLengthOfLIS(ind + 1, ind, nums, dp) + 1;
+        }
+        int notTake = findLengthOfLIS(ind + 1, preInd, nums, dp);
+        return dp[ind][preInd] = max(take, notTake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        return findLengthOfLIS(1, 0, nums, dp);
+    }
+};
+
+//Bottom-Up
+//Tabulation
+//TC: O(N*N)
+//SC: O(N*N)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n + 2, vector<int>(n + 1, 0));
+        for(int ind = n; ind >= 1; ind--){
+            for(int preInd = n; preInd >= 0; preInd--){
+                int take = 0;
+                if(preInd == 0 || nums[ind - 1] > nums[preInd - 1]){
+                    take = dp[ind + 1][ind] + 1;
+                }
+                int notTake = dp[ind + 1][preInd];
+                dp[ind][preInd] = max(take, notTake);
+            }
+        }
+        return dp[1][0];
+    }
+};
+
+//Bottom-Up
+//Tabulation + Space Optimization
+//TC: O(N*N)
+//SC: O(N)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prevDP(n + 1, 0);
+        for(int ind = n; ind >= 1; ind--){
+            vector<int> currDP(n + 1, 0);
+            for(int preInd = n; preInd >= 0; preInd--){
+                int take = 0;
+                if(preInd == 0 || nums[ind - 1] > nums[preInd - 1]){
+                    take = prevDP[ind] + 1;
+                }
+                int notTake = prevDP[preInd];
+                currDP[preInd] = max(take, notTake);
+            }
+            prevDP = currDP;
+        }
+        return prevDP[0];
+    }
+};
+
 //TC: O(N*N)
 //SC: O(N)
 class Solution {
