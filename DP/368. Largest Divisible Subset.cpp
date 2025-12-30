d6a0749f4e0b648823d1439223b573bb72eb3756
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-30 09:09:06 +0530

    Update 368. Largest Divisible Subset.cpp

--- DP/368. Largest Divisible Subset.cpp
+++ DP/368. Largest Divisible Subset.cpp
@@ -1,3 +1,52 @@
+//Method without altering the original array
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        vector<pair<int,int>> tnums(n);
+        for(int i = 0; i < n; i++){
+            tnums[i] = {nums[i], i};
+        }
+        sort(tnums.begin(), tnums.end());
+
+        vector<int> dp(n, 1), hash(n);
+        for(int i = 0; i < n; i++){
+            hash[i] = i;
+        }
+
+        int maxLen = 1, maxInd = 0;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if((tnums[i].first % tnums[j].first) == 0){
+                    if(dp[j] + 1 > dp[i]){
+                        dp[i] = dp[j] + 1;
+                        hash[i] = j;
+                    }
+                }
+            }
+            if(dp[i] > maxLen){
+                maxLen = dp[i];
+                maxInd = i;
+            }
+        }
+
+        vector<int> res;
+        while(hash[maxInd] != maxInd){
+            res.push_back(tnums[maxInd].second);
+            maxInd = hash[maxInd];
+        }
+        res.push_back(tnums[maxInd].second);
+        sort(res.begin(), res.end());
+        
+        for(int i = 0; i < res.size(); i++){
+            res[i] = nums[res[i]];
+        }
+        return res;
+    }
+};
+
 //LIS Variant
 //Ref: https://www.youtube.com/watch?v=Wv6DlL0Sawg
 //Method - 1
