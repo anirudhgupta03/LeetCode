6e1fb6b7e22ed98f0a985a11d0b182e2218c097f
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2025-12-30 20:16:02 +0530

    Update 1547. Minimum Cost to Cut a Stick.cpp

--- DP/1547. Minimum Cost to Cut a Stick.cpp
+++ DP/1547. Minimum Cost to Cut a Stick.cpp
@@ -1,31 +1,60 @@
+//Top-Down
 //TC: O(n*n*n)
 //SC: O(n*n) + Auxilliary Stack Space
 class Solution {
 public:
-    int solve(int i, int j, vector<int> &cuts, vector<vector<int>> &dp){
+    int findMinCost(int l, int r, vector<int> &tcuts, vector<vector<int>> &dp){
+        if(l > r) return 0;
         
-        if(i > j){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
+        if(dp[l][r] != -1) return dp[l][r];
         
-        int mini = INT_MAX;
-        
-        for(int ind = i; ind <= j; ind++){
-            int cost = cuts[j + 1] - cuts[i - 1] + solve(i, ind - 1, cuts, dp) + solve(ind + 1, j, cuts, dp);
-            mini = min(mini, cost);
+        int minCost = INT_MAX;
+        for(int k = l; k <= r; k++){
+            int p1 = findMinCost(l, k - 1, tcuts, dp);
+            int p2 = findMinCost(k + 1, r, tcuts, dp);
+            if(p1 != INT_MAX && p2 != INT_MAX){
+                minCost = min(minCost, tcuts[r + 1] - tcuts[l - 1] + p1 + p2);
+            }
         }
-        return dp[i][j] = mini;
+        return dp[l][r] = minCost;
     }
     int minCost(int n, vector<int>& cuts) {
+        vector<int> tcuts = cuts;
+        tcuts.push_back(0);
+        tcuts.push_back(n);
+        sort(tcuts.begin(), tcuts.end());
         
-        cuts.insert(cuts.begin(), 0);
-        cuts.push_back(n);
-        sort(cuts.begin(), cuts.end());
-        n = cuts.size();
-        vector<vector<int>> dp(n, vector<int>(n, - 1));
-        return solve(1, n - 2, cuts, dp);
+        int len = tcuts.size();
+        vector<vector<int>> dp(len, vector<int>(len, -1));
+        return findMinCost(1, tcuts.size() - 2, tcuts, dp);
+    }
+};
+
+//Bottom-Up
+//TC: O(n*n*n)
+//SC: O(n*n)
+class Solution {
+public:
+    int minCost(int n, vector<int>& cuts) {
+        
+        vector<int> tcuts = cuts;
+        tcuts.push_back(0);
+        tcuts.push_back(n);
+        sort(tcuts.begin(), tcuts.end());
+
+        int len = tcuts.size();
+
+        vector<vector<int>> dp(len, vector<int>(len, 0));
+        
+        for(int l = len - 2; l >= 1; l--){
+            for(int r = l; r <= len - 2; r++){
+                int minCost = INT_MAX;
+                for(int k = l; k <= r; k++){
+                    minCost = min(minCost, tcuts[r + 1] - tcuts[l - 1] + dp[l][k - 1] + dp[k + 1][r]);
+                }
+                dp[l][r] = minCost;
+            }
+        }
+        return dp[1][len - 2];
     }
 };
