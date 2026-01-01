be5a15a7d1e6cbe5d68acb8dba814b6543fa2c86
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2026-01-01 13:34:02 +0530

    Update 1043. Partition Array for Maximum Sum.cpp

--- DP/1043. Partition Array for Maximum Sum.cpp
+++ DP/1043. Partition Array for Maximum Sum.cpp
@@ -29,7 +29,7 @@ class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n, 0);
+        vector<int> dp(n + 1, 0);
         for(int i = n - 1; i >= 0; i--){
             int maxSum = 0, maxVal = 0;
             for(int j = i; j < min(i + k, n); j++){
