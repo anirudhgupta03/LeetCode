17e02d686c7e0ec643c647a8bf450b000e2710c9
Anirudh Gupta <72066036+anirudhgupta03@users.noreply.github.com>
2026-04-25 12:13:50 +0530

    Update 1707. Maximum XOR With an Element From Array.cpp

--- Trie/1707. Maximum XOR With an Element From Array.cpp
+++ Trie/1707. Maximum XOR With an Element From Array.cpp
@@ -1,86 +1,70 @@
 //Ref: https://www.youtube.com/watch?v=Q8LhG9Pi5KM
+struct TrieNode{
+    TrieNode* child[2];
+    TrieNode(){
+        child[0] = child[1] = nullptr;
+    }
+};
 class Solution {
 public:
-    #define ll long long
-    struct TrieNode{
-        TrieNode* child[2];
-        TrieNode(){
-            for(int i = 0; i < 2; i++){
-                child[i] = NULL;
+    void insert(TrieNode* curr, int x){
+        for(int i = 30; i >= 0; i--){
+            int bit = 0;
+            if(x & (1 << i)){
+                bit = 1;
             }
-        }
-    };
-    vector<int> binary(int num){
-        
-        vector<int> v(32);
-        int ind = 31;
-        while(ind >= 0){
-            v[ind] = num % 2;
-            num /= 2;
-            ind--;
-        }
-        
-        return v;
-    }
-    void insert(TrieNode* root, vector<int> bits){
-        
-        for(int i = 0; i < bits.size(); i++){
-            if(root -> child[bits[i]] == NULL){
-                root -> child[bits[i]] = new TrieNode();
+            if(curr -> child[bit] == nullptr){
+                curr -> child[bit] = new TrieNode();
             }
-            root = root -> child[bits[i]];
+            curr = curr -> child[bit];
         }
     }
-    int findMaxXOR(TrieNode* root, vector<int> bits){
-        
-        ll maxxor = 0;
-        for(int i = 0; i < bits.size(); i++){
-            if(root -> child[1 - bits[i]]){
-                maxxor += pow(2, 31 - i);
-                root = root -> child[1 - bits[i]];
+    int findMaxXOR(TrieNode* curr, int x){
+        int ans = 0;
+        for(int i = 30; i >= 0; i--){
+            int bit = 0;
+            if(x & (1 << i)){
+                bit = 1;
+            }
+            if(curr -> child[1 - bit]){
+                ans += (1 << i);
+                curr = curr -> child[1 - bit];
             }
             else{
-                root = root -> child[bits[i]];
+                curr = curr -> child[bit];
             }
+            if(curr == nullptr) return -1;
         }
-        return maxxor;
+        return ans;
     }
-    bool static cmp(vector<int> &v1, vector<int> &v2){
+    bool static cmp(vector<int>& v1, vector<int>& v2){
         return v1[1] < v2[1];
     }
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
         
-        TrieNode* root = new TrieNode();
-        
+        int n = nums.size();
+
+        TrieNode* head = new TrieNode();
+
         sort(nums.begin(), nums.end());
-        
-        for(int i = 0; i < queries.size(); i++){
+
+        for(int i = 0; i< queries.size(); i++){
             queries[i].push_back(i);
         }
-        
         sort(queries.begin(), queries.end(), cmp);
-        
-        vector<int> res(queries.size(), - 1);
-        
-        int ind = 0;
-        
-        for(int i = 0; i < queries.size(); i++){
-            int x = queries[i][0], m = queries[i][1], index = queries[i][2];
-            while(ind < nums.size()){
-                if(nums[ind] <= m){
-                    insert(root, binary(nums[ind]));
-                    ind++;
-                }
-                else{
-                    break;
-                }
+
+        int i = 0;
+        vector<int> res(queries.size());
+
+        for(int j = 0; j < queries.size(); j++){
+            while(i < n && nums[i] <= queries[j][1]){
+                insert(head, nums[i]);
+                i++;
             }
-            if(ind == 0) res[index] = -1;
-            else{
-                int maxxor = findMaxXOR(root, binary(x));
-                res[index] = maxxor;
-            }
+            int maxXOR = findMaxXOR(head, queries[j][0]);
+            res[queries[j][2]] = maxXOR;
         }
+        
         return res;
     }
 };
